ce37926e1009fe8f6d91a0196083b9c740c76a58
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2011-09-12 21:45:38 +0200

    Added an AngularJS example

---
+++ todo-example/angularjs/js/directive.js
@@ -0,0 +1,22 @@
+
+angular.directive('my:blur', function(expression, compiledElement) {
+    var compiler = this;
+    return function(linkElement) {
+        var scope = this;
+        linkElement.bind('blur', function(event) {
+            scope.$apply(expression, linkElement);
+            event.stopPropagation();
+        });
+    };
+});
+
+
+angular.directive("my:focus", function(expression, compiledElement){
+  return function(element){
+    this.$watch(expression, function(){
+    if(angular.formatter.boolean.parse(expression) && element.length > 0){
+      element[0].focus();
+    } 
+    }, element);
+  };
+});
