8ba31b16112b45592f741aa840009417f7ff2a69
addyosmani <addyosmani@gmail.com>
2011-10-20 15:01:20 +0100

    patching broke.js app initialization, preparing index for 0.2 release

--- todo-example/broke/project/my-blog.js
+++ todo-example/broke/project/my-blog.js
@@ -4,9 +4,9 @@
     $(function(){
         broke.init('todo.settings', function(){
             // fill db
-            todo.models.Task.objects.create({"pk": 1, "model": "todo.task", "fields": {"is_complete": false, "title": "Title A"}});
-            todo.models.Task.objects.create({"pk": 2, "model": "todo.task", "fields": {"is_complete": false, "title": "Title B"}});
-            todo.models.Task.objects.create({"pk": 3, "model": "todo.task", "fields": {"is_complete": false, "title": "Title C"}});
+            //todo.models.Task.objects.create({"pk": 1, "model": "todo.task", "fields": {"is_complete": false, "title": "Title A"}});
+            //todo.models.Task.objects.create({"pk": 2, "model": "todo.task", "fields": {"is_complete": false, "title": "Title B"}});
+            //todo.models.Task.objects.create({"pk": 3, "model": "todo.task", "fields": {"is_complete": false, "title": "Title C"}});
             
             broke.events.request('/');
         });
