091cbae155b934e9d3fe631543c44adaf132dcdd
Sindre Sorhus <sindresorhus@gmail.com>
2012-01-04 20:13:10 +0100

    TodoMVC template

---
+++ todo-example/template/js/app.js
@@ -0,0 +1,11 @@
+/*
+
+[MIT licensed](http://en.wikipedia.org/wiki/MIT_License)
+(c) [You](http://addyosmani.github.com/todomvc/)
+
+*/
+(function() {
+
+// Your starting point. Enjoy the ride!
+
+})();
