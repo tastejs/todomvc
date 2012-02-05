e1b9c5b6b6509ac4bb223257e511ef9b27e2ef1b
Camille Reynders <camille.reynders@ibbt.be>
2012-02-05 19:47:55 +0100

    updated dijon

--- architecture-examples/dijon/js/libs/dijon.js
+++ architecture-examples/dijon/js/libs/dijon.js
@@ -81,7 +81,6 @@ dijon.System.prototype = {
 	_createAndSetupInstance : function( key, clazz ){
 		var instance = new clazz();
 		this.injectInto( instance, key );
-		if( "setup" in instance ) instance.setup.call( instance );
 		return instance;
 	},
 
@@ -154,6 +153,7 @@ dijon.System.prototype = {
      * @param {String} sourceKey the key mapped to the object that will be injected
      * @param {String} [targetKey='global'] the key the outlet is assigned to.
 	 * @param {String} [outletName=sourceKey] the name of the property used as an outlet.<br/>
+     * @return {dijon.System}
      * @see dijon.System#unmapOutlet
 	 */
 	mapOutlet : function( sourceKey, targetKey, outletName ){
@@ -161,7 +161,9 @@ dijon.System.prototype = {
         if( targetKey == undefined ) targetKey = "global";
         if( outletName == undefined ) outletName = sourceKey;
         if( ! this._outlets.hasOwnProperty( targetKey ) ) this._outlets[ targetKey ] = {};
-		this._outlets[ targetKey ][ outletName ] = sourceKey
+		this._outlets[ targetKey ][ outletName ] = sourceKey;
+
+        return this;
 	},
 
 	/**
@@ -184,6 +186,7 @@ dijon.System.prototype = {
      * var b = system.getObject( 'foo' ); //now contains 'bar'
 	 * @param {String} key
 	 * @param {Object} useValue
+     * @return {dijon.System}
 	 */
 	mapValue : function( key, useValue ){
 	    if( key == undefined ) throw new Error( 1010 );
@@ -193,6 +196,7 @@ dijon.System.prototype = {
             isSingleton : true
 		}
         if( this.autoMapOutlets ) this.mapOutlet( key );
+        return this;
 	},
 
 	/**
@@ -222,6 +226,7 @@ dijon.System.prototype = {
      *
 	 * @param {String} key
 	 * @param {Function} clazz
+     * @return {dijon.System}
 	 */
 	mapClass : function( key, clazz ){
 	    if( key == undefined ) throw new Error( 1010 );
@@ -232,6 +237,7 @@ dijon.System.prototype = {
 				isSingleton : false
         }
         if( this.autoMapOutlets ) this.mapOutlet( key );
+        return this;
 	},
 
     /**
@@ -248,6 +254,7 @@ dijon.System.prototype = {
      *
      * @param {String} key
      * @param {Function} clazz
+     * @return {dijon.System}
      */
     mapSingleton : function( key, clazz ){
         if( key == undefined ) throw new Error( 1010 );
@@ -258,11 +265,22 @@ dijon.System.prototype = {
             isSingleton : true
         }
         if( this.autoMapOutlets ) this.mapOutlet( key );
+        return this;
     },
 
 	/**
      * Force instantiation of the object mapped to <code>key</code>, whether it was mapped as a singleton or not.
-	 * @param {Class} keyOrClass
+     * @example
+     * var SomeClass = function(){
+     * }
+     * system.mapSingleton( 'o', SomeClass );
+     *
+     * var s1 = system.getInstance( 'o' );
+     * var s2 = system.instantiate( 'o' );
+     *
+     * //s1 and s2 reference different instances of SomeClass
+     *
+	 * @param {String} key
 	 * @return {Object}
 	 */
 	instantiate : function( key ){
@@ -271,9 +289,28 @@ dijon.System.prototype = {
 	},
 
 	/**
-	 * Perform an injection into an object, satisfying all it's dependencies
+	 * Perform an injection into an object's mapped outlets, satisfying all it's dependencies
+     * @example
+     * var UserModel = function(){
+     * }
+     * system.mapSingleton( 'userModel', UserModel );
+     * var SomeClass = function(){
+     *      user = undefined; //inject
+     * }
+     * system.mapSingleton( 'o', SomeClass );
+     * system.mapOutlet( 'userModel', 'o', 'user' );
+     *
+     * var foo = {
+     *      user : undefined //inject
+     * }
+     *
+     * system.injectInto( foo, 'o' );
+     *
+     * //foo.user now holds a reference to the singleton instance of UserModel
 	 * @param {Object} instance
-     * @param {String} [key]
+     * @param {String} [key] use the outlet mappings as defined for <code>key</code>, otherwise only the globally
+     * mapped outlets will be used.
+     * @return {dijon.System}
 	 */
 	injectInto : function( instance, key ){
         if( instance == undefined ) throw new Error( 1010 );
@@ -288,38 +325,128 @@ dijon.System.prototype = {
                 if( ! this.strictInjections || outlet in instance ) instance[ outlet ] = this.getObject( source );
             }
         }
+        if( "setup" in instance ) instance.setup.call( instance );
 
-        return instance;
+        return this;
 	},
 
 	/**
-	 * Remove a rule from the System
+	 * Remove the mapping of <code>key</code> from the system
 	 * @param {String} key
+     * @return {dijon.System}
 	 */
 	unmap : function( key ){
         if( key == undefined ) throw new Error( 1010 );
 		delete this._mappings[ key ];
+
+        return this;
 	},
 
 	/**
-	 * removes an injection point mapping for a given class mapped to <code>key</code>
+	 * removes an injection point mapping for a given object mapped to <code>key</code>
 	 * @param {String} target
 	 * @param {String} outlet
+     * @return {dijon.System}
 	 * @see dijon.System#addOutlet
 	 */
 	unmapOutlet : function( target, outlet ){
         if( target == undefined ) throw new Error( 1010 );
         if( outlet == undefined ) throw new Error( 1010 );
 		delete this._outlets[ target ][ outlet ];
+
+        return this;
 	},
 
     /**
+     * maps a handler for an event/route.<br/>
+     * @example
+     * var userView = {
+     *      showUserProfile : function(){
+     *          //do stuff
+     *      }
+     * }
+     * system.mapValue( 'userView', userView );
+     * <strong>system.mapHandler( 'user/profile', 'userView', 'showUserProfile' );</strong>
+     * system.notify( 'user/profile' );
      *
-     * @param {String} eventName
-     * @param {String} [key=undefined]
-     * @param {String|Function} [handler=eventName]
-     * @param {Boolean} [oneShot=false]
-     * @param {Boolean} [passEvent=false]
+     * //userView.showUserProfile is called
+     * @example
+     * var userView = {
+     *      showUserProfile : function(){
+     *          //do stuff
+     *      }
+     * }
+     * system.mapValue( 'userView', userView );
+     * <strong>system.mapHandler( 'showUserProfile', 'userView' );</strong>
+     * system.notify( 'showUserProfile' );
+     *
+     * //userView.showUserProfile is called
+     * @example
+     * var userView = {
+     *      showUserProfile : function(){
+     *          //do stuff
+     *      }
+     * }
+     * system.mapValue( 'userView', userView );
+     * <strong>system.mapHandler( 'showUserProfile' );</strong>
+     * system.notify( 'showUserProfile' );
+     *
+     * //userView.showUserProfile is called
+     * @example
+     * var showUserProfile = function(){
+     *          //do stuff
+     * }
+     * <strong>system.mapHandler( 'user/profile', undefined, showUserProfile );</strong>
+     * system.notify( 'user/profile' );
+     *
+     * //showUserProfile is called
+     * @example
+     * var userView = {};
+     * var showUserProfile = function(){
+     *          //do stuff
+     * }
+     * system.mapValue( 'userView', userView );
+     * <strong>system.mapHandler( 'user/profile', 'userView', showUserProfile );</strong>
+     * system.notify( 'user/profile' );
+     *
+     * //showUserProfile is called within the scope of the userView object
+     * @example
+     * var userView = {
+     *      showUserProfile : function(){
+     *          //do stuff
+     *      }
+     * }
+     * system.mapValue( 'userView', userView );
+     * <strong>system.mapHandler( 'user/profile', 'userView', 'showUserProfile', true );</strong>
+     * system.notify( 'user/profile' );
+     * system.notify( 'user/profile' );
+     * system.notify( 'user/profile' );
+     *
+     * //userView.showUserProfile is called exactly once [!]
+     * @example
+     * var userView = {
+     *      showUserProfile : function( route ){
+     *          //do stuff
+     *      }
+     * }
+     * system.mapValue( 'userView', userView );
+     * <strong>system.mapHandler( 'user/profile', 'userView', 'showUserProfile', false, true );</strong>
+     * system.notify( 'user/profile' );
+     *
+     * //userView.showUserProfile is called and the route/eventName is passed to the handler
+     * @param {String} eventName/route
+     * @param {String} [key=undefined] If <code>key</code> is <code>undefined</code> the handler will be mapped for all
+     * mapped objects. <strong>USE WITH EXTREME CAUTION</strong>
+     * @param {String|Function} [handler=eventName] If <code>handler</code> is <code>undefined</code> the value of
+     * <code>eventName</code> will be used as the name of the member holding the reference to the to-be-called function.
+     * <code>handler</code> accepts either a string, which will be used as the name of the member holding the reference
+     * to the to-be-called function, or a direct function reference.
+     * @param {Boolean} [oneShot=false] Defines whether the handler should be called exactly once and then automatically
+     * unmapped
+     * @param {Boolean} [passEvent=false] Defines whether the event object should be passed to the handler or not.
+     * @return {dijon.System}
+     * @see dijon.System#notify
+     * @see dijon.System#unmapHandler
      */
     mapHandler : function( eventName, key, handler, oneShot, passEvent ){
         if( eventName == undefined ) throw new Error( 1010 );
@@ -338,13 +465,19 @@ dijon.System.prototype = {
             oneShot: oneShot,
             passEvent: passEvent
         } );
+
+        return this;
     },
 
     /**
-     *
-     * @param {String} eventName
-     * @param {String} [key=undefined]
+     * Unmaps the handler for a specific event/route.
+     * @param {String} eventName Name of the event/route
+     * @param {String} [key=undefined] If <code>key</code> is <code>undefined</code> the handler is removed from the
+     * global mapping space. (If the same event is mapped globally and specifically for an object, then
+     * only the globally mapped one will be removed)
      * @param {String | Function} [handler=eventName]
+     * @return {dijon.System}
+     * @see dijon.System#mapHandler
      */
     unmapHandler : function( eventName, key, handler  ){
         if( eventName == undefined ) throw new Error( 1010 );
@@ -356,15 +489,18 @@ dijon.System.prototype = {
                 var config = handlers[ i ];
                 if( config.handler == handler ){
                     handlers.splice( i, 1 );
-                    return;
+                    break;
                 }
             }
         }
+        return this;
     },
 
     /**
-     *
-     * @param {String} eventName
+     * calls all handlers mapped to <code>eventName/route</code>
+     * @param {String} eventName/route
+     * @return {dijon.System}
+     * @see dijon.System#mapHandler
      */
     notify : function( eventName ){
         var argsWithEvent = Array.prototype.slice.call( arguments );
@@ -400,6 +536,8 @@ dijon.System.prototype = {
                 }
             }
         }
+
+        return this;
     }
 
 };//dijon.System.prototype
