30b47d891c73a23e3217b4e2b5448189468f8068
Sindre Sorhus <sindresorhus@gmail.com>
2012-02-18 22:41:46 +0100

    Add contributor list to the site
    Dynamically updated from the GitHub API

--- site/js/main.js
+++ site/js/main.js
@@ -2,15 +2,39 @@
 
 	// Demos popover
 	$('#demos li a').popover({
+		delay: 200,
 		placement: 'in right',
 		title: function() {
 			return $(this).text() + '<a href="' + $(this).data('source') + '">Go to site</a>';
 		}
 	}).on('click', '.popover', function(e) {
 		// Prevent click on the popover, but allow links inside
-		if ( e.target.nodeName !== 'A' ) {
+		if ( e.target.nodeName.toLowerCase() !== 'a' ) {
 			e.preventDefault();
 		}
+	});
+
+	// Contributor list
+	$.getJSON('https://api.github.com/repos/addyosmani/todomvc/contributors?callback=?', function( res ) {
+		var data = res.data;
+		// Add some previous contributors not on the GitHub list
+		/*
+		[].push.apply(data, [{
+			login: 'test'
+		}, {
+			login: 'test'
+		}, {
+			login: 'test'
+		}]);
+		*/
+		var ret = $.map( res.data, function( elem ) {
+			var username = elem.login;
+			if ( $.inArray( username, [ 'addyosmani', 'boushley', 'sindresorhus' ] ) >= 0 ) {
+				return;
+			}
+			return '<a href="https://github.com/' + username + '">' + username + '</a>';
+		});
+		$('#contributor-list').show().children('span').html( ret.join(', ') );
 	});
 
 })();
