111533bd0b8df93cd86d6244e4575c385d626e88
Aaron Boushley <boushley@gmail.com>
2012-02-24 17:18:05 -0800

    Updated TaskCompleteButton to extend Ext.Button

--- architecture-examples/extjs/js/view/TaskCompleteButton.js
+++ architecture-examples/extjs/js/view/TaskCompleteButton.js
@@ -1,14 +1,8 @@
 Ext.define('Todo.view.TaskCompleteButton' , {
-    extend: 'Ext.Component',
+    extend: 'Ext.Button',
     alias: 'widget.completeButton',
     tpl: new Ext.XTemplate('<a id="clear-completed">{text}</a>', {compiled: true}),
     setText: function (text) {
         this.update({text:text});
-    },
-    listeners: {
-        render: function (component) {
-            var self = this;
-            component.getEl().on('click', function () { self.fireEvent('click', self); });
-        }
     }
 });
