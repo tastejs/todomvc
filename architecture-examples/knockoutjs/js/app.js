6bfa658c1e5e9cf49c5cf64ec1b2c2eb6f385a29
Ryan Niemeyer <rniemeyer@gmail.com>
2012-02-26 14:01:08 -0600

    cleanup for issue #88.

--- architecture-examples/knockoutjs/js/app.js
+++ architecture-examples/knockoutjs/js/app.js
@@ -1,19 +1,19 @@
-(function() {
+(function () {
     //a custom binding to handle the enter key (could go in a separate library)
     ko.bindingHandlers.enterKey = {
-        init: function(element, valueAccessor, allBindingsAccessor, data) {
+        init:function (element, valueAccessor, allBindingsAccessor, data) {
             var wrappedHandler, newValueAccessor;
 
             //wrap the handler with a check for the enter key
-            wrappedHandler = function(data, event) {
+            wrappedHandler = function (data, event) {
                 if (event.keyCode === 13) {
                     valueAccessor().call(this, data, event);
                 }
             };
 
             //create a valueAccessor with the options that we would want to pass to the event binding
-            newValueAccessor = function() {
-                return { keyup: wrappedHandler };
+            newValueAccessor = function () {
+                return { keyup:wrappedHandler };
             };
 
             //call the real event binding's init function
@@ -21,9 +21,20 @@
         }
     };
 
+    //select text when element is focused
+    ko.bindingHandlers.select = {
+        init:function (element) {
+            var handler = function () {
+                element.select();
+            };
+
+            ko.utils.registerEventHandler(element, "focus", handler);
+        }
+    };
+
     //alternative to "visible" binding that will specifically set "block" to override what is in css
     ko.bindingHandlers.block = {
-        update: function (element, valueAccessor) {
+        update:function (element, valueAccessor) {
             var value = ko.utils.unwrapObservable(valueAccessor());
             element.style.display = value ? "block" : "none";
         }
@@ -31,12 +42,11 @@
 
     //alternative to "visible" binding that will specifically set "inline" to override what is in css
     ko.bindingHandlers.inline = {
-        update: function (element, valueAccessor) {
+        update:function (element, valueAccessor) {
             var value = ko.utils.unwrapObservable(valueAccessor());
             element.style.display = value ? "inline" : "none";
         }
     };
-
 
     //represent a single todo item
     var Todo = function (content, done) {
@@ -47,16 +57,19 @@
 
     //can place methods on prototype, as there can be many todos
     ko.utils.extend(Todo.prototype, {
-        edit: function() {  this.editing(true); },
-        stopEditing: function() { this.editing(false); }
+        edit:function () {
+            this.editing(true);
+        },
+        stopEditing:function () {
+            this.editing(false);
+        }
     });
 
-
     //our main view model
-    var ViewModel = function(todos) {
+    var ViewModel = function (todos) {
         var self = this;
         //map array of passed in todos to an observableArray of Todo objects
-        self.todos = ko.observableArray(ko.utils.arrayMap(todos, function(todo) {
+        self.todos = ko.observableArray(ko.utils.arrayMap(todos, function (todo) {
             return new Todo(todo.content, todo.done);
         }));
 
@@ -65,9 +78,12 @@
 
         //add a new todo, when enter key is pressed
         self.add = function (data, event) {
-            var newTodo = new Todo(self.current());
-            self.todos.push(newTodo);
-            self.current("");
+            var newTodo, current = (self.current() || "").replace(/^(\s|\u00A0)+|(\s|\u00A0)+$/g, "");
+            if (current) {
+                newTodo = new Todo(current);
+                self.todos.push(newTodo);
+                self.current("");
+            }
         };
 
         //remove a single todo
@@ -77,16 +93,17 @@
 
         //remove all completed todos
         self.removeCompleted = function () {
-            self.todos.remove(function(todo) {
+            self.todos.remove(function (todo) {
                 return todo.done();
             });
         };
 
         //count of all completed todos
         self.completedCount = ko.computed(function () {
-            return ko.utils.arrayFilter(self.todos(), function(todo) {
-                return todo.done();
-            }).length;
+            return ko.utils.arrayFilter(self.todos(),
+                function (todo) {
+                    return todo.done();
+                }).length;
         });
 
         //count of todos that are not complete
@@ -97,12 +114,12 @@
         //writeable computed observable to handle marking all complete/incomplete
         self.allCompleted = ko.computed({
             //always return true/false based on the done flag of all todos
-            read: function() {
+            read:function () {
                 return !self.remainingCount();
             },
             //set all todos to the written value (true/false)
-            write: function(newValue) {
-                ko.utils.arrayForEach(self.todos(), function(todo) {
+            write:function (newValue) {
+                ko.utils.arrayForEach(self.todos(), function (todo) {
                     //set even if value is the same, as subscribers are not notified in that case
                     todo.done(newValue);
                 });
@@ -110,18 +127,19 @@
         });
 
         //helper function to keep expressions out of markup
-        self.getLabel = function(count) {
+        self.getLabel = function (count) {
             return ko.utils.unwrapObservable(count) === 1 ? "item" : "items";
         };
 
         //internal computed observable that fires whenever anything changes in our todos
-        ko.computed(function() {
-            //get a clean copy of the todos, which also creates a dependency on the observableArray and all observables in each item
-            var todos = ko.toJS(self.todos);
+        ko.computed(
+            function () {
+                //get a clean copy of the todos, which also creates a dependency on the observableArray and all observables in each item
+                var todos = ko.toJS(self.todos);
 
-            //store to local storage
-            amplify.store("todos-knockout", todos);
-        }).extend({ throttle: 1000 }); //save at most once per second
+                //store to local storage
+                amplify.store("todos-knockout", todos);
+            }).extend({ throttle:1000 }); //save at most once per second
     };
 
     //check local storage for todos
