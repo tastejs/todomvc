53d5a33567f359b6496e8e678109d28e19731b8b
Ryan Niemeyer <rniemeyer@gmail.com>
2012-02-26 22:03:45 -0600

    apply focus() asynchronously to prevent issues with element not being visible yet.

--- architecture-examples/knockoutjs/js/app.js
+++ architecture-examples/knockoutjs/js/app.js
@@ -28,14 +28,20 @@
         }
     };
 
-    //select text when element is focused
-    ko.bindingHandlers.select = {
-        init:function (element) {
-            var handler = function () {
+    //wrapper to hasfocus that also selects text and applies focus async
+    ko.bindingHandlers.selectAndFocus = {
+        init:function (element, valueAccessor, allBindingsAccessor) {
+            ko.bindingHandlers.hasfocus.init(element, valueAccessor, allBindingsAccessor);
+            ko.utils.registerEventHandler(element, "focus", function () {
                 element.select();
-            };
-
-            ko.utils.registerEventHandler(element, "focus", handler);
+            });
+        },
+        update:function (element, valueAccessor) {
+            ko.utils.unwrapObservable(valueAccessor()); //for dependency
+            //ensure that element is visible before trying to focus
+            setTimeout(function () {
+                ko.bindingHandlers.hasfocus.update(element, valueAccessor);
+            }, 0);
         }
     };
 
