02c7ab142b9fccb82176d132386e86d7f42e4bfe
Christoph Burgdorf <christoph.burgdorf@bvsn.org>
2012-03-01 09:22:19 +0100

    Select text on focus (unfortunatly I had to take jquery-1.7.1.min.js as a new reference)

--- architecture-examples/angularjs/js/directive.js
+++ architecture-examples/angularjs/js/directive.js
@@ -22,10 +22,12 @@ angular.directive('my:dblclick', function(expression, compiledElement) {
 });
 
 angular.directive("my:focus", function(expression, compiledElement){
+
   return function(element){
     this.$watch(expression, function(){
       if(angular.formatter.boolean.parse(expression)){
         element[0].focus();
+        $(element[0]).select();
       } 
     }, element);
   };
