81f139e88b1a1d362c597fb997e23571cd8ec22c
Stas SUȘCOV <stas@net.utcluj.ro>
2012-03-09 18:19:23 +0200

    Updated docs, fixed observer.

--- dependency-examples/emberjs_require/js/app/models/todo.js
+++ dependency-examples/emberjs_require/js/app/models/todo.js
@@ -1,15 +1,18 @@
-/**
- * Todo entry model
- */
-define('app/models/todo', [
-    'ember'
-    ], function(Ember){
-    return Em.Object.extend({
+define('app/models/todo', ['ember'],
+  /**
+   * Todo entry model
+   *
+   * @returns Class
+   */
+  function(){
+    return Ember.Object.extend({
       id: null,
       title: null,
       isDone: false,
+      // Observer that will react on item change and will update the storage
       todoChanged: function() {
-        Em.Logger.log('Todo changed:', this);
+        Todos.Models.Store.update(this);
       }.observes('title', 'isDone')
     });
-});
+  }
+);
