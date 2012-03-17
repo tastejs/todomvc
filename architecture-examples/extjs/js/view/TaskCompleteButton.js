4f9d077688bae1fb691c78724fb1eb578a032f20
Aaron Boushley <boushley@gmail.com>
2012-03-17 15:49:22 -0700

    Fix focus outlline. Switch creation method for xtemplate.

--- architecture-examples/extjs/js/view/TaskCompleteButton.js
+++ architecture-examples/extjs/js/view/TaskCompleteButton.js
@@ -1,7 +1,7 @@
 Ext.define('Todo.view.TaskCompleteButton' , {
 	extend: 'Ext.Button',
 	alias: 'widget.completeButton',
-	tpl: new Ext.XTemplate('<a id="clear-completed">{text}</a>', {compiled: true}),
+	tpl: Ext.create('Ext.XTemplate', '<a id="clear-completed">{text}</a>', {compiled: true}),
 	setText: function (text) {
 		this.update({text:text});
 	}
