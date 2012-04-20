c800c4a1ca208b52a065469334acca9fb4af0cc0
Sindre Sorhus <sindresorhus@gmail.com>
2012-04-20 15:01:15 +0200

    Fix popover bug and improve it with hoverIntent

--- site/js/main.js
+++ site/js/main.js
@@ -1,13 +1,22 @@
+/*global $ */
 (function() {
+	'use strict';
 
 	// Demos popover
-	$('#demos li a').popover({
-		delay: 200,
-		placement: 'in right',
-		title: function() {
-			return $( this ).text() + '<a href="' + $( this ).data('source') + '">Go to site</a>';
-		}
-	}).on('click', '.popover', function( e ) {
+	function hover() {
+		$( this ).popover('toggle');
+	}
+
+	$('#demos li a').each(function() {
+		var $this = $( this );
+		$this.popover({
+			placement: 'in right',
+			title: $this.text() + '<a href="' + $this.data('source') + '">Go to site</a>'
+		});
+	})
+	.off()
+	.hoverIntent( hover, hover )
+	.on('click', '.popover', function( e ) {
 		// Prevent click on the popover, but allow links inside
 		if ( e.target.nodeName.toLowerCase() !== 'a' ) {
 			e.preventDefault();
