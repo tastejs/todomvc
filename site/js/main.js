59d7860615701184c980e2e2cdc132f03230d7fc
Sindre Sorhus <sindresorhus@gmail.com>
2012-04-20 10:03:07 +0200

    Update the labs contributor list + add some custom labs styles

--- site/js/main.js
+++ site/js/main.js
@@ -14,28 +14,4 @@
 		}
 	});
 
-	// Contributor list
-	$.ajaxSetup({
-		cache: true
-	});
-	$.getJSON('https://github.com/api/v2/json/repos/show/addyosmani/todomvc/contributors?callback=?', function( res ) {
-		var data = res.contributors;
-		// Add some previous contributors not on the GitHub list
-		[].push.apply( data, [{
-			login: 'tomdale',
-			name: 'Tom Dale'
-		}, {
-			login: 'justinbmeyer',
-			name: 'Justin Meyer'
-		}]);
-		var ret = $.map( data, function( elem ) {
-			var username = elem.login;
-			if ( $.inArray( username, [ 'addyosmani', 'boushley', 'sindresorhus' ] ) >= 0 ) {
-				return;
-			}
-			return '<a href="https://github.com/' + username + '">' + ( elem.name || username ) + '</a>';
-		});
-		$('#contributor-list').show().children('span').html( ret.join(', ') );
-	});
-
 })();
