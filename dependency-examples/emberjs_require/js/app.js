0cbd1e3f4ab8894e45af312901f468beb8ea57dc
Stas SUȘCOV <stas@net.utcluj.ro>
2012-04-23 03:07:01 +0300

    Add chai and mocha to load when `location.hash` matches 'specs'.

--- dependency-examples/emberjs_require/js/app.js
+++ dependency-examples/emberjs_require/js/app.js
@@ -4,7 +4,9 @@ require.config({
   paths: {
     jquery: '../../../assets/jquery.min',
     ember: 'libs/ember-0.9.7.1.min',
-    text: 'libs/require/text'
+    text: 'libs/require/text',
+    mocha: 'libs/mocha',
+    chai: 'libs/chai'
   }
 });
 
@@ -16,7 +18,33 @@ define('app', [
   'ember',
   ], function($, Store, TodosController) {
     var App = Ember.Application.create({
+
       VERSION: '0.2-omfg',
+
+      // Sets up mocha to run some integration tests
+      specsRunner: function(chai) {
+        // Create placeholder for mocha output
+        // TODO: Make this shit look better and inside body
+        $('body').before('<div id="mocha"></div>');
+
+        // Setup mocha and expose chai matchers
+        window.expect = chai.expect;
+        mocha.setup('bdd');
+
+        // Load testsuite
+        require([
+          'app/specs/models/store',
+          'app/specs/views/basic_acceptance',
+          'app/specs/controllers/todos'
+        ], function() {
+          mocha.run().globals([
+            '$', 'Ember', 'Todos'
+          ]);
+        });
+
+      },
+
+      // Constructor
       init: function() {
         this._super();
 
@@ -28,21 +56,14 @@ define('app', [
           })
         );
 
-        if (typeof(mocha) !== 'undefined') {
-          window.expect = chai.expect;
-          mocha.setup('bdd');
-          require([
-            'app/specs/models/store',
-            'app/specs/views/basic_acceptance',
-            'app/specs/controllers/main'
-          ], function(){
-            mocha
-              .run()
-              .globals(['$', 'Ember', 'Todos'])
-          });
+        // Run specs if asked
+        if ( location.hash.match(/specs/) ) {
+          require(['chai', 'mocha'], this.specsRunner);
         }
       }
     });
+
+    // Expose the application globally
     return window.Todos = App;
   }
 );
