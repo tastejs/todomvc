9c72b9ddf67d11debb6aec18ecc3195a5ff80e0a
Marcus Krejpowicz <krebbl@gmail.com>
2012-05-02 19:20:21 +0200

    - added packaged version of framework and dependencies - cleaned up code - fixed issues

--- architecture-examples/rappidjs/js/core/Content.js
+++
@@ -1,7 +0,0 @@
-requirejs(["rAppid"], function (rAppid) {
-    rAppid.defineClass("js.core.Content", ["js.core.Component"], function (Component) {
-        return Component.inherit(({
-            // TODO
-        }));
-    });
-});
--- architecture-examples/rappidjs/js/core/Imports.js
+++
@@ -1,6 +0,0 @@
-requirejs(["rAppid"], function (rAppid) {
-    rAppid.defineClass("js.core.Imports", ["js.core.Element"], function (Component) {
-        return Component.inherit({
-        });
-    });
-});
