9e536a3c32edf47cf7a1f7ed1302d1bcd7f3b43b
Rhsy <rhysbrettbowen@hotmail.com>
2012-05-10 20:56:38 +0200

    Close #163: PlastronJS app

---
+++ labs/architecture-examples/plastronjs/README.md
@@ -0,0 +1,46 @@
+# PlastronJS • [TodoMVC](http://todomvc.com)
+
+A todo app using [PlastronJS](https://github.com/rhysbrettbowen/PlastronJS)
+and [Closure Tools](https://developers.google.com/closure/)
+
+## Run
+
+Just open the index.html in your browser
+
+## Run uncompiled
+
+The app is built with [Plovr](http://plovr.com/). To run the unminified version you need to [download Plovr](http://plovr.com/download.html), create a directory called "build" and put the jar file in the build folder.
+
+You will then need to [download PlastronJS](https://github.com/rhysbrettbowen/PlastronJS) to js/lib (you will need to create the folder).
+
+Next open a command prompt in the base directory of the plastronjs todomvc example and type in.
+
+```
+java -jar build/plovr.jar serve plovr.json
+```
+
+Change the script src at the bottom of the index.html from js/compiled.js to:
+
+```
+http://localhost:9810/compile?id=todomvc&mode=raw
+```
+
+You can now view the uncompiled example and play around with it!
+
+# Compilation
+
+Once you have done the steps above you can compile any changes you make by running the below command:
+
+```
+java -jar build/plovr.jar build plovr.json
+```
+
+and view the compiled version fo the page by changing the bottom script src back to js/compiled.
+
+## Need help?
+
+shoot me a quick message on [twitter](https://twitter.com/#!/RhysBB)
+
+## Credit
+
+Created by [Rhys Brett-Bowen](http://rhysbrettbowen.com)
---
+++ labs/architecture-examples/plastronjs/js/controllers/todocontrol.js
@@ -0,0 +1,90 @@
+goog.provide('todomvc.todocontrol');
+
+goog.require('goog.dom');
+goog.require('goog.events.KeyCodes');
+goog.require('mvc.Control');
+goog.require('todomvc.templates');
+
+
+/**
+ * this is the control for a todo item.
+ *
+ * @constructor
+ * @param {mvc.Model} model for the control.
+ * @extends {mvc.Control}
+ */
+todomvc.todocontrol = function(model) {
+  goog.base(this, model);
+
+};
+goog.inherits(todomvc.todocontrol, mvc.Control);
+
+
+
+/**
+ * overrides goog.ui.Component#createDom with the todo template.
+ *
+ * @inheritDoc
+ */
+todomvc.todocontrol.prototype.createDom = function() {
+  var el = soy.renderAsFragment(todomvc.templates.todoItem, {
+    model: this.getModel().toJson()
+  }, null);
+ this.setElementInternal(/** @type {Element} */(el));
+};
+
+
+/**
+ * setup for event listeners.
+ *
+ * @inheritDoc
+ */
+todomvc.todocontrol.prototype.enterDocument = function() {
+
+  var model = this.getModel();
+
+  // toggle complete
+  this.click(function(e) {
+      model.set('completed', e.target.checked);
+  }, 'toggle');
+
+  // delete the model
+  this.click(function(e) {
+    model.dispose();
+  }, 'destroy');
+
+  // dblclick to edit
+  this.on(goog.events.EventType.DBLCLICK, function(e) {
+    this.makeEditable(true);
+  }, 'view');
+
+  // save on edit
+  var inputEl = this.getEls('.edit')[0];
+  this.on(goog.events.EventType.KEYUP, function(e) {
+    if (e.keyCode === goog.events.KeyCodes.ENTER) {
+      model.set('title', inputEl.value);
+    }
+  }, 'edit');
+
+  this.on(goog.events.EventType.BLUR, function(e) {
+    model.set('title', inputEl.value);
+  }, 'edit');
+};
+
+
+/**
+ * make (un)editable
+ *
+ * @param {boolean} editable whether to make editable.
+ */
+todomvc.todocontrol.prototype.makeEditable = function(editable) {
+
+  var inputEl = this.getEls('.edit')[0];
+
+  goog.dom.classes.enable(this.getElement(), 'editing', editable);
+  if (editable) {
+    inputEl.value = this.getModel().get('title');
+    inputEl.select();
+  }
+
+};
