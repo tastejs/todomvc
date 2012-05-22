4e5a65bb10b90bce63d8e7e5d8867e548f1c831d
James Thomas <james.thomas@uk.ibm.com>
2012-05-22 19:59:17 +0100

    Removing unneeded files and trimming CSS.

--- architecture-examples/dojo/js/lib/dojo/cldr/nls/fr-ch/gregorian.js
+++
@@ -1,9 +0,0 @@
-define(
-//begin v1.x content
-{
-	"timeFormat-full": "HH.mm:ss 'h' zzzz",
-	"dateFormat-full": "EEEE, d MMMM y",
-	"dateFormat-short": "dd.MM.yy"
-}
-//end v1.x content
-);
--- architecture-examples/dojo/js/lib/dojo/cldr/nls/pt-pt/number.js
+++
@@ -1,8 +0,0 @@
-define(
-//begin v1.x content
-{
-	"currencyFormat": "#,##0.00 ¤",
-	"group": " "
-}
-//end v1.x content
-);
