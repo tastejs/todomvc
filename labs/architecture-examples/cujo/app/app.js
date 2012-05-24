e8e8dcba17aaf71da26de7282ae22424328d9276
Brian Cavalier <brian@hovercraftstudios.com>
2012-05-24 15:18:24 -0400

    Fix paths after dir reorg

--- labs/architecture-examples/cujo/app/app.js
+++ labs/architecture-examples/cujo/app/app.js
@@ -1,15 +1,16 @@
 (function( curl ) {
 
 	var config = {
+		baseUrl: 'app',
 		paths: {
-			curl: 'lib/curl/src/curl'
+			curl: '../lib/curl/src/curl'
 		},
 		pluginPath: 'curl/plugin',
 		packages: [
-			{ name: 'wire', location: 'lib/wire', main: 'wire' },
-			{ name: 'when', location: 'lib/when', main: 'when' },
-			{ name: 'aop', location: 'lib/aop', main: 'aop' },
-			{ name: 'cola', location: 'lib/cola' }
+			{ name: 'wire', location: '../lib/wire', main: 'wire' },
+			{ name: 'when', location: '../lib/when', main: 'when' },
+			{ name: 'aop', location: '../lib/aop', main: 'aop' },
+			{ name: 'cola', location: '../lib/cola' }
 		]
 	};
 
