7b72277dd389d0f1fd57f208372ea5d42af6f01a
Brian Cavalier <brian@hovercraftstudios.com>
2012-05-24 16:05:25 -0400

    Internationalized. Extracted strings to i18n files, added placeholders to templates

---
+++ labs/architecture-examples/cujo/app/controls/strings.js
@@ -0,0 +1,13 @@
+define({
+	// TODO: Deal with singular vs. plural
+	itemsLeft: {
+		singular: 'item left',
+		plural: 'items left'
+	},
+	filter: {
+		all: 'All',
+		active: 'Active',
+		completed: 'Completed'
+	},
+	clearCompleted: 'Clear completed'
+});
