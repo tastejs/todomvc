352327f7572174c646f44b9c7689957c7ede2d8f
Aaron Boushley <boushley@gmail.com>
2012-05-27 16:51:24 +0200

    Merge ExtJS branch into master

--- architecture-examples/extjs/js/view/TaskToolbar.js
+++ architecture-examples/extjs/js/view/TaskToolbar.js
@@ -1,11 +1,12 @@
 Ext.define('Todo.view.TaskToolbar' , {
-    hidden: true,
-    extend: 'Ext.Toolbar',
-    alias : 'widget.taskToolbar',
-    items: [{
-        xtype: 'button',
-        hidden: true
-    }, {
-        xtype: 'container'
-    }]
+        hidden: true,
+        extend: 'Ext.Toolbar',
+        alias : 'widget.taskToolbar',
+        autoEl: {tag: 'footer'},
+        requires: ['Todo.view.TaskCompleteButton'],
+        items: [{
+                        xtype: 'completeButton'
+        }, {
+                xtype: 'container'
+        }]
 });
