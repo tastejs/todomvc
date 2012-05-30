56e25fe45099a8c7b850688e9d34a478ef44168a
Mikael Karon <mikael@karon.se>
2012-05-30 22:13:16 +0800

    add 'left' to end of counter

--- architecture-examples/troopjs/js/widget/count.js
+++ architecture-examples/troopjs/js/widget/count.js
@@ -8,7 +8,7 @@ define( [ "troopjs-core/component/widget", "jquery" ], function CountModule(Widg
 		"hub:memory/todos/change" : function onChange(topic, items) {
 			var count = $.grep(items, filter, true).length;
 
-			this.$element.html("<strong>" + count + "</strong>" + (count === 1 ? " item" : " items"));
+			this.$element.html("<strong>" + count + "</strong> " + (count === 1 ? "item" : "items") + " left");
 		}
 	});
 });
