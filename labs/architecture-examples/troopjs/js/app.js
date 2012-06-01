37a4b5e9599e1f9c293d9499219f7b134d428722
Mikael Karon <mikael@karon.se>
2012-06-01 16:05:48 +0800

    Bit the bullet. RequireJS 2.0 compatible application.
    Remover included RequireJS (load from assets).

--- labs/architecture-examples/troopjs/js/app.js
+++ labs/architecture-examples/troopjs/js/app.js
@@ -1,12 +1,21 @@
 require({
-	"baseUrl" : "js",
 	"paths" : {
 		"jquery" : "../../../../assets/jquery.min",
-		"troopjs-bundle" : "lib/troopjs-bundle"
+		"troopjs-bundle" : "lib/troopjs-bundle.min"
 	},
-	"priority": [ "jquery", "config", "troopjs-bundle" ]
-}, [ "jquery", "widget/application" ], function App(jQuery, Application) {
-	jQuery(document).ready(function ready($) {
-		Application($(this.body), "app/todos").start();
+}, [ "require", "jquery", "troopjs-bundle" ], function Deps(parentRequire, jQuery) {
+
+	// Application and plug-ins
+	parentRequire([
+		"widget/application",
+		"troopjs-jquery/weave",
+		"troopjs-jquery/destroy",
+		"troopjs-jquery/hashchange",
+		"troopjs-jquery/action" ], function App(Application) {
+
+		// Hook ready
+		jQuery(document).ready(function ready($) {
+			Application($(this.body), "app/todos").start();
+		});
 	});
 });
