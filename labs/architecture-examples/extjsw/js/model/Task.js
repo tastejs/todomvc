0ce80bbff657cd82d49853275b6d71f8b77acf9a
ettavolt <ettavolt@gmail.com>
2012-06-06 20:57:13 +0300

    Some deletes.

--- labs/architecture-examples/extjsw/js/model/Task.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Todo.model.Task', {
-    extend:'Ext.data.Model',
-    requires:['Ext.data.UuidGenerator'],
-    fields:['id', 'title', {name:'completed', type:'boolean'}],
-    proxy:{
-        type:'localstorage',
-        id:'todos-extjs'
-    }
-});
