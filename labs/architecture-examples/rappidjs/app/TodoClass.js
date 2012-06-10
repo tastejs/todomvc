c3d15a9beb3139667e659ee0dde5d1bc8a36d741
Marcus Krejpowicz <krebbl@gmail.com>
2012-06-10 20:35:12 +0200

    Revert "added LocalStorage DataSource Persistence and did some bug fixes"
    This reverts commit 6d9efa836dd5ceb4d60ae71f83cd7ee2961c60b3.

--- labs/architecture-examples/rappidjs/app/TodoClass.js
+++ labs/architecture-examples/rappidjs/app/TodoClass.js
@@ -1,17 +1,33 @@
-define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection/TodoList", "js/data/FilterDataView", "js/data/LocalStorageDataSource"],
-    function (Application, I18n, Todo, TodoList, FilterDataView, DataSource) {
+define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection/TodoList", "js/data/ListView"],
+    function (Application, I18n, Todo, TodoList, ListView) {
 
         var ENTER_KEY = 13;
 
         return Application.inherit("app.TodoClass", {
+            inject: {
+                i18n: I18n
+            },
             /**
              * Initializes the app
              * In this method we set the initial models
              */
             initialize: function () {
-                this.set("todoList", null);
-                this.set("filterList", null);
-                this.callBase();
+
+                this.set("todoList", new TodoList());
+                this.set("filterList", new ListView({
+                    list: this.get("todoList"),
+                    filter: 'all',
+                    filterFnc: function (item) {
+                        var filter = this.$.filter;
+                        if (filter == "active") {
+                            return !item.isCompleted();
+                        } else if (filter == "completed") {
+                            return item.isCompleted();
+                        } else {
+                            return true;
+                        }
+                    }}));
+                this.set("newTodo", new Todo());
             },
             /**
              * Are triggered
@@ -25,70 +41,41 @@ define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection
             showCompleted: function () {
                 this.$.filterList.set("filter", "completed");
             },
+            isStringEqual: function (route, filter) {
+                return route == filter;
+            },
             /**
              * The rest is just controller stuff
              */
-            addNewTodo: function (e, input) {
+            addNewTodo: function (e) {
                 if (e.$.keyCode === ENTER_KEY) {
-                    var title = input.get("value").trim();
-                    if (title) {
-                        var newTodo = this.$.dataSource.createEntity(Todo);
-                        newTodo.set({title: title, completed: false});
+                    var tmp = this.get("newTodo");
+                    if (tmp.hasTitle()) {
+                        var newTodo = new Todo({title: tmp.get("title")});
+                        newTodo.setCompleted(false);
                         this.get("todoList").add(newTodo);
-
-                        // save the new item
-                        newTodo.save();
-
-                        input.set('value','');
+                        tmp.set("title", "");
                     }
                 }
             },
             markAllComplete: function (e, input) {
-                this.get("todoList").markAll(input.$el.checked);
+                this.get("todoList").markAll(input.get("checked"));
             },
             clearCompleted: function () {
                 this.get("todoList").clearCompleted();
             },
             removeTodo: function (e) {
-                var todo = e.$, self = this;
-                todo.remove(null, function(err){
-                    if(!err){
-                        self.get("todoList").remove(todo);
-                    }
-                });
+                this.get("todoList").remove(e.$);
             },
             /**
              * Start the application and render it to the body ...
              */
             start: function (parameter, callback) {
-                this.set('todoList', this.$.dataSource.createCollection(TodoList));
-
-                // fetch all todos, can be done sync because we use localStorage
-                this.$.todoList.fetch();
-
-                this.set('filterList', new FilterDataView({
-                    baseList: this.get("todoList"),
-                    filter: 'all',
-                    filterFnc: function (item) {
-                        var filter = this.$.filter;
-                        if (filter == "active") {
-                            return !item.isCompleted();
-                        } else if (filter == "completed") {
-                            return item.isCompleted();
-                        } else {
-                            return true;
-                        }
-                    }}));
-
                 // false - disables autostart
                 this.callBase(parameter, false);
 
-                // load locale and start by calling callback
+                this.$.i18n.set("locale", "en_EN", {silent: true});
                 this.$.i18n.loadLocale("en_EN", callback);
-            },
-            // compares 2 strings
-            isStringEqual: function (str1, str2) {
-                return str1 == str2;
             }
         });
     });
