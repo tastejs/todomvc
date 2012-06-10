5c90b910b99520b2d67d79fba5e5e23f057c07b6
Marcus Krejpowicz <krebbl@gmail.com>
2012-06-10 19:21:38 +0200

    Added localStorage, persistency and fixed some bugs …

--- labs/architecture-examples/rappidjs/app/TodoClass.js
+++ labs/architecture-examples/rappidjs/app/TodoClass.js
@@ -1,47 +1,17 @@
-<<<<<<< HEAD
 define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection/TodoList", "js/data/FilterDataView", "js/data/LocalStorageDataSource"],
     function (Application, I18n, Todo, TodoList, FilterDataView, DataSource) {
-=======
-define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection/TodoList", "js/data/ListView"],
-    function (Application, I18n, Todo, TodoList, ListView) {
->>>>>>> ff2959b913b2a0489bcd4d67eb2c021582beaa36
 
         var ENTER_KEY = 13;
 
         return Application.inherit("app.TodoClass", {
-<<<<<<< HEAD
-=======
-            inject: {
-                i18n: I18n
-            },
->>>>>>> ff2959b913b2a0489bcd4d67eb2c021582beaa36
             /**
              * Initializes the app
              * In this method we set the initial models
              */
             initialize: function () {
-<<<<<<< HEAD
                 this.set("todoList", null);
                 this.set("filterList", null);
                 this.callBase();
-=======
-
-                this.set("todoList", new TodoList());
-                this.set("filterList", new ListView({
-                    list: this.get("todoList"),
-                    filter: 'all',
-                    filterFnc: function (item) {
-                        var filter = this.$.filter;
-                        if (filter == "active") {
-                            return !item.isCompleted();
-                        } else if (filter == "completed") {
-                            return item.isCompleted();
-                        } else {
-                            return true;
-                        }
-                    }}));
-                this.set("newTodo", new Todo());
->>>>>>> ff2959b913b2a0489bcd4d67eb2c021582beaa36
             },
             /**
              * Are triggered
@@ -55,7 +25,6 @@ define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection
             showCompleted: function () {
                 this.$.filterList.set("filter", "completed");
             },
-<<<<<<< HEAD
             /**
              * The rest is just controller stuff
              */
@@ -71,52 +40,27 @@ define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection
                         newTodo.save();
 
                         input.set('value','');
-=======
-            isStringEqual: function (route, filter) {
-                return route == filter;
-            },
-            /**
-             * The rest is just controller stuff
-             */
-            addNewTodo: function (e) {
-                if (e.$.keyCode === ENTER_KEY) {
-                    var tmp = this.get("newTodo");
-                    if (tmp.hasTitle()) {
-                        var newTodo = new Todo({title: tmp.get("title")});
-                        newTodo.setCompleted(false);
-                        this.get("todoList").add(newTodo);
-                        tmp.set("title", "");
->>>>>>> ff2959b913b2a0489bcd4d67eb2c021582beaa36
                     }
                 }
             },
             markAllComplete: function (e, input) {
-<<<<<<< HEAD
                 this.get("todoList").markAll(input.$el.checked);
-=======
-                this.get("todoList").markAll(input.get("checked"));
->>>>>>> ff2959b913b2a0489bcd4d67eb2c021582beaa36
             },
             clearCompleted: function () {
                 this.get("todoList").clearCompleted();
             },
             removeTodo: function (e) {
-<<<<<<< HEAD
                 var todo = e.$, self = this;
                 todo.remove(null, function(err){
                     if(!err){
                         self.get("todoList").remove(todo);
                     }
                 });
-=======
-                this.get("todoList").remove(e.$);
->>>>>>> ff2959b913b2a0489bcd4d67eb2c021582beaa36
             },
             /**
              * Start the application and render it to the body ...
              */
             start: function (parameter, callback) {
-<<<<<<< HEAD
                 this.set('todoList', this.$.dataSource.createCollection(TodoList));
 
                 // fetch all todos, can be done sync because we use localStorage
@@ -145,13 +89,6 @@ define(["js/core/Application", "js/core/I18n", "app/model/Todo", "app/collection
             // compares 2 strings
             isStringEqual: function (str1, str2) {
                 return str1 == str2;
-=======
-                // false - disables autostart
-                this.callBase(parameter, false);
-
-                this.$.i18n.set("locale", "en_EN", {silent: true});
-                this.$.i18n.loadLocale("en_EN", callback);
->>>>>>> ff2959b913b2a0489bcd4d67eb2c021582beaa36
             }
         });
     });
--- labs/architecture-examples/rappidjs/app/model/Todo.js
+++ labs/architecture-examples/rappidjs/app/model/Todo.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 define(["js/data/Model"], function (Model) {
     return Model.inherit("app.model.Todo", {
-=======
-define(["js/data/Model"], function (Bindable) {
-    return Bindable.inherit("app.model.Todo", {
->>>>>>> ff2959b913b2a0489bcd4d67eb2c021582beaa36
         defaults: {
             title: "",
             completed: false
