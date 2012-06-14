b5788982766be51cd5c100c79249efd7e4a47d1d
John Hann <john@e-numera.com>
2012-06-14 12:12:42 -0400

    changed cleanInput to cleanTodo

--- labs/architecture-examples/cujo/app/create/cleanTodo.js
+++ labs/architecture-examples/cujo/app/create/cleanTodo.js
@@ -2,7 +2,7 @@ define(function() {
 
 	return function(todo) {
 		todo.text = todo.text && todo.text.trim() || '';
-		todo.complete = false;
+		todo.complete = !!todo.complete;
 
 		return todo;
 	}
