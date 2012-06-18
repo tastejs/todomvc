ce1e4cb02f2a49201c56facc5fe937325d2de8ed
Igor Minar <iiminar@gmail.com>
2012-06-17 23:15:45 -0700

    AngularJS: unifying indentation and ws
    per https://github.com/rwldrn/idiomatic.js/#idiomatic-style-manifesto
    
    - Never mix spaces and tabs.
    - ...  choose between soft indents (spaces) or real tabs, consider this law.
    
    I went with spaces everywhere.

--- architecture-examples/angularjs/js/controllers/todo.js
+++ architecture-examples/angularjs/js/controllers/todo.js
@@ -3,101 +3,101 @@
 var todomvc = angular.module('todomvc', []);
 
 function TodoController($scope, $location) {
-	$scope.todos = retrieveStore();
-	$scope.newTodo = "";
-	if($location.path()=='') $location.path('/');
-	$scope.location = $location;
+  $scope.todos = retrieveStore();
+  $scope.newTodo = "";
+  if($location.path()=='') $location.path('/');
+  $scope.location = $location;
 
-	// Call updateStore() whenever the todos array changes.
-	$scope.$watch('todos', updateStore, true);
+  // Call updateStore() whenever the todos array changes.
+  $scope.$watch('todos', updateStore, true);
 
-	$scope.$watch(function() {return $location.path(); }, function(path) { 
-		$scope.statusFilter = path == '/active' ? 
-			{ completed: false } : path == '/completed' ?
-				{ completed: true } : null;
-	});
+  $scope.$watch(function() {return $location.path(); }, function(path) {
+    $scope.statusFilter = path == '/active' ?
+      { completed: false } : path == '/completed' ?
+        { completed: true } : null;
+  });
 
-	$scope.todoForms = {
-		one: 'item left',
-		other: 'items left'
-	};
+  $scope.todoForms = {
+    one: 'item left',
+    other: 'items left'
+  };
 
-	function retrieveStore() {
-		var store = localStorage.getItem('todos-angularjs');
-		return (store && JSON.parse(store)) || [];
-	};
+  function retrieveStore() {
+    var store = localStorage.getItem('todos-angularjs');
+    return (store && JSON.parse(store)) || [];
+  };
 
-	function updateStore() {
-		var isEditing = $scope.todos.filter(function(val) {
-			return val.editing;
-		}).length;
+  function updateStore() {
+    var isEditing = $scope.todos.filter(function(val) {
+      return val.editing;
+    }).length;
 
-		if (!isEditing) {
-			localStorage.setItem('todos-angularjs', JSON.stringify($scope.todos));
-		}
-	};
+    if (!isEditing) {
+      localStorage.setItem('todos-angularjs', JSON.stringify($scope.todos));
+    }
+  };
 
-	$scope.addTodo = function() {
-		if (this.newTodo.trim().length === 0) {
-			return;
-		}
+  $scope.addTodo = function() {
+    if (this.newTodo.trim().length === 0) {
+      return;
+    }
 
-		$scope.todos.push({
-			title: this.newTodo,
-			completed: false,
-			editing: false
-		});
+    $scope.todos.push({
+      title: this.newTodo,
+      completed: false,
+      editing: false
+    });
 
-		this.newTodo = '';
-	};
+    this.newTodo = '';
+  };
 
-	$scope.editTodo = function(todo) {
-		//cancel any active editing operation
-		$scope.todos.forEach(function(val) {
-			val.editing = false;
-		});
-		todo.editing = true;
-	};
+  $scope.editTodo = function(todo) {
+    //cancel any active editing operation
+    $scope.todos.forEach(function(val) {
+      val.editing = false;
+    });
+    todo.editing = true;
+  };
 
-	$scope.finishEditing = function(todo) {
-		if (todo.title.trim().length === 0) {
-			$scope.removeTodo(todo);
-		} else {
-			todo.editing = false;
-		}
-	};
+  $scope.finishEditing = function(todo) {
+    if (todo.title.trim().length === 0) {
+      $scope.removeTodo(todo);
+    } else {
+      todo.editing = false;
+    }
+  };
 
-	$scope.removeTodo = function(todo) {
-		for (var i = 0, len = $scope.todos.length; i < len; ++i) {
-			if (todo === $scope.todos[i]) {
-				$scope.todos.splice(i, 1);
-			}
-		}
-	};
+  $scope.removeTodo = function(todo) {
+    for (var i = 0, len = $scope.todos.length; i < len; ++i) {
+      if (todo === $scope.todos[i]) {
+        $scope.todos.splice(i, 1);
+      }
+    }
+  };
 
-	$scope.remainingTodos = function() {
-		return $scope.todos.filter(function(val) {
-			return !val.completed;
-		});
-	};
+  $scope.remainingTodos = function() {
+    return $scope.todos.filter(function(val) {
+      return !val.completed;
+    });
+  };
 
-	$scope.completedTodos = function() {
-		return $scope.todos.filter(function(val) {
-			return val.completed;
-		});
-	}
+  $scope.completedTodos = function() {
+    return $scope.todos.filter(function(val) {
+      return val.completed;
+    });
+  }
 
-	$scope.clearDoneTodos = function() {
-		$scope.todos = $scope.remainingTodos();
-	};
+  $scope.clearDoneTodos = function() {
+    $scope.todos = $scope.remainingTodos();
+  };
 
-	$scope.markAllDone = function() {
-		var markDone = true;
-		if (!$scope.remainingTodos().length) {
-			markDone = false;
-		}
-		$scope.todos.forEach(function(todo) {
-			todo.completed = markDone;
-		});
-	};
+  $scope.markAllDone = function() {
+    var markDone = true;
+    if (!$scope.remainingTodos().length) {
+      markDone = false;
+    }
+    $scope.todos.forEach(function(todo) {
+      todo.completed = markDone;
+    });
+  };
 };
--- architecture-examples/angularjs/js/directives/todo-directives.js
+++ architecture-examples/angularjs/js/directives/todo-directives.js
@@ -1,20 +1,20 @@
 todomvc.directive('todoBlur', function() {
-        return function( scope, elem, attrs ) {
-                elem.bind('blur', function() {
-                        scope.$apply( attrs.todoBlur );
-                });
-        };
+  return function( scope, elem, attrs ) {
+    elem.bind('blur', function() {
+      scope.$apply( attrs.todoBlur );
+    });
+  };
 });
 
 todomvc.directive('todoFocus', function( $timeout ) {
-        return function( scope, elem, attrs ) {
-                scope.$watch( attrs.todoFocus, function( newval ) {
-                        if ( newval ) {
-                                $timeout(function() {
-                                        elem[0].focus();
-                                        elem[0].select();
-                                }, 0 );
-                        }
-                });
-        };
+  return function( scope, elem, attrs ) {
+    scope.$watch( attrs.todoFocus, function( newval ) {
+      if ( newval ) {
+        $timeout(function() {
+          elem[0].focus();
+          elem[0].select();
+        }, 0 );
+      }
+    });
+  };
 });
