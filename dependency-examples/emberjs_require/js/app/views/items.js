e20be98e04ce1129d20889f5910ca98b08d56523
Stas SUȘCOV <stas@net.utcluj.ro>
2012-06-20 15:29:08 +0300

    Rewrite the items view.
    Cleanup `Ember.CollectionView` template, on editing use a contextual view.
    Fix race condition issue with focusOut/insertNewline when editing last item.

--- dependency-examples/emberjs_require/js/app/views/items.js
+++ dependency-examples/emberjs_require/js/app/views/items.js
@@ -9,8 +9,44 @@ define('app/views/items', [
 	 * @returns Class
 	 */
 	function( items_html ) {
-		return Ember.View.extend({
-			template: Ember.Handlebars.compile( items_html )
+		return Ember.CollectionView.extend({
+			contentBinding: 'controller',
+			tagName: 'ul',
+			elementId: 'todo-list',
+			itemViewClass: Ember.View.extend({
+				template: Ember.Handlebars.compile( items_html ),
+				classNames: [ 'view' ],
+				classNameBindings: ['content.completed', 'content.editing'],
+				doubleClick: function() {
+					this.get( 'content' ).set( 'editing', true );
+				},
+				removeItem: function() {
+					this.get( 'controller' ).removeObject( this.get( 'content' ) );
+				},
+				ItemEditorView: Ember.TextField.extend({
+					valueBinding: 'content.title',
+					classNames: [ 'edit' ],
+					change: function() {
+						if ( Ember.empty( this.getPath( 'content.title' ) ) ) {
+							this.get( 'controller' ).removeObject( this.get( 'content' ) );
+						}
+					},
+					whenDone: function() {
+						this.get( 'content' ).set( 'editing', false );
+					},
+					focusOut: function() {
+						this.whenDone();
+					},
+					didInsertElement: function() {
+						this.$().focus();
+					},
+					insertNewline: function() {
+						this.whenDone();
+					}
+				})
+			}),
+
+
 		})
 	}
 );
