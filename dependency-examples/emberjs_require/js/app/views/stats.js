920eaf35a11cc2c11ec2df65e1e9322019245e5d
Stas SUȘCOV <stas@net.utcluj.ro>
2012-06-24 00:47:45 +0300

    Update stats view bindings and force view scope (might be an Ember bug).

--- dependency-examples/emberjs_require/js/app/views/stats.js
+++ dependency-examples/emberjs_require/js/app/views/stats.js
@@ -10,13 +10,13 @@ define('app/views/stats', [
 	 */
 	function( stats_html ) {
 		return Ember.View.extend({
+			entriesBinding: 'controller.namespace.entriesController',
 			elementId: 'todo-count',
 			tagName: 'span',
-			remainingBinding: 'controller.remaining',
 			template: Ember.Handlebars.compile( stats_html ),
 			oneLeft: function() {
-				return this.get( 'remaining' ) === 1;
-			}.observes( 'remaining' )
+				return this.getPath( 'entries.remaining' ) === 1;
+			}.property( 'entries.remaining' )
 		})
 	}
 );
