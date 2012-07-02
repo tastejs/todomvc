a053160db922ff5de71a240233088b33f0c96016
Sindre Sorhus <sindresorhus@gmail.com>
2012-07-02 10:49:44 +0200

    ExtJS: Tab indentation

--- labs/architecture-examples/extjs/js/view/TaskField.js
+++ labs/architecture-examples/extjs/js/view/TaskField.js
@@ -1,33 +1,33 @@
 Ext.define('Todo.view.TaskField' , {
 
-        enableKeyEvents: true,
+	enableKeyEvents: true,
 
-        alias : 'widget.taskField',
+	alias : 'widget.taskField',
 
-        extend: 'Ext.Component',
+	extend: 'Ext.Component',
 
-        emptyText: 'What needs to be done?',
+	emptyText: 'What needs to be done?',
 
-        afterRender: function() {
-                this.callParent(arguments);
-                this.field = this.el.first();
-                this.field.on('keyup', this.onKeyup, this);
-        },
+	afterRender: function() {
+		this.callParent(arguments);
+		this.field = this.el.first();
+		this.field.on('keyup', this.onKeyup, this);
+	},
 
-        onKeyup: function(event) {
-                this.fireEvent('keyup', this, event);
-        },
+	onKeyup: function(event) {
+		this.fireEvent('keyup', this, event);
+	},
 
-        getValue: function() {
-                return this.field.dom.value;
-        },
+	getValue: function() {
+		return this.field.dom.value;
+	},
 
-        setValue: function(value) {
-                this.field.dom.value = value;
-        },
+	setValue: function(value) {
+		this.field.dom.value = value;
+	},
 
-        reset: function() {
-                this.setValue('');
-        }
+	reset: function() {
+		this.setValue('');
+	}
 
 });
