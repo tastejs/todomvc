8eb617a5edfe38e37601337ec61b541eba8cf7df
John Hann <john@e-numera.com>
2012-07-06 14:48:06 -0400

    added poly.js

---
+++ labs/architecture-examples/cujo/lib/poly/poly.js
@@ -0,0 +1,21 @@
+/**
+ * polyfill / shim plugin for AMD loaders
+ *
+ * (c) copyright 2011-2012 Brian Cavalier and John Hann
+ *
+ * poly is part of the cujo.js family of libraries (http://cujojs.com/)
+ *
+ * Licensed under the MIT License at:
+ * 		http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+define(function () {
+
+	var poly = {};
+
+	poly.version = '0.4';
+
+    return poly;
+
+});
