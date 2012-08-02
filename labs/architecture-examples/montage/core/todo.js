29732c99e9c1ae8d5a09219271e0c5ab9eba2df8
Mike Czepiel <mczepiel@motorola.com>
2012-08-03 00:58:48 +0200

    Close GH-251: Montage.

---
+++ labs/architecture-examples/montage/core/todo.js
@@ -0,0 +1,20 @@
+var Montage = require("montage").Montage;
+
+exports.Todo = Montage.create(Montage, {
+
+    initWithTitle: {
+        value: function(title) {
+            this.title = title;
+            return this;
+        }
+    },
+
+    title: {
+        value: null
+    },
+
+    completed: {
+        value: false
+    }
+
+});
