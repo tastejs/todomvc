67be50caa27550b3d3b2b81740b09628c836ec78
Sébastien Chopin <atinux@gmail.com>
2012-08-03 12:38:17 +0200

    **First, thanks to do this awesome job with TodoMVC, It's great to see some projects like this :)**
    Here my modifications to respect the "best-pratices" in Backbone.js :
    
    **Model** - js/models/todo.js
    
     - remove `initialize` method in Model (useless because `defaults` do already the job)
     - remove `clear` method, we should not change `clear` behavior of Backbone.Model -> http://backbonejs.org/#Model-clear
    
    **Views** - js/views/app.js and js/views/todos.js
    
     - Call model.destroy() (model.clear(); is not logic)
     - Use this.$el instead of $(this.el) to improve readability and caching
    
    Correct this issue: https://github.com/addyosmani/todomvc/issues/223 with adding `window.app.Todos.on( 'change:completed', this.addAll, this );` and `add` event call now `addAll` method in *js/views/app.js*
    
    Issue related too: https://github.com/addyosmani/todomvc/issues/253

--- architecture-examples/backbone/js/models/todo.js
+++ architecture-examples/backbone/js/models/todo.js
@@ -8,18 +8,10 @@
 	window.app.Todo = Backbone.Model.extend({
 
 		// Default attributes for the todo.
+		// Ensure that each todo created has `title` and `completed` keys
 		defaults: {
 			title: '',
 			completed: false
-		},
-
-		// Ensure that each todo created has `title`.
-		initialize: function() {
-			if ( !this.get('title') ) {
-				this.set({
-					'title': this.defaults.title
-				});
-			}
 		},
 
 		// Toggle the `completed` state of this todo item.
@@ -27,11 +19,6 @@
 			this.save({
 				completed: !this.get('completed')
 			});
-		},
-
-		// Remove this Todo from *localStorage* and delete its view.
-		clear: function() {
-			this.destroy();
 		}
 
 	});
