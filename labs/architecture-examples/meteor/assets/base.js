831f059ba49e38cba530b1a4702146c070700d10
MStumpp <mstumpp@gmail.com>
2012-08-22 17:01:21 +0200

    some code formatting

--- labs/architecture-examples/meteor/assets/base.js
+++
@@ -1,9 +0,0 @@
-if (Meteor.is_client) {
-(function( window ) {
-    'use strict';
-
-    if ( location.hostname === 'todomvc.com' ) {
-        var _gaq=[['_setAccount','UA-31081062-1'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'));
-    }
-})( window );
-}
