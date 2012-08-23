ee5374511b6f03fece7d0f8e118da807e0229590
asudoh <asudoh@gmail.com>
2012-08-23 23:33:07 +0900

    Removed test files from dojo18 branch.

--- architecture-examples/dojo/js/todo/tests/dojoConfig.js
+++
@@ -1,7 +0,0 @@
-(function(require){
-	require.baseUrl = "../../dojo";
-	require.packages = (require.packages || []).concat([{
-		name: "todo",
-		location: "../todomvc/architecture-examples/dojo/js/todo"
-	}]);
-})(require);
--- architecture-examples/dojo/js/todo/tests/module.js
+++
@@ -1,8 +0,0 @@
-define([
-	"doh",
-	"./ctrl/TodoListRefController",
-	"./model/SimpleTodoModel"
-], function(doh){
-	var userArgs = window.location.search.replace(/[\?&](dojoUrl|testUrl|testModule)=[^&]*/g, "").replace(/^&/, "?");
-	doh.registerUrl("todo.tests.app18", require.toUrl("todo/tests/app18.html") + userArgs, 999999);
-});
