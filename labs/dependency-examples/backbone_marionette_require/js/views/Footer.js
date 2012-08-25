143d12515909b8e337a5a227e595a45a92163f45
Jarrod Overson <joverson@onerecovery.com>
2012-08-25 13:46:46 -0700

    forgot about styling filter anchors

--- labs/dependency-examples/backbone_marionette_require/js/views/Footer.js
+++ labs/dependency-examples/backbone_marionette_require/js/views/Footer.js
@@ -1,4 +1,4 @@
-/*global define*/
+/*global define, $*/
 
 define(['marionette','vent','templates','views/ActiveCount'], function (Marionette,vent,templates,ActiveCount) {
   "use strict";
@@ -8,11 +8,19 @@ define(['marionette','vent','templates','views/ActiveCount'], function (Marionet
     regions : {
       count : '#todo-count strong'
     },
+    ui : {
+      filters : '#filters a'
+    },
     events : {
-      'click #clear-completed' : 'clearCompletedClick'
+      'click #clear-completed' : 'clearCompletedClick',
+      'click #filters a' : 'onFilterClick'
     },
     onRender : function() {
       this.count.show(new ActiveCount({collection : this.collection}));
+    },
+    onFilterClick : function(evt) {
+      this.ui.filters.removeClass('selected');
+      $(evt.currentTarget).addClass('selected');
     },
     clearCompletedClick : function() {
       vent.trigger('todoList:clear:completed');
