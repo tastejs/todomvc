e6f66f54a02cc57f0df8dd27203ceef575c2c16f
Jarrod Overson <joverson@onerecovery.com>
2012-08-29 11:01:02 -0700

    clearing out dev config

--- labs/dependency-examples/backbone_marionette_require/app.build.js
+++ labs/dependency-examples/backbone_marionette_require/app.build.js
@@ -4,12 +4,8 @@
   baseUrl: "js/",
   mainConfigFile: 'js/main.js',
   out: "js/main.built.js",
+  include : 'main',
 
-  //The directory path to save the output. If not specified, then
-  //the path will default to be a directory called "build" as a sibling
-  //to the build file. All relative paths are relative to the build file.
-  //  dir: "../some/path",
-  locale: "en-us",
   optimize: "uglify",
 
   uglify: {
@@ -19,25 +15,19 @@
     max_line_length: 1000
   },
 
-  optimizeCss: "standard.keepLines",
-
   inlineText: true,
   useStrict: false,
 
-  // Breaks anything that defines a 'require'
-  //namespace: 'commit',
   skipPragmas: false,
   skipModuleInsertion: false,
   stubModules: ['text'],
   optimizeAllPluginResources: false,
   findNestedDependencies: false,
   removeCombined: false,
-  include : 'main',
 
   fileExclusionRegExp: /^\./,
 
   preserveLicenseComments: true,
 
   logLevel: 0
-
 })
