134825f037b272d98a078c73d1c42114b37e1fb5
Ryan Eastridge <ryan@eastridge.xxx>
2012-09-19 22:12:09 +0200

    add Backbone.history.start()

--- labs/architecture-examples/thorax/js/app.js
+++ labs/architecture-examples/thorax/js/app.js
@@ -2,7 +2,7 @@ var app = app || {};
 var ENTER_KEY = 13;
 
 $(function() {
-
+  Backbone.history.start();
 	// Kick things off by creating the **App**.
 	var view = new Thorax.Views['app']();
   $('body').append(view.el);
