eeaaa744ae6ab60ffdc8d0a58278c2554bcf1efa
Mathieu Lorber <mathieu.lorber@gmail.com>
2012-09-23 12:52:47 +0200

    Todo element class Delete todo feature

--- labs/architecture-examples/dart/todomvc/todomvc.dart
+++ labs/architecture-examples/dart/todomvc/todomvc.dart
@@ -10,22 +10,33 @@ void main() {
       todo.done = false;
       todo.content = newTodoElement.value.trim();
       newTodoElement.value = "";
-      todoListElement.nodes.add(buildTodoElement(todo));
+      todoListElement.nodes.add(new TodoElement(todo).element());
     }
   });
 }
 
-Element buildTodoElement(Todo todo) {
-  return new Element.html('''
-  <li ${todo.done ? 'class="completed"' : ''}>
-    <div class="view">
-      <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''}>
-      <label>${todo.content}</label>
-      <button class="destroy"></button>
-    </div>
-    <input class="edit" value="${todo.content}">
-  </li>
-  ''');
+class TodoElement {
+  Todo todo;
+  
+  TodoElement(this.todo) {
+  }
+  
+  Element element() {
+    Element element = new Element.html('''
+        <li ${todo.done ? 'class="completed"' : ''}>
+        <div class="view">
+        <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''}>
+        <label>${todo.content}</label>
+        <button class="destroy"></button>
+        </div>
+        <input class="edit" value="${todo.content}">
+        </li>
+    ''');
+    element.query(".destroy").on.click.add((MouseEvent e) {
+      element.remove();
+    });
+    return element;
+  }
 }
 
 class Todo {
