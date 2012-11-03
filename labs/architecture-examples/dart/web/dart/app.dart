54a2ac0c15ca22b448b9b9d355a1f23451b2b9f9
Sindre Sorhus <sindresorhus@gmail.com>
2012-11-03 20:20:08 +0100

    Dart app: convert to tabs

--- labs/architecture-examples/dart/web/dart/app.dart
+++ labs/architecture-examples/dart/web/dart/app.dart
@@ -8,35 +8,35 @@ part 'TodoWidget.dart';
 part 'TodoApp.dart';
 
 void main() {
-  new TodoApp();
+	new TodoApp();
 }
 
 class Todo {
-  String id;
-  String title;
-  bool completed;
+	String id;
+	String title;
+	bool completed;
 
-  Todo(String this.id, String this.title, {bool this.completed : false});
+	Todo(String this.id, String this.title, {bool this.completed : false});
 
-  Todo.fromJson(Map json) {
-    id = json['id'];
-    title = json['title'];
-    completed = json['completed'];
-  }
+	Todo.fromJson(Map json) {
+		id = json['id'];
+		title = json['title'];
+		completed = json['completed'];
+	}
 
-  Map toJson() {
-    return {'id': id, 'title': title, 'completed': completed};
-  }
+	Map toJson() {
+		return {'id': id, 'title': title, 'completed': completed};
+	}
 }
 
 class UUID {
-  static Random random = new Random();
+	static Random random = new Random();
 
-  static String createUuid() {
-    return "${S4()}${S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}";
-  }
+	static String createUuid() {
+		return "${S4()}${S4()}-${S4()}-${S4()}-${S4()}-${S4()}${S4()}${S4()}";
+	}
 
-  static String S4() {
-    return random.nextInt(65536).toRadixString(16);
-  }
+	static String S4() {
+		return random.nextInt(65536).toRadixString(16);
+	}
 }
