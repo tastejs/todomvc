383084f9cfb4441dd0f3a6f4d21a3d86ad10b97a
Erik de Jonge <erik@a8.nl>
2012-11-07 14:30:35 +0100

    start coffeescript angular branch

---
+++ labs/coffeescript-examples/angularjs-perf-coffee/js/directives/todoFocus.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Directive that places focus on the element it is applied to when the expression it binds to evaluates to true.
+ */
+todomvc.directive('todoFocus', function( $timeout ) {
+  return function( scope, elem, attrs ) {
+    scope.$watch(attrs.todoFocus, function( newval ) {
+      if ( newval ) {
+        $timeout(function() {
+          elem[0].focus();
+        }, 0, false);
+      }
+    });
+  };
+});
