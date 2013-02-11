552fad064fbe5cdc92f2535b3efa9d6acd3a7811
Pascal Hartig <phartig@rdrei.net>
2013-02-12 00:15:04 +0100

    AngularJS: Added unit tests

---
+++ architecture-examples/angularjs/test/config/testacular.conf.js
@@ -0,0 +1,14 @@
+basePath = '../../';
+
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'js/libs/angular/angular.min.js',
+  'test/libs/angular-mocks.js',
+  'js/**/*.js',
+  'test/unit/**/*.js'
+];
+
+autoWatch = true;
+
+browsers = ['Chrome'];
