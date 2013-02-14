81906dc83a4bbd128a1aeaa091a1bc090588561a
Pascal Hartig <phartig@rdrei.net>
2013-02-14 23:54:28 +0100

    ts+backbone: Fixed footer, remaining count
    The remaining todos counter was the same bug fixed in
    89e5d5c7afc8e2f1a27bdbd436dfd83dc36fc3b3.
    
    \#main and \#footer weren't hidden if there were no todos in the list.

--- labs/architecture-examples/typescript-backbone/js/app.ts
+++ labs/architecture-examples/typescript-backbone/js/app.ts
@@ -143,7 +143,7 @@ class TodoList extends Backbone.Collection {
 
 	// Filter down the list to only todo items that are still not finished.
 	remaining() {
-		return this.without(this.done());
+		return this.without.apply(this, this.done());
 	}
 
 	// We keep the Todos in sequential order, despite being saved by unordered
@@ -254,6 +254,8 @@ class AppView extends Backbone.View {
 
 	input: any;
 	allCheckbox: HTMLInputElement;
+	mainElement: HTMLElement;
+	footerElement: HTMLElement;
 	statsTemplate: (params: any) => string;
 
 	constructor () {
@@ -269,6 +271,8 @@ class AppView extends Backbone.View {
 
 		this.input = this.$('#new-todo');
 		this.allCheckbox = this.$('.mark-all-done')[0];
+		this.mainElement = this.$('#main')[0];
+		this.footerElement = this.$('#footer')[0];
 		this.statsTemplate = _.template($('#stats-template').html());
 
 		Todos.bind('add', this.addOne);
@@ -284,11 +288,19 @@ class AppView extends Backbone.View {
 		var done = Todos.done().length;
 		var remaining = Todos.remaining().length;
 
-		this.$('#todo-stats').html(this.statsTemplate({
-			total: Todos.length,
-			done: done,
-			remaining: remaining
-		}));
+		if (Todos.length) {
+			this.mainElement.style.display = 'block';
+			this.footerElement.style.display = 'block';
+
+			this.$('#todo-stats').html(this.statsTemplate({
+				total: Todos.length,
+				done: done,
+				remaining: remaining
+			}));
+		} else {
+			this.mainElement.style.display = 'none';
+			this.footerElement.style.display = 'none';
+		}
 
 		this.allCheckbox.checked = !remaining;
 	}
