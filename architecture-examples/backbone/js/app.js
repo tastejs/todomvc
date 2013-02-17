3f45e13be8a7c97e4666f3b31f3122229b247b3a
Pascal Hartig <phartig@rdrei.net>
2013-02-17 22:58:14 +0100

    Backbone: jshint style

--- architecture-examples/backbone/js/app.js
+++ architecture-examples/backbone/js/app.js
@@ -1,7 +1,10 @@
+/*global $*/
+/*jshint unused:false*/
 var app = app || {};
 var ENTER_KEY = 13;
 
-$(function() {
+$(function () {
+	'use strict';
 
 	// Kick things off by creating the **App**.
 	new app.AppView();
--- architecture-examples/backbone/js/routers/router.js
+++ architecture-examples/backbone/js/routers/router.js
@@ -1,17 +1,18 @@
+/*global Backbone*/
 var app = app || {};
 
-(function() {
+(function () {
 	'use strict';
 
 	// Todo Router
 	// ----------
 
 	var Workspace = Backbone.Router.extend({
-		routes:{
+		routes: {
 			'*filter': 'setFilter'
 		},
 
-		setFilter: function( param ) {
+		setFilter: function (param) {
 			// Set the current filter to be used
 			app.TodoFilter = param.trim() || '';
 
