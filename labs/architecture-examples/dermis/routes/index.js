dcd98b741092b7a9daafda54e04bb1621cc3e640
Pascal Hartig <phartig@rdrei.net>
2013-02-20 10:03:51 +0100

    dermis: jshint style for routes

--- labs/architecture-examples/dermis/routes/index.js
+++ labs/architecture-examples/dermis/routes/index.js
@@ -1,10 +1,13 @@
-define(['js/models/Todo', 'js/models/Todos'], function(Todo, Todos){
-  var app = {
-    show: function(){
-      Todos.set('mode', 'all');
-      $('.selected').removeClass('selected');
-      $('a[href$="#/"]').addClass('selected');
-    }
-  };
-  return app;
+/*global define $*/
+define(['js/models/Todo', 'js/models/Todos'], function (Todo, Todos) {
+	'use strict';
+
+	var app = {
+		show: function () {
+			Todos.set('mode', 'all');
+			$('.selected').removeClass('selected');
+			$('a[href$="#/"]').addClass('selected');
+		}
+	};
+	return app;
 });
