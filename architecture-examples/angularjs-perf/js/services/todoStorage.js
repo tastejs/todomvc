72e8e90ebfc5859a0cf8887c7b460db007271f8c
Pascal Hartig <phartig@rdrei.net>
2013-02-20 17:32:02 +0100

    angular-perf: jshint style

--- architecture-examples/angularjs-perf/js/services/todoStorage.js
+++ architecture-examples/angularjs-perf/js/services/todoStorage.js
@@ -1,18 +1,19 @@
+/*global todomvc*/
 'use strict';
 
 /**
  * Services that persists and retrieves TODOs from localStorage.
- */
-todomvc.factory( 'todoStorage', function() {
-  var STORAGE_ID = 'todos-angularjs-perf';
+*/
+todomvc.factory('todoStorage', function () {
+	var STORAGE_ID = 'todos-angularjs-perf';
 
-  return {
-    get: function() {
-      return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-    },
+	return {
+		get: function () {
+			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+		},
 
-    put: function( todos ) {
-      localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-    }
-  };
+		put: function (todos) {
+			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
+		}
+	};
 });
