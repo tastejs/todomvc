48e183af36b07da5020f33595b9929a55f932019
Pascal Hartig <phartig@rdrei.net>
2013-02-27 09:16:37 +0100

    olives: jshint style

--- labs/architecture-examples/olives/js/uis/List.js
+++ labs/architecture-examples/olives/js/uis/List.js
@@ -1,78 +1,80 @@
-define( 'Todos/List',
+/*global define*/
+define('Todos/List',
 
-[ 'Olives/OObject', 'Olives/Event-plugin', 'Olives/Model-plugin', 'Todos/Tools' ],
+['Olives/OObject', 'Olives/Event-plugin', 'Olives/Model-plugin', 'Todos/Tools'],
 
 // The List UI
-function List( OObject, EventPlugin, ModelPlugin, Tools ) {
+function List(OObject, EventPlugin, ModelPlugin, Tools) {
+	'use strict';
 
-	return function ListInit( view, model, stats ) {
+	return function ListInit(view, model, stats) {
 		// The OObject (the controller) inits with a default model which is a simple store
 		// But it can be init'ed with any other store, like the LocalStore
-		var list = new OObject( model ),
+		var list = new OObject(model),
 
 		ENTER_KEY = 13;
 
 		// The plugins
 		list.plugins.addAll({
-			'event': new EventPlugin( list ),
-			'model': new ModelPlugin( model, {
+			'event': new EventPlugin(list),
+			'model': new ModelPlugin(model, {
 				'toggleClass': Tools.toggleClass
 			}),
-			'stats': new ModelPlugin( stats, {
+			'stats': new ModelPlugin(stats, {
 				'toggleClass': Tools.toggleClass,
-				'toggleCheck': function ( value ) {
+				'toggleCheck': function (value) {
 					this.checked = model.getNbItems() === value ? 'on' : '';
 				}
 			})
 		});
 
 		// Remove the completed task
-		list.remove = function remove( event, node ) {
-			model.del( node.getAttribute('data-model_id') );
+		list.remove = function remove(event, node) {
+			model.del(node.getAttribute('data-model_id'));
 		};
 
 		// Un/check all tasks
-		list.toggleAll = function toggleAll( event, node ) {
+		list.toggleAll = function toggleAll(event, node) {
 			var checked = !!node.checked;
 
-			model.loop( function ( value, idx ) {
-				this.update( idx, 'completed', checked );
+			model.loop(function (value, idx) {
+				this.update(idx, 'completed', checked);
 			}, model);
 		};
 
 		// Enter edit mode
-		list.startEdit = function ( event, node ) {
+		list.startEdit = function (event, node) {
 			var taskId = node.getAttribute('data-model_id');
 
-			Tools.toggleClass.call( view.querySelector('li[data-model_id="' + taskId + '"]'), true, 'editing' );
+			Tools.toggleClass.call(view.querySelector('li[data-model_id="' + taskId + '"]'), true, 'editing');
 			view.querySelector('input.edit[data-model_id="' + taskId + '"]').focus();
 		};
 
 		// Leave edit mode
-		list.stopEdit = function ( event, node ) {
+		list.stopEdit = function (event, node) {
 			var taskId = node.getAttribute('data-model_id'),
 				value;
 
-			if ( event.keyCode === ENTER_KEY ) {
+			if (event.keyCode === ENTER_KEY) {
 				value = node.value.trim();
 
-				if ( value ) {
-					model.update( taskId, 'title', value );
+				if (value) {
+					model.update(taskId, 'title', value);
 				} else {
-					model.del( taskId );
+					model.del(taskId);
 				}
 
 				// When task #n is removed, #n+1 becomes #n, the dom node is updated to the new value, so editing mode should exit anyway
-				if ( model.has( taskId ) ) {
-					Tools.toggleClass.call( view.querySelector('li[data-model_id="' + taskId + '"]'), false, 'editing' );
+				if (model.has(taskId)) {
+					Tools.toggleClass.call(view.querySelector('li[data-model_id="' + taskId + '"]'), false, 'editing');
 				}
-			} else if ( event.type === 'blur' ) {
-				Tools.toggleClass.call( view.querySelector('li[data-model_id="' + taskId + '"]'), false, 'editing' );
+			} else if (event.type === 'blur') {
+				Tools.toggleClass.call(view.querySelector('li[data-model_id="' + taskId + '"]'), false, 'editing');
 			}
 		};
 
 		// Alive applies the plugins to the HTML view
-		list.alive( view );
+		list.alive(view);
 
 	};
 
