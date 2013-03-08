cb03b745e9608b39ce2607638f98f5e972472d22
Sindre Sorhus <sindresorhus@gmail.com>
2013-03-08 23:46:12 +0100

    Montage - convert to tabs

--- labs/architecture-examples/montage/core/todo.js
+++ labs/architecture-examples/montage/core/todo.js
@@ -2,19 +2,19 @@ var Montage = require("montage").Montage;
 
 exports.Todo = Montage.create(Montage, {
 
-    initWithTitle: {
-        value: function(title) {
-            this.title = title;
-            return this;
-        }
-    },
+	initWithTitle: {
+		value: function(title) {
+			this.title = title;
+			return this;
+		}
+	},
 
-    title: {
-        value: null
-    },
+	title: {
+		value: null
+	},
 
-    completed: {
-        value: false
-    }
+	completed: {
+		value: false
+	}
 
 });
--- labs/architecture-examples/montage/ui/main.reel/item-count-converter.js
+++ labs/architecture-examples/montage/ui/main.reel/item-count-converter.js
@@ -1,12 +1,12 @@
 var Montage = require("montage").Montage,
-    Converter = require("montage/core/converter/converter").Converter;
+	Converter = require("montage/core/converter/converter").Converter;
 
 exports.ItemCountConverter = Montage.create(Converter, {
 
-    convert: {
-        value: function(itemCount) {
-            return (1 === itemCount) ? "item" : "items";
-        }
-    }
+	convert: {
+		value: function(itemCount) {
+			return (1 === itemCount) ? "item" : "items";
+		}
+	}
 
 });
