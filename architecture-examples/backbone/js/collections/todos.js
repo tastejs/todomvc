1877dd109186a7096005f0582cf7466b7b2f013e
Sindre Sorhus <sindresorhus@gmail.com>
2013-03-09 16:59:55 +0100

    Backbone app - new name for Backbone.LocalStorage func

--- architecture-examples/backbone/js/collections/todos.js
+++ architecture-examples/backbone/js/collections/todos.js
@@ -1,4 +1,4 @@
-/*global Backbone Store*/
+/*global Backbone */
 var app = app || {};
 
 (function () {
@@ -15,7 +15,7 @@ var app = app || {};
 		model: app.Todo,
 
 		// Save all of the todo items under the `"todos"` namespace.
-		localStorage: new Store('todos-backbone'),
+		localStorage: new Backbone.LocalStorage('todos-backbone'),
 
 		// Filter down the list of all todo items that are finished.
 		completed: function () {
