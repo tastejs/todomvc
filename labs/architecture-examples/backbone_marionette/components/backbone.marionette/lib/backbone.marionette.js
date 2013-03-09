6f25047dff10a66f4b344da986454a809ca7034c
Sindre Sorhus <sindresorhus@gmail.com>
2013-03-09 19:18:41 +0100

    Backbone.Marionette app - use Bower components
    #475

--- labs/architecture-examples/backbone_marionette/components/backbone.marionette/lib/backbone.marionette.js
+++ labs/architecture-examples/backbone_marionette/components/backbone.marionette/lib/backbone.marionette.js
@@ -1,4 +1,4 @@
- // Backbone.Marionette, v1.0.0-rc5
+ // Backbone.Marionette, v1.0.0-rc6
  // Copyright (c)2013 Derick Bailey, Muted Solutions, LLC.
  // Distributed under MIT license
  // http://github.com/marionettejs/backbone.marionette
@@ -1240,8 +1240,16 @@ Marionette.CollectionView = Marionette.View.extend({
   // the collection view.
   render: function(){
     this.isClosed = false;
-
     this.triggerBeforeRender();
+    this._renderChildren();
+    this.triggerRendered();
+    return this;
+  },
+
+  // Internal method. Separated so that CompositeView can have
+  // more control over events being triggered, around the rendering
+  // process
+  _renderChildren: function(){
     this.closeEmptyView();
     this.closeChildren();
 
@@ -1250,9 +1258,6 @@ Marionette.CollectionView = Marionette.View.extend({
     } else {
       this.showEmptyView();
     }
-
-    this.triggerRendered();
-    return this;
   },
 
   // Internal method to loop through each item in the
@@ -1462,7 +1467,7 @@ Marionette.CompositeView = Marionette.CollectionView.extend({
     if (this.collection){
       this.listenTo(this.collection, "add", this.addChildView, this);
       this.listenTo(this.collection, "remove", this.removeItemView, this);
-      this.listenTo(this.collection, "reset", this.renderCollection, this);
+      this.listenTo(this.collection, "reset", this._renderChildren, this);
     }
   },
 
@@ -1500,22 +1505,27 @@ Marionette.CompositeView = Marionette.CollectionView.extend({
   // but the collection will not re-render.
   render: function(){
     this.isClosed = false;
-
     this.resetItemViewContainer();
 
+    this.triggerBeforeRender();
     var html = this.renderModel();
     this.$el.html(html);
-
     // the ui bindings is done here and not at the end of render since they 
     // will not be available until after the model is rendered, but should be
     // available before the collection is rendered.
     this.bindUIElements();
-
     this.triggerMethod("composite:model:rendered");
 
-    this.renderCollection();
+    this._renderChildren();
+
     this.triggerMethod("composite:rendered");
+    this.triggerRendered();
     return this;
+  },
+
+  _renderChildren: function(){
+    Marionette.CollectionView.prototype._renderChildren.call(this);
+    this.triggerMethod("composite:collection:rendered");
   },
 
   // Render the collection for the composite view
@@ -1523,7 +1533,6 @@ Marionette.CompositeView = Marionette.CollectionView.extend({
     var args = Array.prototype.slice.apply(arguments);
     Marionette.CollectionView.prototype.render.apply(this, args);
 
-    this.triggerMethod("composite:collection:rendered");
   },
 
   // Render an individual model, if we have one, as
