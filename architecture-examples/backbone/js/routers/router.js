c5b1781e39fa5ab900cbe2ef8bf7514fdc379f30
Pascal Hartig <phartig@rdrei.net>
2013-03-20 14:23:54 +0100

    Backbone: Updated to 1.0

--- architecture-examples/backbone/js/routers/router.js
+++ architecture-examples/backbone/js/routers/router.js
@@ -13,7 +13,7 @@ var app = app || {};
 
 		setFilter: function (param) {
 			// Set the current filter to be used
-			app.TodoFilter = param.trim() || '';
+			app.TodoFilter = param || '';
 
 			// Trigger a collection filter event, causing hiding/unhiding
 			// of Todo view items
