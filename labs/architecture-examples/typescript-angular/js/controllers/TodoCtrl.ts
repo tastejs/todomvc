fcb8994e1b180c94814847a2e246c6425d04038a
Aviv Ben-Yosef <aviv.by@gmail.com>
2013-03-21 22:03:17 +0200

    fix typo

--- labs/architecture-examples/typescript-angular/js/controllers/TodoCtrl.ts
+++ labs/architecture-examples/typescript-angular/js/controllers/TodoCtrl.ts
@@ -5,7 +5,7 @@ module todos {
 
     /**
      * The main controller for the app. The controller:
-     * - retrieves and persist the model via the todoStorage service
+     * - retrieves and persists the model via the todoStorage service
      * - exposes the model to the template and provides event handlers
      */
     export class TodoCtrl {
