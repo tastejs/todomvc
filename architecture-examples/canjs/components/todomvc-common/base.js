3a6d6afadb9102e5d9d7e5d17a3363de174c757b
Pascal Hartig <phartig@rdrei.net>
2013-03-24 17:30:41 +0100

    Update todomvc-common to 0.1.2
    Fixed #501 (part 2)

--- architecture-examples/canjs/components/todomvc-common/base.js
+++ architecture-examples/canjs/components/todomvc-common/base.js
@@ -5,6 +5,15 @@
 		var _gaq=[['_setAccount','UA-31081062-1'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'));
 	}
 
+	function getSourcePath() {
+		// If accessed via addyosmani.github.com/todomvc/, strip the project
+		// path.
+		if (location.hostname.indexOf('github.com') > 0) {
+			return location.pathname.replace(/todomvc\//, '');
+		}
+		return location.pathname;
+	}
+
 	function appendSourceLink() {
 		var sourceLink = document.createElement('a');
 		var paragraph = document.createElement('p');
@@ -12,7 +21,7 @@
 		var urlBase = 'https://github.com/addyosmani/todomvc/tree/gh-pages';
 
 		if (footer) {
-			sourceLink.href = urlBase + location.pathname;
+			sourceLink.href = urlBase + getSourcePath();
 			sourceLink.appendChild(document.createTextNode('Check out the source'));
 			paragraph.appendChild(sourceLink);
 			footer.appendChild(paragraph);
