cb1c87227b3b80ac009614f307b5cf8d58813853
Dan Shearmur <dan.shearmur@semantico.com>
2013-04-02 16:25:55 +0100

    Make all _gaq config options global (window)

--- labs/architecture-examples/socketstream/client/code/libs/base.js
+++ labs/architecture-examples/socketstream/client/code/libs/base.js
@@ -2,6 +2,6 @@
 	'use strict';
 
 	if ( location.hostname === 'todomvc.com' ) {
-		var _gaq=[['_setAccount','UA-31081062-1'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'));
+		window._gaq=[['_setAccount','UA-31081062-1'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'));
 	}
 })( window );
