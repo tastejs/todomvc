847aace5c5e9fa1c5312c3f5c9e28c9746b0eb62
gregloby <gregloby@gmail.com>
2013-06-18 18:02:59 +0200

    file-name -> constructorName  -  consistency changes for Backbone.js ver.

--- architecture-examples/backbone/js/collections/todos.js
+++ architecture-examples/backbone/js/collections/todos.js
@@ -9,7 +9,7 @@ var app = app || {};
 
 	// The collection of todos is backed by *localStorage* instead of a remote
 	// server.
-	var TodoList = Backbone.Collection.extend({
+	var Todos = Backbone.Collection.extend({
 		// Reference to this collection's model.
 		model: app.Todo,
 
@@ -44,5 +44,5 @@ var app = app || {};
 	});
 
 	// Create our global collection of **Todos**.
-	app.todos = new TodoList();
+	app.todos = new Todos();
 })();
