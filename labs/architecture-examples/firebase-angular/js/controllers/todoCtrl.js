8c48866bbb8c031d554d60a23b3956e053671b0d
Pascal Hartig <phartig@rdrei.net>
2013-08-04 17:23:28 +0200

    firebase-angular: refactor controller

--- labs/architecture-examples/firebase-angular/js/controllers/todoCtrl.js
+++ labs/architecture-examples/firebase-angular/js/controllers/todoCtrl.js
@@ -1,4 +1,4 @@
-/*global todomvc, angular */
+/*global todomvc, angular, Firebase */
 'use strict';
 
 /**
@@ -7,8 +7,73 @@
  * - exposes the model to the template and provides event handlers
  */
 todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, angularFire) {
-	var url = 'https://todomvc-angular.firebaseio.com/';
+	this.onDataLoaded = function onDataLoaded($scope, $location, url) {
+		$scope.$watch('todos', function () {
+			var total = 0;
+			var remaining = 0;
+			angular.forEach($scope.todos, function (todo) {
+				total++;
+				if (todo.completed === false) {
+					remaining++;
+				}
+			});
+			$scope.remainingCount = remaining;
+			$scope.completedCount = total - remaining;
+			$scope.allChecked = !$scope.remainingCount;
+		}, true);
 
+		$scope.addTodo = function () {
+			var newTodo = $scope.newTodo.trim();
+			if (!newTodo.length) {
+				return;
+			}
+			$scope.todos[new Firebase(url).push().name()] = {
+				title: newTodo,
+				completed: false
+			};
+			$scope.newTodo = '';
+		};
+
+		$scope.editTodo = function (id) {
+			$scope.editedTodo = $scope.todos[id];
+			$scope.originalTodo = angular.extend({}, $scope.editedTodo);
+		};
+
+		$scope.doneEditing = function (id) {
+			$scope.editedTodo = null;
+			var title = $scope.todos[id].title.trim();
+			if (!title) {
+				$scope.removeTodo(id);
+			}
+		};
+
+		$scope.revertEditing = function (id) {
+			$scope.todos[id] = $scope.originalTodo;
+			$scope.doneEditing(id);
+		};
+
+		$scope.removeTodo = function (id) {
+			delete $scope.todos[id];
+		};
+
+		$scope.clearCompletedTodos = function () {
+			var notCompleted = {};
+			angular.forEach($scope.todos, function (todo, id) {
+				if (!todo.completed) {
+					notCompleted[id] = todo;
+				}
+			});
+			$scope.todos = notCompleted;
+		};
+
+		$scope.markAll = function (completed) {
+			angular.forEach($scope.todos, function (todo) {
+				todo.completed = completed;
+			});
+		};
+	};
+
+	var url = 'https://todomvc-angular.firebaseio.com/';
 	$scope.newTodo = '';
 	$scope.editedTodo = null;
 
@@ -17,69 +82,7 @@ todomvc.controller('TodoCtrl', function TodoCtrl($scope, $location, angularFire)
 	}
 	$scope.location = $location;
 
-	angularFire(url, $scope, 'todos', {}).then(function() {
-		onDataLoaded($scope, $location, url);
-	});
+	angularFire(url, $scope, 'todos', {}).then(function () {
+		this.onDataLoaded($scope, $location, url);
+	}.bind(this));
 });
-
-function onDataLoaded($scope, $location, url) {
-	$scope.$watch('todos', function () {
-		var total = 0;
-		var remaining = 0;
-		angular.forEach($scope.todos, function(todo) {
-			total++;
-			if (todo.completed === false) remaining++;
-		});
-		$scope.remainingCount = remaining;
-		$scope.completedCount = total - remaining;
-		$scope.allChecked = !$scope.remainingCount;
-	}, true);
-
-	$scope.addTodo = function () {
-		var newTodo = $scope.newTodo.trim();
-		if (!newTodo.length) {
-			return;
-		}
-		$scope.todos[new Firebase(url).push().name()] = {
-			title: newTodo,
-			completed: false
-		};
-		$scope.newTodo = '';
-	};
-
-	$scope.editTodo = function (id) {
-		$scope.editedTodo = $scope.todos[id];
-		$scope.originalTodo = angular.extend({}, $scope.editedTodo);
-	};
-
-	$scope.doneEditing = function (id) {
-		$scope.editedTodo = null;
-		var title = $scope.todos[id].title.trim();
-		if (!title) {
-			$scope.removeTodo(id);
-		}
-	};
-
-	$scope.revertEditing = function (id) {
-		$scope.todos[id] = $scope.originalTodo;
-		$scope.doneEditing(id);
-	};
-
-	$scope.removeTodo = function (id) {
-		delete $scope.todos[id];
-	};
-
-	$scope.clearCompletedTodos = function () {
-		var notCompleted = {};
-		angular.forEach($scope.todos, function (todo, id) {
-			if (!todo.completed) notCompleted[id] = todo;
-		});
-		$scope.todos = notCompleted;
-	};
-
-	$scope.markAll = function (completed) {
-		angular.forEach($scope.todos, function (todo) {
-			todo.completed = completed;
-		});
-	};
-}
