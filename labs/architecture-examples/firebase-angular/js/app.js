20100b515b5152e7f98dedb079d27d260f3fe0d1
Pascal Hartig <phartig@rdrei.net>
2013-08-04 17:14:24 +0200

    firebase-angular: refactored todo filtering

--- labs/architecture-examples/firebase-angular/js/app.js
+++ labs/architecture-examples/firebase-angular/js/app.js
@@ -15,13 +15,17 @@ todomvc.filter('todoFilter', function ($location) {
 		angular.forEach(input, function (todo, id) {
 			var path = $location.path();
 			if (path === '/active') {
-				if (todo.completed === false) filtered[id] = todo;
+				if (!todo.completed) {
+					filtered[id] = todo;
+				}
 			} else if (path === '/completed') {
-				if (todo.completed === true) filtered[id] = todo;
+				if (todo.completed) {
+					filtered[id] = todo;
+				}
 			} else {
 				filtered[id] = todo;
 			}
 		});
 		return filtered;
-	}
+	};
 });
