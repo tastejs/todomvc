6551cbbb5d08b24e706ca1c311c6e77bdf8a5a08
abhinav <abhinavgujjar@gmail.com>
2013-08-08 12:45:30 +0530

    Added Durandal framework

---
+++ labs/dependency-examples/durandal/bower_components/durandal/system.js
@@ -0,0 +1,165 @@
+﻿define(['require'], function (require) {
+    var isDebugging = false,
+        nativeKeys = Object.keys,
+        hasOwnProperty = Object.prototype.hasOwnProperty,
+        toString = Object.prototype.toString,
+        system,
+        treatAsIE8 = false;
+
+    //see http://patik.com/blog/complete-cross-browser-console-log/
+    // Tell IE9 to use its built-in console
+    if (Function.prototype.bind && (typeof console === 'object' || typeof console === 'function') && typeof console.log == 'object') {
+        try {
+            ['log', 'info', 'warn', 'error', 'assert', 'dir', 'clear', 'profile', 'profileEnd']
+                .forEach(function(method) {
+                    console[method] = this.call(console[method], console);
+                }, Function.prototype.bind);
+        } catch (ex) {
+            treatAsIE8 = true;
+        }
+    }
+
+    // callback for dojo's loader 
+    // note: if you wish to use Durandal with dojo's AMD loader,
+    // currently you must fork the dojo source with the following
+    // dojo/dojo.js, line 1187, the last line of the finishExec() function: 
+    //  (add) signal("moduleLoaded", [module.result, module.mid]);
+    // an enhancement request has been submitted to dojo to make this
+    // a permanent change. To view the status of this request, visit:
+    // http://bugs.dojotoolkit.org/ticket/16727
+
+    if (require.on) {
+        require.on("moduleLoaded", function (module, mid) {
+            system.setModuleId(module, mid);
+        });
+    }
+
+    // callback for require.js loader
+    if (typeof requirejs !== 'undefined') {
+        requirejs.onResourceLoad = function (context, map, depArray) {
+            system.setModuleId(context.defined[map.id], map.id);
+        };
+    }
+
+    var noop = function() { };
+
+    var log = function() {
+        try {
+            // Modern browsers
+            if (typeof console != 'undefined' && typeof console.log == 'function') {
+                // Opera 11
+                if (window.opera) {
+                    var i = 0;
+                    while (i < arguments.length) {
+                        console.log('Item ' + (i + 1) + ': ' + arguments[i]);
+                        i++;
+                    }
+                }
+                    // All other modern browsers
+                else if ((Array.prototype.slice.call(arguments)).length == 1 && typeof Array.prototype.slice.call(arguments)[0] == 'string') {
+                    console.log((Array.prototype.slice.call(arguments)).toString());
+                } else {
+                    console.log(Array.prototype.slice.call(arguments));
+                }
+            }
+                // IE8
+            else if ((!Function.prototype.bind || treatAsIE8) && typeof console != 'undefined' && typeof console.log == 'object') {
+                Function.prototype.call.call(console.log, console, Array.prototype.slice.call(arguments));
+            }
+
+            // IE7 and lower, and other old browsers
+        } catch(ignore) {}
+    };
+
+    system = {
+        version:"1.2.0",
+        noop: noop,
+        getModuleId: function(obj) {
+            if (!obj) {
+                return null;
+            }
+            
+            if (typeof obj == 'function') {
+                return obj.prototype.__moduleId__;
+            }
+
+            if (typeof obj == 'string') {
+                return null;
+            }
+
+            return obj.__moduleId__;
+        },
+        setModuleId: function (obj, id) {
+            if (!obj) {
+                return;
+            }
+
+            if (typeof obj == 'function') {
+                obj.prototype.__moduleId__ = id;
+                return;
+            }
+
+            if (typeof obj == 'string') {
+                return;
+            }
+
+            obj.__moduleId__ = id;
+        },
+        debug: function(enable) {
+            if (arguments.length == 1) {
+                isDebugging = enable;
+                if (isDebugging) {
+                    this.log = log;
+                    this.log('Debug mode enabled.');
+                } else {
+                    this.log('Debug mode disabled.');
+                    this.log = noop;
+                }
+            } else {
+                return isDebugging;
+            }
+        },
+        isArray: function(obj) {
+            return toString.call(obj) === '[object Array]';
+        },
+        log: noop,
+        defer: function(action) {
+            return $.Deferred(action);
+        },
+        guid: function() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        },
+        acquire: function() {
+            var modules = Array.prototype.slice.call(arguments, 0);
+            return this.defer(function(dfd) {
+                require(modules, function() {
+                    var args = arguments;
+                    setTimeout(function() {
+                        dfd.resolve.apply(dfd, args);
+                    }, 1);
+                });
+            }).promise();
+        }
+    };
+
+    system.keys = nativeKeys || function(obj) {
+        if (obj !== Object(obj)) {
+            throw new TypeError('Invalid object');
+        }
+
+        var keys = [];
+
+        for (var key in obj) {
+            if (hasOwnProperty.call(obj, key)) {
+                keys[keys.length] = key;
+            }
+        }
+
+        return keys;
+    };
+
+    return system;
+});
---
+++ labs/dependency-examples/durandal/js/viewmodels/shell.js
@@ -0,0 +1,15 @@
+﻿/*global define*/
+define([
+	'bower_components/durandal/plugins/router',
+], function (router) {
+
+	'use strict';
+	
+	return {
+		router: router,
+		filter: undefined, // this is used as the global cache to figure out the filter in effect.
+		activate: function () {
+			return router.activate('todoapp');
+		}
+	};
+});
