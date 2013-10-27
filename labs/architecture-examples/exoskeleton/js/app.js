133d5ccaebdd1e0733c421824d78aaf8ea73709d
Paul Miller <paul@paulmillr.com>
2013-10-27 07:19:25 +0200

    Add exoskeleton todomvc.

---
+++ labs/architecture-examples/exoskeleton/js/app.js
@@ -0,0 +1,12 @@
+/*global $ */
+/*jshint unused:false */
+var app = app || {};
+var ENTER_KEY = 13;
+var ESCAPE_KEY = 27;
+
+document.addEventListener('DOMContentLoaded', function () {
+	'use strict';
+
+	// kick things off by creating the `App`
+	new app.AppView();
+}, false);
