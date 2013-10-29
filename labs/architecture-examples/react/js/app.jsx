6a65984733ccfbbb398c2e8d5773512f4c7c296c
petehunt <floydophone@gmail.com>
2013-10-29 16:33:39 -0700

    comment from @chenglou

--- labs/architecture-examples/react/js/app.jsx
+++ labs/architecture-examples/react/js/app.jsx
@@ -61,6 +61,9 @@
 		toggleAll: function (event) {
 			var checked = event.target.checked;
 
+      // Note: it's usually better to use immutable data structures since they're easier to
+      // reason about and React works very well with them. That's why we use map() and filter()
+      // everywhere instead of mutating the array or todo items themselves.
 			var newTodos = this.state.todos.map(function (todo) {
 				return Utils.extend({}, todo, {completed: checked});
 			});
