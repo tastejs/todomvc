2191e0291427abb0610ce2f168a3bdbde3088ac8
Cory Forsyth <cory.forsyth@gmail.com>
2013-10-30 10:06:51 +0100

    Close GH-670: Ember 1.0.0 and Ember Data 1.0.0-beta.1.

--- architecture-examples/emberjs/js/controllers/todo_controller.js
+++ architecture-examples/emberjs/js/controllers/todo_controller.js
@@ -1,17 +1,48 @@
-/*global Todos Ember */
+/*global Todos, Ember */
 'use strict';
 
 Todos.TodoController = Ember.ObjectController.extend({
 	isEditing: false,
 
-	editTodo: function () {
-		this.set('isEditing', true);
-	},
+	// We use the bufferedTitle to store the original value of
+	// the model's title so that we can roll it back later in the
+	// `cancelEditing` action.
+	bufferedTitle: Ember.computed.oneWay('title'),
 
-	removeTodo: function () {
-		var todo = this.get('model');
+	actions: {
+		editTodo: function () {
+			this.set('isEditing', true);
+		},
 
-		todo.deleteRecord();
-		todo.get('store').commit();
+		doneEditing: function () {
+			var bufferedTitle = this.get('bufferedTitle');
+
+			if (Ember.isEmpty(bufferedTitle.trim())) {
+				// The `doneEditing` action gets sent twice when the user hits
+				// enter (once via 'insert-newline' and once via 'focus-out').
+				//
+				// We debounce our call to 'removeTodo' so that it only gets
+				// sent once.
+				Ember.run.debounce(this, this.send, 'removeTodo', 0);
+			} else {
+				var todo = this.get('model');
+				todo.set('title', this.get('bufferedTitle'));
+				todo.save();
+			}
+
+			this.set('isEditing', false);
+		},
+
+		cancelEditing: function () {
+			this.set('bufferedTitle', this.get('title'));
+			this.set('isEditing', false);
+		},
+
+		removeTodo: function () {
+			var todo = this.get('model');
+
+			todo.deleteRecord();
+			todo.save();
+		}
 	}
 });
