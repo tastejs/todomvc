fe136aea08d62346275003899ba569bd83af2060
Ralf S. Engelschall <rse@engelschall.com>
2013-11-30 19:58:05 +0100

    provide ComponentJS TodoMVC Example implementation

---
+++ labs/architecture-examples/componentjs/app/app-ui-composite-main.js
@@ -0,0 +1,90 @@
+
+/*  component of the "main" UI composite  */
+cs.ns("app.ui.composite").main = cs.clazz({
+    mixin: [ cs.marker.controller, cs.marker.view ],
+    protos: {
+        create: function () {
+            /*  create todo widget components  */
+            cs(this).create("todo-model/todo-view", app.ui.widget.todo.model, app.ui.widget.todo.view)
+            cs(this).property("ComponentJS:state-auto-increase", true)
+        },
+        prepare: function () {
+            var self = this
+            var todoModel = cs(self, "//todo-model")
+
+            /*  two-way bind URL route to presentation model  */
+            var router = new Router({
+                "/":          function () { todoFilterSelect("all")       },
+                "/active":    function () { todoFilterSelect("active")    },
+                "/completed": function () { todoFilterSelect("completed") }
+            })
+            var todoFilterSelect = function (filter) {
+                todoModel.value("state:status-filter-selected", filter)
+                todoModel.value("cmd:item-list-updated", true)
+            }
+            todoModel.observe({ name: "event:status-filter-select", func: function (ev, value) {
+                var route = "/" + value;
+                if (route === "/all")
+                    route = "/";
+                if (router.getRoute() !== route) {
+                    window.location.hash = "#" + route
+                    router.setRoute(route)
+                }
+            }})
+            router.init()
+
+            /*  react on item CRUD model event value changes  */
+            todoModel.observe({ name: "event:new-item-create", func: function (/* ev, value */) {
+                var text = todoModel.value("data:new-item-text")
+                todoModel.value("data:new-item-text", "")
+                var todoList = app.sv.todo()
+                var todoItem = new app.dm.TodoItem({ title: text })
+                todoList.itemAdd(todoItem)
+                app.sv.save()
+                bm2pm()
+            }})
+            todoModel.observe({ name: "event:item-list-item-modified", func: function (ev, item) {
+                var todoList = app.sv.todo()
+                var todoItem = todoList.itemById(item.id)
+                todoItem.title     = item.title
+                todoItem.completed = item.completed
+                app.sv.save()
+                bm2pm()
+            }})
+            todoModel.observe({ name: "event:item-list-item-removed", func: function (ev, item) {
+                var todoList = app.sv.todo()
+                var todoItem = todoList.itemById(item.id)
+                todoList.itemDel(todoItem)
+                app.sv.save()
+                bm2pm()
+            }})
+
+            /*  transfer business model into presentation model  */
+            var bm2pm = function () {
+                var pmItems = []
+                var bmTodoList = app.sv.todo()
+                _.forEach(bmTodoList.items, function (bmTodoItem) {
+                    pmItems.push({
+                        id:        bmTodoItem.id,
+                        title:     bmTodoItem.title,
+                        completed: bmTodoItem.completed,
+                        editing:   false
+                    })
+                })
+                todoModel.value("data:item-list", pmItems)
+                todoModel.value("cmd:item-list-updated", true)
+            }
+
+            /*  initially load business model and trigger transfer into presentation model  */
+            app.sv.load()
+            bm2pm()
+        },
+        render: function () {
+            /*  render view mask  */
+            var ui = $.markup("main")
+            cs(this).plug(ui)
+            cs(this).socket({ ctx: $(".main__todo", ui), spool: "materialized" });
+        }
+    }
+})
+
---
+++ labs/architecture-examples/componentjs/app/app.js
@@ -0,0 +1,18 @@
+
+/*  application bootstrap class  */
+ComponentJS.symbol("cs")
+cs.ns("app").boot = cs.clazz({
+    statics: {
+        init: function () {
+            /*  bootstrap ComponentJS framework  */
+            cs.bootstrap()
+            cs.debug(0)
+        },
+        main: function () {
+            /*  fire up the component tree  */
+            cs.create("/ui", app.ui.composite.root)
+            cs("/ui").state(_.isObject(document) ? "visible" : "prepared")
+        }
+    }
+})
+
