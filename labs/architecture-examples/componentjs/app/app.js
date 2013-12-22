a1d0084828a3d4ca297b19bf77d691275bb6d91a
Pascal Hartig <passy@twitter.com>
2013-12-22 20:30:56 +0100

    WIP: ComponentJS cleanup

--- labs/architecture-examples/componentjs/app/app.js
+++ labs/architecture-examples/componentjs/app/app.js
@@ -1,18 +1,23 @@
+/*global cs, app, ComponentJS, _*/
+/* application bootstrap class */
 
-/*  application bootstrap class  */
-ComponentJS.symbol("cs")
-cs.ns("app").boot = cs.clazz({
-    statics: {
-        init: function () {
-            /*  bootstrap ComponentJS framework  */
-            cs.bootstrap()
-            cs.debug(0)
-        },
-        main: function () {
-            /*  fire up the component tree  */
-            cs.create("/ui", app.ui.composite.root)
-            cs("/ui").state(_.isObject(document) ? "visible" : "prepared")
-        }
-    }
-})
-
+(function () {
+	'use strict';
+	ComponentJS.symbol('cs');
+	cs.ns('app').boot = cs.clazz({
+		statics: {
+			init: function () {
+				/* bootstrap ComponentJS framework */
+				cs.bootstrap();
+				// ComponentJS provides debugging levels from 0 (disabled) to 9
+				// (all debug messages)
+				cs.debug(0);
+			},
+			main: function () {
+				/* fire up the component tree */
+				cs.create('/ui', app.ui.composite.root);
+				cs('/ui').state(_.isObject(document) ? 'visible' : 'prepared');
+			}
+		}
+	});
+}());
