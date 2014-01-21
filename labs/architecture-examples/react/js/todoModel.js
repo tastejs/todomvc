a656e4afb809dc44fe5de58e9e9900a5eca76b85
petehunt <floydophone@gmail.com>
2014-01-20 23:51:46 -0800

    Make React TodoMVC more MVC-ish

---
+++ labs/architecture-examples/react/js/todoModel.js
@@ -0,0 +1,86 @@
+/**
+ * @jsx React.DOM
+ */
+/*jshint quotmark:false */
+/*jshint white:false */
+/*jshint trailing:false */
+/*jshint newcap:false */
+/*global Utils */
+
+(function (window) {
+	'use strict';
+
+	// Generic "model" object. You can use whatever
+	// framework you want. For this application it
+	// may not even be worth separating this logic
+	// out, but we do this to demonstrate one way to
+	// separate out parts of your application.
+	window.TodoModel = function (key) {
+		this.key = key;
+		this.todos = Utils.store(key);
+		this.onChanges = [];
+	};
+
+	window.TodoModel.prototype.subscribe = function (onChange) {
+		this.onChanges.push(onChange);
+	};
+
+	window.TodoModel.prototype.inform = function () {
+		Utils.store(this.key, this.todos);
+		this.onChanges.forEach(function (cb) { cb(); });
+	};
+
+	window.TodoModel.prototype.addTodo = function (title) {
+		this.todos = this.todos.concat({
+			id: Utils.uuid(),
+			title: title,
+			completed: false
+		});
+
+		this.inform();
+	};
+
+	window.TodoModel.prototype.toggleAll = function (checked) {
+		// Note: it's usually better to use immutable data structures since they're easier to
+		// reason about and React works very well with them. That's why we use map() and filter()
+		// everywhere instead of mutating the array or todo items themselves.
+		this.todos = this.todos.map(function (todo) {
+			return Utils.extend({}, todo, {completed: checked});
+		});
+
+		this.inform();
+	};
+
+	window.TodoModel.prototype.toggle = function (todoToToggle) {
+		this.todos = this.todos.map(function (todo) {
+			return todo !== todoToToggle ? todo : Utils.extend({}, todo, {completed: !todo.completed});
+		});
+
+		this.inform();
+	};
+
+	window.TodoModel.prototype.destroy = function (todo) {
+		this.todos = this.todos.filter(function (candidate) {
+			return candidate.id !== todo.id;
+		});
+
+		this.inform();
+	};
+
+	window.TodoModel.prototype.save = function (todoToSave, text) {
+		this.todos = this.todos.map(function (todo) {
+			return todo !== todoToSave ? todo : Utils.extend({}, todo, {title: text});
+		});
+
+		this.inform();
+	};
+
+	window.TodoModel.prototype.clearCompleted = function () {
+		this.todos = this.todos.filter(function (todo) {
+			return !todo.completed;
+		});
+
+		this.inform();
+	};
+
+})(this);
