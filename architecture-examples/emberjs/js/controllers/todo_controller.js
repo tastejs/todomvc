193752e8e166583a6a9ec475b0851198061fdac2
Peter Müller <munter@fumle.dk>
2014-02-13 23:59:03 +0100

    Close GH-847: Ember: Prevent global leakage of 'use strict' directive.

--- architecture-examples/emberjs/js/controllers/todo_controller.js
+++ architecture-examples/emberjs/js/controllers/todo_controller.js
@@ -1,58 +1,59 @@
 /*global Todos, Ember */
-'use strict';
+(function () {
+	'use strict';
+	Todos.TodoController = Ember.ObjectController.extend({
+		isEditing: false,
 
-Todos.TodoController = Ember.ObjectController.extend({
-	isEditing: false,
+		// We use the bufferedTitle to store the original value of
+		// the model's title so that we can roll it back later in the
+		// `cancelEditing` action.
+		bufferedTitle: Ember.computed.oneWay('title'),
 
-	// We use the bufferedTitle to store the original value of
-	// the model's title so that we can roll it back later in the
-	// `cancelEditing` action.
-	bufferedTitle: Ember.computed.oneWay('title'),
+		actions: {
+			editTodo: function () {
+				this.set('isEditing', true);
+			},
 
-	actions: {
-		editTodo: function () {
-			this.set('isEditing', true);
-		},
+			doneEditing: function () {
+				var bufferedTitle = this.get('bufferedTitle').trim();
 
-		doneEditing: function () {
-			var bufferedTitle = this.get('bufferedTitle').trim();
+				if (Ember.isEmpty(bufferedTitle)) {
+					// The `doneEditing` action gets sent twice when the user hits
+					// enter (once via 'insert-newline' and once via 'focus-out').
+					//
+					// We debounce our call to 'removeTodo' so that it only gets
+					// made once.
+					Ember.run.debounce(this, 'removeTodo', 0);
+				} else {
+					var todo = this.get('model');
+					todo.set('title', bufferedTitle);
+					todo.save();
+				}
 
-			if (Ember.isEmpty(bufferedTitle)) {
-				// The `doneEditing` action gets sent twice when the user hits
-				// enter (once via 'insert-newline' and once via 'focus-out').
-				//
-				// We debounce our call to 'removeTodo' so that it only gets
-				// made once.
-				Ember.run.debounce(this, 'removeTodo', 0);
-			} else {
-				var todo = this.get('model');
-				todo.set('title', bufferedTitle);
-				todo.save();
+				// Re-set our newly edited title to persist its trimmed version
+				this.set('bufferedTitle', bufferedTitle);
+				this.set('isEditing', false);
+			},
+
+			cancelEditing: function () {
+				this.set('bufferedTitle', this.get('title'));
+				this.set('isEditing', false);
+			},
+
+			removeTodo: function () {
+				this.removeTodo();
 			}
-
-			// Re-set our newly edited title to persist its trimmed version
-			this.set('bufferedTitle', bufferedTitle);
-			this.set('isEditing', false);
-		},
-
-		cancelEditing: function () {
-			this.set('bufferedTitle', this.get('title'));
-			this.set('isEditing', false);
 		},
 
 		removeTodo: function () {
-			this.removeTodo();
-		}
-	},
+			var todo = this.get('model');
 
-	removeTodo: function () {
-		var todo = this.get('model');
+			todo.deleteRecord();
+			todo.save();
+		},
 
-		todo.deleteRecord();
-		todo.save();
-	},
-
-	saveWhenCompleted: function () {
-		this.get('model').save();
-	}.observes('isCompleted')
-});
+		saveWhenCompleted: function () {
+			this.get('model').save();
+		}.observes('isCompleted')
+	});
+})();
--- architecture-examples/emberjs/js/views/edit_todo_view.js
+++ architecture-examples/emberjs/js/views/edit_todo_view.js
@@ -1,12 +1,14 @@
 /*global Todos, Ember */
-'use strict';
+(function () {
+	'use strict';
 
-Todos.EditTodoView = Ember.TextField.extend({
-	focusOnInsert: function () {
-		// Re-set input value to get rid of a reduntant text selection
-		this.$().val(this.$().val());
-		this.$().focus();
-	}.on('didInsertElement')
-});
+	Todos.EditTodoView = Ember.TextField.extend({
+		focusOnInsert: function () {
+			// Re-set input value to get rid of a reduntant text selection
+			this.$().val(this.$().val());
+			this.$().focus();
+		}.on('didInsertElement')
+	});
 
-Ember.Handlebars.helper('edit-todo', Todos.EditTodoView);
+	Ember.Handlebars.helper('edit-todo', Todos.EditTodoView);
+})();
