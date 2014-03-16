71eec828281706992928d8b9d61148ac5d63db3d
Marcelo Eden <edendroid@gmail.com>
2014-03-15 23:25:48 -0700

    footer presenter

--- labs/architecture-examples/riotjs/js/presenters/footer_presenter.js
+++ labs/architecture-examples/riotjs/js/presenters/footer_presenter.js
@@ -2,22 +2,24 @@
 
 function footerPresenter(element, options) {
     var todo = options.model,
-        template = options.template;
+        template = options.template,
+        filter = "#/";
 
     // Bind user events
     element.on('click', '#filters a', function(e){
         var $target = $(e.target);
         e.preventDefault();
-        $.route($(this).attr("href"));
+        filter = $target.attr("href");
+        $.route(filter);
     });
 
     // Bind model events
     todo.on('add remove toggle load', counts);
 
     function counts() {
-        var data = getData(), hash = window.location.hash;
+        var data = getData(), hash = location.hash;
         element.html($.render(template, data));
-        $('a[href="'+ hash +'"]', element).addClass('selected');
+        $('a[href="'+ filter +'"]', element).addClass('selected');
         toggle(data);
     }
 
