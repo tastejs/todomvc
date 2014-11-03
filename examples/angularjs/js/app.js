7fe92656d226cb49bc2364964bab06b8fcffc8d9
TasteBot <travis@rdrei.net>
2014-11-03 14:55:13 +0000

    update the build files for gh-pages [ci skip]

--- examples/angularjs/js/app.js
+++ examples/angularjs/js/app.js
@@ -9,13 +9,24 @@ angular.module('todomvc', ['ngRoute'])
 	.config(function ($routeProvider) {
 		'use strict';
 
-		$routeProvider.when('/', {
+		var routeConfig = {
 			controller: 'TodoCtrl',
-			templateUrl: 'todomvc-index.html'
-		}).when('/:status', {
-			controller: 'TodoCtrl',
-			templateUrl: 'todomvc-index.html'
-		}).otherwise({
-			redirectTo: '/'
-		});
+			templateUrl: 'todomvc-index.html',
+			resolve: {
+				store: function (todoStorage) {
+					// Get the correct module (API or localStorage).
+					return todoStorage.then(function (module) {
+						module.get(); // Fetch the todo records in the background.
+						return module;
+					});
+				}
+			}
+		};
+
+		$routeProvider
+			.when('/', routeConfig)
+			.when('/:status', routeConfig)
+			.otherwise({
+				redirectTo: '/'
+			});
 	});
