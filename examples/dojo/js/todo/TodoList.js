40fe1ba3e2d889ea1d2b22aa3675d16b104e32ba
TasteBot <travis@rdrei.net>
2014-12-12 23:08:47 +0000

    update the build files for gh-pages [ci skip]

--- examples/dojo/js/todo/TodoList.js
+++ examples/dojo/js/todo/TodoList.js
@@ -1,23 +1,24 @@
 define([
-	"dojo/_base/declare",
-	"dijit/_TemplatedMixin",
-	"dijit/_WidgetsInTemplateMixin",
-	"dojox/mvc/WidgetList",
-	"dojox/mvc/_InlineTemplateMixin",
-	"todo/CssToggleWidget",
-	"todo/ctrl/_HashCompletedMixin"
+	'dojo/_base/declare',
+	'dijit/_TemplatedMixin',
+	'dijit/_WidgetsInTemplateMixin',
+	'dojox/mvc/WidgetList',
+	'dojox/mvc/_InlineTemplateMixin',
+	'todo/CssToggleWidget',
+	'todo/ctrl/_HashCompletedMixin'
 ], function(declare, _TemplatedMixin, _WidgetsInTemplateMixin, WidgetList, _InlineTemplateMixin, CssToggleWidget, _HashCompletedMixin){
 	return declare([WidgetList, _InlineTemplateMixin], {
 		childClz: declare([CssToggleWidget,  _TemplatedMixin, _WidgetsInTemplateMixin, _HashCompletedMixin], {
-			_setCompletedAttr: {type: "classExists", className: "completed"},
-			_setHiddenAttr: {type: "classExists", className: "hidden"},
+			_setCompletedAttr: { type: 'classExists', className: 'completed' },
+			_setHiddenAttr: { type: 'classExists', className: 'hidden' },
 
-			onRemoveClick: function(){
+			onRemoveClick: function () {
 				this.parent.listCtrl.removeItem(this.uniqueId);
+				this.destroyRecursive();
 			},
 
-			onEditBoxChange: function(){
-				if(!this.editBox.value){
+			onEditBoxChange: function () {
+				if (!this.editBox.value) {
 					this.parent.listCtrl.removeItem(this.uniqueId);
 				}
 			}
