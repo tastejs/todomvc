90e05ce9a8a1544753eb2b49ca987ca6d5147f66
TasteBot <travis@rdrei.net>
2015-03-10 15:00:19 +0000

    update the build files for gh-pages [ci skip]

---
+++ examples/canjs_require/node_modules/canjs/amd/can/util/library.js
@@ -0,0 +1,12 @@
+/*!
+ * CanJS - 2.0.7
+ * http://canjs.us/
+ * Copyright (c) 2014 Bitovi
+ * Wed, 26 Mar 2014 16:12:27 GMT
+ * Licensed MIT
+ * Includes: CanJS default build
+ * Download from: http://canjs.us/
+ */
+define(["can/util/jquery"], function (can) {
+	return can;
+});
