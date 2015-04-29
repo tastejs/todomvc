0dca9e86cefd40b2b5bbaf7272b570f03990da54
Leonardo Giovanetti <leorou@gmail.com>
2015-04-29 20:34:34 -0300

    Making the controller function anonymous to avoid confusions as the name is assigned elsewhere

--- examples/angularjs_require/js/controllers/todo.js
+++ examples/angularjs_require/js/controllers/todo.js
@@ -11,7 +11,7 @@ define([
 	'angular'
 ], function (angular) {
 	return ['$scope', '$location', 'todoStorage', 'filterFilter',
-		function TodoController($scope, $location, todoStorage, filterFilter) {
+		function($scope, $location, todoStorage, filterFilter) {
 			var todos = $scope.todos = todoStorage.get();
 
 			$scope.newTodo = '';
