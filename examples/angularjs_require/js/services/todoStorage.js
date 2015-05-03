57cda79cc8ca51ccaac7b075deb619003a258a28
TasteBot <travis@rdrei.net>
2015-05-03 13:13:59 +0000

    update the build files for gh-pages [ci skip]

--- examples/angularjs_require/js/services/todoStorage.js
+++ examples/angularjs_require/js/services/todoStorage.js
@@ -4,18 +4,25 @@
 /**
  * Services that persists and retrieves TODOs from localStorage.
  */
-define(['app'], function (app) {
-	app.factory('todoStorage', function () {
-		var STORAGE_ID = 'todos-angularjs-requirejs';
+ 
+define([
+	'angular'
+], function (angular) {
+	var moduleName = 'TodoStorageModule';
+	angular
+		.module(moduleName, [])
+		.factory('todoStorage', function () {
+			var STORAGE_ID = 'todos-angularjs-requirejs';
 
-		return {
-			get: function () {
-				return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-			},
+			return {
+				get: function () {
+					return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+				},
 
-			put: function (todos) {
-				localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-			}
-		};
-	});
+				put: function (todos) {
+					localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
+				}
+			};
+		});
+	return moduleName;
 });
