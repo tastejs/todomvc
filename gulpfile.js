3892594bd66cd689be420b64e769eeddcbc59f3f
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-07-03 10:36:56 +0200

    Fix code style in `gulpfile.js`

--- gulpfile.js
+++ gulpfile.js
@@ -9,82 +9,82 @@ var pagespeed = require('psi');
 var app = require('./server');
 
 var AUTOPREFIXER_BROWSERS = [
-  'ie >= 10',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4.4',
-  'bb >= 10'
+	'ie >= 10',
+	'ie_mob >= 10',
+	'ff >= 30',
+	'chrome >= 34',
+	'safari >= 7',
+	'opera >= 23',
+	'ios >= 7',
+	'android >= 4.4',
+	'bb >= 10'
 ];
 
 // Lint JavaScript
 gulp.task('jshint', function () {
-  return gulp.src('site-assets/*.js')
-    .pipe($.jshint())
-    .pipe($.jshint.reporter('jshint-stylish'));
+	return gulp.src('site-assets/*.js')
+		.pipe($.jshint())
+		.pipe($.jshint.reporter('jshint-stylish'));
 });
 
 // Optimize Images
 gulp.task('images', function () {
-  return gulp.src('site-assets/*.{png,jpg,svg}')
-    .pipe($.cache($.imagemin({
-      progressive: true,
-      interlaced: true
-    })))
-    .pipe(gulp.dest('dist/site-assets'))
-    .pipe($.size({title: 'images'}));
+	return gulp.src('site-assets/*.{png,jpg,svg}')
+		.pipe($.cache($.imagemin({
+			progressive: true,
+			interlaced: true
+		})))
+		.pipe(gulp.dest('dist/site-assets'))
+		.pipe($.size({title: 'images'}));
 });
 
 // Copy All Files At The Root Level (app)
 gulp.task('copy', function () {
-  return gulp.src([
-    'examples/**',
-    'bower_components/**',
-    'learn.json',
-    'CNAME',
-    '.nojekyll',
-    'site-assets/favicon.ico'
-  ], {
-    dot: true,
-    base: './'
-  }).pipe(gulp.dest('dist'))
-    .pipe($.size({title: 'copy'}));
+	return gulp.src([
+		'examples/**',
+		'bower_components/**',
+		'learn.json',
+		'CNAME',
+		'.nojekyll',
+		'site-assets/favicon.ico'
+	], {
+		dot: true,
+		base: './'
+	}).pipe(gulp.dest('dist'))
+	.pipe($.size({title: 'copy'}));
 });
 
 // Compile and Automatically Prefix Stylesheets
 gulp.task('styles', function () {
-  // For best performance, don't add Sass partials to `gulp.src`
-  return gulp.src([
-      'site-assets/*.css',
-    ])
-    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-    .pipe(gulp.dest('dist/site-assets'))
-    .pipe($.size({title: 'styles'}))
-    .pipe(gulp.dest('.tmp/site-assets'));
+	// For best performance, don't add Sass partials to `gulp.src`
+	return gulp.src([
+		'site-assets/*.css'
+	])
+	.pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+	.pipe(gulp.dest('dist/site-assets'))
+	.pipe($.size({title: 'styles'}))
+	.pipe(gulp.dest('.tmp/site-assets'));
 });
 
 // Scan Your HTML For Assets & Optimize Them
 gulp.task('html', function () {
-  var assets = $.useref.assets({searchPath: '{.tmp,.}'});
+	var assets = $.useref.assets({searchPath: '{.tmp,.}'});
 
-  return gulp.src('index.html')
-    .pipe(assets)
-    // Concatenate And Minify JavaScript
-    .pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
-    // Concatenate And Minify Styles
-    .pipe($.if('*.css', $.csso()))
-    .pipe(assets.restore())
-    .pipe($.useref())
-    // Output Files
-    .pipe(gulp.dest('dist'))
-    // Running vulcanize over the written output
-    // because it requires access to the written
-    // CSS and JS.
-    .pipe($.vulcanize({ dest: 'dist', strip: true }))
-    .pipe($.size({title: 'html'}));
+	return gulp.src('index.html')
+		.pipe(assets)
+		// Concatenate And Minify JavaScript
+		.pipe($.if('*.js', $.uglify({preserveComments: 'some'})))
+		// Concatenate And Minify Styles
+		.pipe($.if('*.css', $.csso()))
+		.pipe(assets.restore())
+		.pipe($.useref())
+		// Output Files
+		.pipe(gulp.dest('dist'))
+		// Running vulcanize over the written output
+		// because it requires access to the written
+		// CSS and JS.
+		.pipe($.vulcanize({ dest: 'dist', strip: true }))
+		.pipe($.size({title: 'html'}));
 });
 
 // Clean Output Directory
@@ -92,20 +92,20 @@ gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
 // Build Production Files, the Default Task
 gulp.task('default', ['clean'], function (cb) {
-  runSequence(['styles', 'copy'], ['jshint', 'html', 'images'], cb);
+	runSequence(['styles', 'copy'], ['jshint', 'html', 'images'], cb);
 });
 
 // Run PageSpeed Insights
 // Update `url` below to the public URL for your site
 gulp.task('pagespeed', pagespeed.bind(null, {
-  // By default, we use the PageSpeed Insights
-  // free (no API key) tier. You can use a Google
-  // Developer API key if you have one. See
-  // http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
-  url: 'https://todomvc.com',
-  strategy: 'mobile'
+	// By default, we use the PageSpeed Insights
+	// free (no API key) tier. You can use a Google
+	// Developer API key if you have one. See
+	// http://goo.gl/RkN0vE for info key: 'YOUR_API_KEY'
+	url: 'https://todomvc.com',
+	strategy: 'mobile'
 }));
 
 gulp.task('serve', function (cb) {
-  app.listen(8080, cb);
+	app.listen(8080, cb);
 });
