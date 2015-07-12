fa37e0f201e380f8cd032d14bb29971882cd0f31
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-07-12 18:23:52 +0200

    Vanilladart: new app UI & switch to npm for dependencies.
    * Included `todomvc-app-css`, this includes the new UI.
    * Replaced bower by NPM.
    * Include all files that aren't needed in the repo in the gitignore
    
    Ref gh-1110

---
+++ examples/vanilladart/build/web/dart/packages/browser/interop.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// TODO(jmesserly): remove this script after a deprecation period.
+if (typeof console == "object" && typeof console.warn == "function") {
+  console.warn('<script src="packages/browser/interop.js"> is no longer ' +
+      'needed for dart:js. See http://pub.dartlang.org/packages/browser.');
+}
