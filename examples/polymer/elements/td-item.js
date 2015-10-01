de865c4457da3898caa92cbb33b8a6b44092d997
Sam Saccone <samccone@gmail.com>
2015-09-30 18:27:00 -0700

    Extract js to external files
    * Also fix minor jscs spacing issues

---
+++ examples/polymer/elements/td-item.js
@@ -0,0 +1,61 @@
+(function () {
+	'use strict';
+
+	Polymer({
+		is: 'td-item',
+		extends: 'li',
+		properties: {
+			editing: {
+				type: Boolean,
+				value: false
+			},
+			item: {
+				type: Object,
+				value: function () {
+					return {};
+				}
+			}
+		},
+		observers: ['setRootClass(item.completed, editing)'],
+		setRootClass: function (completed, editing) {
+			this.classList[completed ? 'add' : 'remove']('completed');
+			this.classList[editing ? 'add' : 'remove']('editing');
+		},
+		onBlur: function () {
+			this.commitAction();
+			this.editing = false;
+		},
+		editAction: function () {
+			this.editing = true;
+			this.async(function () {
+				var elm = this.querySelector('#edit');
+				// It looks like polymer is trying to be smart here and not updating the
+				// title attribute on the input when it is changed. To work around this, we manually have
+				// to set the value again when we go into edit mode.
+				elm.value = this.item.title;
+				elm.focus();
+			});
+		},
+		commitAction: function () {
+			if (this.editing) {
+				this.editing = false;
+				this.set('item.title', this.querySelector('#edit').value.trim());
+				if (this.item.title === '') {
+					this.destroyAction();
+				}
+				this.fire('td-item-changed');
+			}
+		},
+		cancelAction: function () {
+			this.editing = false;
+		},
+		itemChangeAction: function (e, details) {
+			this.set('item.completed', e.target.checked);
+			this.fire('td-item-changed');
+		},
+		destroyAction: function () {
+			this.fire('td-destroy-item', this.item);
+		}
+	});
+})();
+
