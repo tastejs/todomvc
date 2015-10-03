ee6b9f574bbda5583bfb67ffdc810e60090e0148
Eric Bidelman <ebidel@gmail.com>
2015-10-03 08:48:02 -0700

    Feedback

--- examples/polymer/vulcanize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# vulcanize elements.html \
-#     --inline-script --inline-css --strip-comments | \
-#     crisper -h elements.build.html -j elements.build.js
-
-polybuild --maximum-crush elements/elements.html
