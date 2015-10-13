6d482c8cff5c4c74acdfbd3f1a7d5ee43252fafa
remojansen <remo.jansen@wolksoftware.com>
2015-10-13 20:21:46 +0100

    new app typescript-react

---
+++ examples/typescript-react/js/constants.ts
@@ -0,0 +1,8 @@
+
+const ALL_TODOS = 'all';
+const ACTIVE_TODOS = 'active';
+const COMPLETED_TODOS = 'completed';
+const ENTER_KEY = 13;
+const ESCAPE_KEY = 27;
+
+export { ALL_TODOS, ACTIVE_TODOS, COMPLETED_TODOS, ENTER_KEY, ESCAPE_KEY };
