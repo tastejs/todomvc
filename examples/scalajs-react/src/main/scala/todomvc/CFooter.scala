d046c9fafab6ec4e08d4e406e4cdb61349ef710d
TasteBot <travis@rdrei.net>
2015-10-18 15:52:15 +0000

    update the build files for gh-pages [ci skip]

--- examples/scalajs-react/src/main/scala/todomvc/CFooter.scala
+++ examples/scalajs-react/src/main/scala/todomvc/CFooter.scala
@@ -1,67 +1,51 @@
 package todomvc
 
-import japgolly.scalajs.react.ScalazReact._
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.prefix_<^._
-
-import scalaz.effect.IO
-import scalaz.syntax.std.list._
-import scalaz.syntax.std.string._
+import org.scalajs.dom.html
 
 object CFooter {
 
-  case class Props private[CFooter](
+  case class Props(
     filterLink:       TodoFilter => ReactTag,
-    onClearCompleted: IO[Unit],
+    onClearCompleted: Callback,
     currentFilter:    TodoFilter,
     activeCount:      Int,
     completedCount:   Int
   )
 
-  case class Backend($: BackendScope[Props, Unit]) {
-    val clearButton =
-      <.button(^.className := "clear-completed", ^.onClick ~~> $.props.onClearCompleted, "Clear completed")
+  class Backend($: BackendScope[Props, Unit]) {
+    def clearButton(P: Props): ReactTagOf[html.Button] =
+      <.button(
+        ^.className := "clear-completed",
+        ^.onClick --> P.onClearCompleted,
+        "Clear completed",
+        (P.completedCount == 0) ?= ^.visibility.hidden
+      )
 
-    def filterLink(s: TodoFilter) =
-      <.li($.props.filterLink(s)(($.props.currentFilter == s) ?= (^.className := "selected"), s.title))
+    def filterLink(P: Props)(s: TodoFilter): ReactTagOf[html.LI] =
+      <.li(P.filterLink(s)((P.currentFilter == s) ?= (^.className := "selected"), s.title))
 
-    def withSpaces(ts: TagMod*) =
-      ts.toList.intersperse(" ")
-
-    def render =
+    def render(P: Props): ReactTagOf[html.Element] =
       <.footer(
         ^.className := "footer",
         <.span(
           ^.className := "todo-count",
-          withSpaces(<.strong($.props.activeCount), "item" plural $.props.activeCount, "left")
+          <.strong(P.activeCount),
+          s" ${if (P.activeCount == 1) "item" else "items"} left"
         ),
         <.ul(
           ^.className := "filters",
-          withSpaces(TodoFilter.values map filterLink)
+          TodoFilter.values map filterLink(P)
         ),
-        clearButton(($.props.completedCount == 0) ?= ^.visibility.hidden)
+        clearButton(P)
       )
   }
 
   private val component = ReactComponentB[Props]("CFooter")
     .stateless
-    .backend(Backend)
-    .render(_.backend.render)
+    .renderBackend[Backend]
     .build
 
-  def apply(filterLink:      TodoFilter => ReactTag,
-            onClearCompleted:IO[Unit],
-            currentFilter:   TodoFilter,
-            activeCount:     Int,
-            completedCount:  Int) =
-
-    component(
-      Props(
-        filterLink       = filterLink,
-        onClearCompleted = onClearCompleted,
-        currentFilter    = currentFilter,
-        activeCount      = activeCount,
-        completedCount   = completedCount
-      )
-    )
+  def apply(P: Props) = component(P)
 }
