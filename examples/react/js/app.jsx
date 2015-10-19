ee2348ceeb87970fa4e5d82e5ab45e2e9e295cd3
Pascal Hartig <phartig@rdrei.net>
2015-10-19 16:45:48 +0100

    React: Remove direct DOM manipulation
    Turn the `<input>` into a
    [Controlled Component](https://facebook.github.io/react/docs/forms.html#controlled-components)
    and avoid fiddling with the DOM in an imperative way.

--- examples/react/js/app.jsx
+++ examples/react/js/app.jsx
@@ -20,7 +20,8 @@ var app = app || {};
 		getInitialState: function () {
 			return {
 				nowShowing: app.ALL_TODOS,
-				editing: null
+				editing: null,
+				newTodo: ''
 			};
 		},
 
@@ -34,18 +35,22 @@ var app = app || {};
 			router.init('/');
 		},
 
+		handleChange: function (event) {
+			this.setState({newTodo: event.target.value});
+		},
+
 		handleNewTodoKeyDown: function (event) {
-			if (event.keyCode !== ENTER_KEY) {
+			if (event.keyCode != ENTER_KEY) {
 				return;
 			}
 
 			event.preventDefault();
 
-			var val = React.findDOMNode(this.refs.newField).value.trim();
+			var val = this.state.newTodo.trim();
 
 			if (val) {
 				this.props.model.addTodo(val);
-				React.findDOMNode(this.refs.newField).value = '';
+				this.setState({newTodo: ''});
 			}
 		},
 
@@ -147,10 +152,11 @@ var app = app || {};
 					<header className="header">
 						<h1>todos</h1>
 						<input
-							ref="newField"
 							className="new-todo"
 							placeholder="What needs to be done?"
+							value={this.state.newTodo}
 							onKeyDown={this.handleNewTodoKeyDown}
+							onChange={this.handleChange}
 							autoFocus={true}
 						/>
 					</header>
