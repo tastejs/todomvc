fdbc8455da6816c3a7c855e79d6598c0a4bd9d8c
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-12-21 10:35:39 +0000

    Updated to Angular 2 beta
    Updated app to Angular 2 beta
      + Removed @View decorator
      + Changed directives from snake-case to camel-case
      + Added new dependency to Rx
      + Removed olf dependency to traceur (not sure this was actually needed?)
    
    Changes to build process
     + Moved typescript configration to tsconfig.json in order to make it a little easier to understand / visualise
    
    Updated README

--- examples/angular2/services/store.js
+++ examples/angular2/services/store.js
@@ -1,16 +1,5 @@
-/// <reference path="../typings/node-uuid/node-uuid.d.ts" />
-/// <reference path="../typings/storejs/storejs.d.ts" />
-/// <reference path="../typings/angular2/angular2.d.ts" />
-if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
-    switch (arguments.length) {
-        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
-        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
-        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
-    }
-};
-var angular2_1 = require('angular2/angular2');
 var uuid = require('node-uuid');
+require('store.js');
 var Todo = (function () {
     function Todo(title) {
         this.uid = uuid.v4();
@@ -82,9 +71,7 @@ var TodoStore = (function () {
         this.todos.push(new Todo(title));
         this._updateStore();
     };
-    TodoStore = __decorate([
-        angular2_1.Injectable()
-    ], TodoStore);
     return TodoStore;
 })();
 exports.TodoStore = TodoStore;
+//# sourceMappingURL=store.js.map
