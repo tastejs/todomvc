f95b8ad2748766a131a13b2bdf344c067a21cc62
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-12-22 06:12:54 +0000

    Moved to a more conventional angular 2 structure - seperated bootstrap

---
+++ examples/angular2/app/app.ts
@@ -0,0 +1,49 @@
+import {Component} from 'angular2/core';
+import {TodoStore, Todo} from './services/store';
+
+const ESC_KEY = 27;
+const ENTER_KEY = 13;
+
+@Component({
+	selector: 'todo-app',
+	templateUrl: 'app/app.html'
+})
+export default class TodoApp {
+	todoStore: TodoStore;
+	constructor() {
+		this.todoStore = new TodoStore();
+	}
+	stopEditing(todo: Todo, editedTitle) {
+		todo.setTitle(editedTitle.value);
+		todo.editing = false;
+	}
+	cancelEditingTodo(todo: Todo) { todo.editing = false; }
+	updateEditingTodo(editedTitle, todo: Todo) {
+		editedTitle = editedTitle.value.trim();
+		todo.editing = false;
+
+		if (editedTitle.length === 0) {
+			return this.todoStore.remove(todo);
+		}
+
+		todo.setTitle(editedTitle);
+	}
+	editTodo(todo: Todo) {
+		todo.editing = true;
+	}
+	removeCompleted() {
+		this.todoStore.removeCompleted();
+	}
+	toggleCompletion(todo: Todo) {
+		this.todoStore.toggleCompletion(todo);
+	}
+	remove(todo: Todo){
+		this.todoStore.remove(todo);
+	}
+	addTodo($event, newtodo) {
+		if ($event.which === ENTER_KEY && newtodo.value.trim().length) {
+			this.todoStore.add(newtodo.value);
+			newtodo.value = '';
+		}
+	}
+}
---
+++ examples/angular2/app/bootstrap.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser';
+import TodoApp from './app'
+
+bootstrap(TodoApp);
