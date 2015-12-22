e5d026c10bb3eb8441dac2bb93be7fab0498f546
mriccia <mricciardi90@gmail.com>
2015-12-22 14:53:05 +0000

    Including @samccone suggestions

--- site-assets/learn.js
+++ site-assets/learn.js
@@ -1,119 +1,6 @@
-/* global _ */
-(function ($) {
+/* global _, $ */
+(function ($, _) {
     'use strict';
-
-    /* jshint ignore:start */
-    // Underscore's Template Module
-    // Courtesy of underscorejs.org
-    var _ = (function (_) {
-        _.defaults = function (object) {
-            if (!object) {
-                return object;
-            }
-            for (var argsIndex = 1, argsLength = arguments.length; argsIndex < argsLength; argsIndex++) {
-                var iterable = arguments[argsIndex];
-                if (iterable) {
-                    for (var key in iterable) {
-                        if (object[key] == null) {
-                            object[key] = iterable[key];
-                        }
-                    }
-                }
-            }
-            return object;
-        };
-
-        // By default, Underscore uses ERB-style template delimiters, change the
-        // following template settings to use alternative delimiters.
-        _.templateSettings = {
-            evaluate    : /<%([\s\S]+?)%>/g,
-            interpolate : /<%=([\s\S]+?)%>/g,
-            escape      : /<%-([\s\S]+?)%>/g
-        };
-
-        // When customizing `templateSettings`, if you don't want to define an
-        // interpolation, evaluation or escaping regex, we need one that is
-        // guaranteed not to match.
-        var noMatch = /(.)^/;
-
-        // Certain characters need to be escaped so that they can be put into a
-        // string literal.
-        var escapes = {
-            "'":      "'",
-            '\\':     '\\',
-            '\r':     'r',
-            '\n':     'n',
-            '\t':     't',
-            '\u2028': 'u2028',
-            '\u2029': 'u2029'
-        };
-
-        var escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g;
-
-        // JavaScript micro-templating, similar to John Resig's implementation.
-        // Underscore templating handles arbitrary delimiters, preserves whitespace,
-        // and correctly escapes quotes within interpolated code.
-        _.template = function(text, data, settings) {
-            var render;
-            settings = _.defaults({}, settings, _.templateSettings);
-
-            // Combine delimiters into one regular expression via alternation.
-            var matcher = new RegExp([
-                (settings.escape || noMatch).source,
-                (settings.interpolate || noMatch).source,
-                (settings.evaluate || noMatch).source
-            ].join('|') + '|$', 'g');
-
-            // Compile the template source, escaping string literals appropriately.
-            var index = 0;
-            var source = "__p+='";
-            text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
-                source += text.slice(index, offset)
-                    .replace(escaper, function(match) { return '\\' + escapes[match]; });
-
-                if (escape) {
-                    source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'";
-                }
-                if (interpolate) {
-                    source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
-                }
-                if (evaluate) {
-                    source += "';\n" + evaluate + "\n__p+='";
-                }
-                index = offset + match.length;
-                return match;
-            });
-            source += "';\n";
-
-            // If a variable is not specified, place data values in local scope.
-            if (!settings.variable) source = 'with(obj||{}){\n' + source + '}\n';
-
-            source = "var __t,__p='',__j=Array.prototype.join," +
-                "print=function(){__p+=__j.call(arguments,'');};\n" +
-                source + "return __p;\n";
-
-            try {
-                render = new Function(settings.variable || 'obj', '_', source);
-            } catch (e) {
-                e.source = source;
-                throw e;
-            }
-
-            if (data) return render(data, _);
-            var template = function(data) {
-                return render.call(this, data, _);
-            };
-
-            // Provide the compiled function source as a convenience for precompilation.
-            template.source = 'function(' + (settings.variable || 'obj') + '){\n' + source + '}';
-
-            return template;
-        };
-
-        return _;
-    })({});
-    /* jshint ignore:end */
-
     function redirect() {
         if (location.hostname === 'tastejs.github.io') {
             location.href = location.href.replace('tastejs.github.io/todomvc', 'todomvc.com');
@@ -186,11 +73,12 @@
     Learn.prototype.append = function (el) {
         var contentSection = document.createElement('div');
         contentSection.id = el.issueLabel;
-        contentSection.innerHTML = _.template(this.template, el);
+        var compiled = _.template(this.template);
+        contentSection.innerHTML = compiled(el);
         contentSection.className = 'learn';
         var linkToContent = document.createElement('a');
-        linkToContent.href = '#' + el.issueLabel;
-        linkToContent.innerHTML = el.name;
+        linkToContent.href = '#' + _.escape(el.issueLabel);
+        $(linkToContent).text(el.name);
         // Localize demo links
         var demoLinks = contentSection.querySelectorAll('.demo-link');
         Array.prototype.forEach.call(demoLinks, function (demoLink) {
@@ -212,4 +100,4 @@
             element[ element.attr( "href" ) === hash ? "addClass" : "removeClass" ]( "selected" );
         });
     });
-})($);
+})($, _);
