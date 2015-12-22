978abedb8d410633f9451c4c7520c288fa2514e7
dmitriz <dmitri14@gmail.com>
2015-12-22 11:14:21 +0000

    angularjs-perf: Wrap into anonymous functions

--- examples/angularjs-perf/js/controllers/todoCtrl.js
+++ examples/angularjs-perf/js/controllers/todoCtrl.js
@@ -1,95 +1,97 @@
 /*global angular */
-'use strict';
+(function () {
+	'use strict';
 
-angular.module('todoCtrl', [])
+	angular.module('todoCtrl', [])
 
-/**
- * The main controller for the app. The controller:
- * - retrieves and persists the model via the todoStorage service
- * - exposes the model to the template and provides event handlers
- */
-.controller('TodoCtrl', function TodoCtrl($scope, $location, $filter, todoStorage) {
-	var todos = $scope.todos = todoStorage.get();
-
-	$scope.newTodo = '';
-	$scope.remainingCount = $filter('filter')(todos, {completed: false}).length;
-	$scope.editedTodo = null;
-
-	if ($location.path() === '') {
-		$location.path('/');
-	}
-
-	$scope.location = $location;
-
-	$scope.$watch('location.path()', function (path) {
-		$scope.statusFilter = { '/active': {completed: false}, '/completed': {completed: true} }[path];
-	});
-
-	$scope.$watch('remainingCount == 0', function (val) {
-		$scope.allChecked = val;
-	});
-
-	$scope.addTodo = function () {
-		var newTodo = $scope.newTodo.trim();
-		if (newTodo.length === 0) {
-			return;
-		}
-
-		todos.push({
-			title: newTodo,
-			completed: false
-		});
-		todoStorage.put(todos);
+	/**
+	 * The main controller for the app. The controller:
+	 * - retrieves and persists the model via the todoStorage service
+	 * - exposes the model to the template and provides event handlers
+	 */
+	.controller('TodoCtrl', function TodoCtrl($scope, $location, $filter, todoStorage) {
+		var todos = $scope.todos = todoStorage.get();
 
 		$scope.newTodo = '';
-		$scope.remainingCount++;
-	};
-
-	$scope.editTodo = function (todo) {
-		$scope.editedTodo = todo;
-		// Clone the original todo to restore it on demand.
-		$scope.originalTodo = angular.extend({}, todo);
-	};
-
-	$scope.doneEditing = function (todo) {
+		$scope.remainingCount = $filter('filter')(todos, {completed: false}).length;
 		$scope.editedTodo = null;
-		todo.title = todo.title.trim();
 
-		if (!todo.title) {
-			$scope.removeTodo(todo);
+		if ($location.path() === '') {
+			$location.path('/');
 		}
 
-		todoStorage.put(todos);
-	};
+		$scope.location = $location;
 
-	$scope.revertEditing = function (todo) {
-		todos[todos.indexOf(todo)] = $scope.originalTodo;
-		$scope.doneEditing($scope.originalTodo);
-	};
-
-	$scope.removeTodo = function (todo) {
-		$scope.remainingCount -= todo.completed ? 0 : 1;
-		todos.splice(todos.indexOf(todo), 1);
-		todoStorage.put(todos);
-	};
-
-	$scope.todoCompleted = function (todo) {
-		$scope.remainingCount += todo.completed ? -1 : 1;
-		todoStorage.put(todos);
-	};
-
-	$scope.clearCompletedTodos = function () {
-		$scope.todos = todos = todos.filter(function (val) {
-			return !val.completed;
+		$scope.$watch('location.path()', function (path) {
+			$scope.statusFilter = { '/active': {completed: false}, '/completed': {completed: true} }[path];
 		});
-		todoStorage.put(todos);
-	};
 
-	$scope.markAll = function (completed) {
-		todos.forEach(function (todo) {
-			todo.completed = completed;
+		$scope.$watch('remainingCount == 0', function (val) {
+			$scope.allChecked = val;
 		});
-		$scope.remainingCount = completed ? 0 : todos.length;
-		todoStorage.put(todos);
-	};
-});
+
+		$scope.addTodo = function () {
+			var newTodo = $scope.newTodo.trim();
+			if (newTodo.length === 0) {
+				return;
+			}
+
+			todos.push({
+				title: newTodo,
+				completed: false
+			});
+			todoStorage.put(todos);
+
+			$scope.newTodo = '';
+			$scope.remainingCount++;
+		};
+
+		$scope.editTodo = function (todo) {
+			$scope.editedTodo = todo;
+			// Clone the original todo to restore it on demand.
+			$scope.originalTodo = angular.extend({}, todo);
+		};
+
+		$scope.doneEditing = function (todo) {
+			$scope.editedTodo = null;
+			todo.title = todo.title.trim();
+
+			if (!todo.title) {
+				$scope.removeTodo(todo);
+			}
+
+			todoStorage.put(todos);
+		};
+
+		$scope.revertEditing = function (todo) {
+			todos[todos.indexOf(todo)] = $scope.originalTodo;
+			$scope.doneEditing($scope.originalTodo);
+		};
+
+		$scope.removeTodo = function (todo) {
+			$scope.remainingCount -= todo.completed ? 0 : 1;
+			todos.splice(todos.indexOf(todo), 1);
+			todoStorage.put(todos);
+		};
+
+		$scope.todoCompleted = function (todo) {
+			$scope.remainingCount += todo.completed ? -1 : 1;
+			todoStorage.put(todos);
+		};
+
+		$scope.clearCompletedTodos = function () {
+			$scope.todos = todos = todos.filter(function (val) {
+				return !val.completed;
+			});
+			todoStorage.put(todos);
+		};
+
+		$scope.markAll = function (completed) {
+			todos.forEach(function (todo) {
+				todo.completed = completed;
+			});
+			$scope.remainingCount = completed ? 0 : todos.length;
+			todoStorage.put(todos);
+		};
+	});
+})();
--- examples/angularjs-perf/js/services/todoStorage.js
+++ examples/angularjs-perf/js/services/todoStorage.js
@@ -1,21 +1,23 @@
 /*global angular */
-'use strict';
+(function () {
+	'use strict';
 
-angular.module('todoStorage', [])
+	angular.module('todoStorage', [])
 
-/**
- * Services that persists and retrieves TODOs from localStorage
-*/
-.factory('todoStorage', function () {
-	var STORAGE_ID = 'todos-angularjs-perf';
+	/**
+	 * Services that persists and retrieves TODOs from localStorage
+	*/
+	.factory('todoStorage', function () {
+		var STORAGE_ID = 'todos-angularjs-perf';
 
-	return {
-		get: function () {
-			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-		},
+		return {
+			get: function () {
+				return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+			},
 
-		put: function (todos) {
-			localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
-		}
-	};
-});
+			put: function (todos) {
+				localStorage.setItem(STORAGE_ID, JSON.stringify(todos));
+			}
+		};
+	});
+})();
