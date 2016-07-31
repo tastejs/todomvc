933439560222125f469823a281ff7849eee20789
Yang Bo <atryyang@thoughtworks.com>
2016-07-31 13:04:20 +0800

    Squashed 'examples/binding-scala/' changes from 3385b9d..1e71eb4
    1e71eb4 Publish to GitHub Pages
    d2d2692 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    d22821a Replace Any to Event
    f1a73ab Publish to GitHub Pages
    0e8d094 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    f4077b5 Seperate event handlers and HTML elements
    5c10f67 Publish to GitHub Pages
    4a164d0 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    6661116 Seperate event handlers and HTML elements
    8cd4a2f Publish to GitHub Pages
    b54987f Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    0c51d90 Adjust indention
    181600f Adjust indention
    4ae6c9b Remove unused imports
    8b6b49d Publish to GitHub Pages
    6121028 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    1e6e913 Avoid DOM property `htmlFor` in favor of HTML attribute `for`
    e130601 Avoid deprecated `each` method, in favor of `bind`
    3bd0e84 Publish to GitHub Pages
    7583e90 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    cee4dc1 Avoid MountPoint
    4978768 Add explicit type signature
    7928e68 Publish to GitHub Pages
    2d48f1c Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    d593f00 Avoid DRY string
    b43c268 Add a comment for Todo class
    4adc337 Publish to GitHub Pages
    6126301 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    a9e31ae Update readme.md
    bdb5573 Publish to GitHub Pages
    abd1575 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    4f0f26f Fix compilation error
    1343e1a Upgrade Binding.scala to 7.0.0
    e564d01 Publish to GitHub Pages
    1e7b918 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    17cea8d Use class attributes instead of className
    f4a630f Upgrade Binding.scala version
    a1b4fff Publish to GitHub Pages
    42301b5 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    51742f0 More clear indention
    575fa93 Publish to GitHub Pages
    4d1e2b6 Merge branch 'gh-pages' of github.com:ThoughtWorksInc/todo into gh-pages
    5773f76 Update Main.scala
    
    git-subtree-dir: examples/binding-scala
    git-subtree-split: 1e71eb427fe2365b8f014c8bbc8f8978a5efd5c1

--- js/build.sbt
+++ js/build.sbt
@@ -1,6 +1,6 @@
 enablePlugins(ScalaJsMap)
 
-libraryDependencies += "com.thoughtworks.binding" %%% "dom" % "4.0.2"
+libraryDependencies += "com.thoughtworks.binding" %%% "dom" % "7.0.0"
 
 libraryDependencies += "com.lihaoyi" %%% "upickle" % "0.3.9"
 
--- js/src/main/scala/com/thoughtworks/todo/Main.scala
+++ js/src/main/scala/com/thoughtworks/todo/Main.scala
@@ -1,8 +1,8 @@
 package com.thoughtworks.todo
 
+import com.thoughtworks.binding.Binding
 import com.thoughtworks.binding.Binding._
 import com.thoughtworks.binding.dom
-import scala.collection.GenSeq
 import scala.scalajs.js.annotation.JSExport
 import org.scalajs.dom._
 import org.scalajs.dom.ext._
@@ -11,8 +11,8 @@ import upickle.default._
 
 @JSExport object Main {
 
+  /** @note [[Todo]] is not a case class because we want to distinguish two [[Todo]]s with the same content */
   final class Todo(val title: String, val completed: Boolean)
-
   object Todo {
     def apply(title: String, completed: Boolean) = new Todo(title, completed)
     def unapply(todo: Todo) = Option((todo.title, todo.completed))
@@ -21,21 +21,14 @@ import upickle.default._
   final case class TodoList(text: String, hash: String, items: BindingSeq[Todo])
 
   object Models {
-    def load() = LocalStorage("todos-binding.scala") match {
-      case None => Seq()
-      case Some(json) => read[Seq[Todo]](json)
-    }
-    def save(todos: Seq[Todo]) = LocalStorage("todos-binding.scala") = write(todos)
+    val LocalStorageName = "todos-binding.scala"
+    def load() = LocalStorage(LocalStorageName).toSeq.flatMap(read[Seq[Todo]])
+    def save(todos: Seq[Todo]) = LocalStorage(LocalStorageName) = write(todos)
 
     val allTodos = Vars[Todo](load(): _*)
 
-    val localStorageMountPoint = new MultiMountPoint(allTodos) {
-      override def set(newValue: Seq[Todo]) = save(newValue)
-      override def splice(oldSeq: Seq[Todo], from: Int, that: GenSeq[Todo], replaced: Int) = {
-        save(oldSeq.view(0, from) ++ that ++ oldSeq.view(from + replaced, oldSeq.length))
-      }
-    }
-    localStorageMountPoint.watch()
+    @dom val autoSave: Binding[Unit] = save(allTodos.bind)
+    autoSave.watch()
 
     val editingTodo = Var[Option[Todo]](None)
 
@@ -44,103 +37,106 @@ import upickle.default._
     val completed = TodoList("Completed", "#/completed", for {todo <- allTodos if todo.completed} yield todo)
     val todoLists = Seq(all, active, completed)
 
-    def getCurrentTodoList = todoLists.find(_.hash == location.hash).getOrElse(all)
+    def getCurrentTodoList = todoLists.find(_.hash == window.location.hash).getOrElse(all)
     val currentTodoList = Var(getCurrentTodoList)
-    @dom val hashBinding = location.hash = currentTodoList.each.hash
+    @dom val hashBinding: Binding[Unit] = window.location.hash = currentTodoList.bind.hash
     hashBinding.watch()
-    window.onhashchange = { _: Any => currentTodoList := getCurrentTodoList }
+    window.onhashchange = { _: Event => currentTodoList := getCurrentTodoList }
   }
   import Models._
 
   @dom def header = {
-    <header className="header">
-      <h1>todos</h1>
-      <input className="new-todo" autofocus={true} placeholder="What needs to be done?" onkeydown={event: KeyboardEvent =>
-      event.keyCode match {
-        case KeyCode.Enter =>
-          dom.currentTarget[HTMLInputElement].value.trim match {
+    val keyDownHandler = { event: KeyboardEvent =>
+      (event.currentTarget, event.keyCode) match {
+        case (input: HTMLInputElement, KeyCode.Enter) =>
+          input.value.trim match {
             case "" =>
             case title =>
-              allTodos.get += new Todo(title, false)
-              dom.currentTarget[HTMLInputElement].value = ""
+              allTodos.get += Todo(title, completed = false)
+              input.value = ""
           }
         case _ =>
-        }
-      }/>
+      }
+    }
+    <header class="header">
+      <h1>todos</h1>
+      <input class="new-todo" autofocus={true} placeholder="What needs to be done?" onkeydown={keyDownHandler}/>
     </header>
   }
 
   @dom def todoListItem(todo: Todo) = {
+    // onblur is not only triggered by user interaction, but also triggered by programmatic DOM changes.
+    // In order to suppress this behavior, we have to replace the onblur event listener to a dummy handler before programmatic DOM changes.
     val suppressOnBlur = Var(false)
-    def submit(newTitle: String): Unit = {
+    def ignoreEvent = { _: Event => }
+    def submit = { event: Event =>
       suppressOnBlur := true
       editingTodo := None
-      newTitle.trim match {
+      event.currentTarget.asInstanceOf[HTMLInputElement].value.trim match {
         case "" =>
           allTodos.get.remove(allTodos.get.indexOf(todo))
         case trimmedTitle =>
           allTodos.get(allTodos.get.indexOf(todo)) = new Todo(trimmedTitle, todo.completed)
       }
     }
-    val edit = <input className="edit" value={ todo.title } onblur={ 
-      if (suppressOnBlur.each) { _: Any =>
-    } else { _: Any =>
-      submit(dom.currentTarget[HTMLInputElement].value)
-      }
-    } onkeydown={ event: KeyboardEvent =>
+    def keyDownHandler = { event: KeyboardEvent =>
       event.keyCode match {
         case KeyCode.Escape =>
           suppressOnBlur := true
           editingTodo := None
         case KeyCode.Enter =>
-          submit(dom.currentTarget[HTMLInputElement].value)
+          submit(event)
         case _ =>
       }
-    }/>;
-    <li className={s"${if (todo.completed) "completed" else ""} ${if (editingTodo.each.contains(todo)) "editing" else ""}"}>
-      <div className="view">
-        <input className="toggle" type="checkbox" checked={todo.completed} onclick={_: Any =>
+    }
+    val edit = <input class="edit" value={ todo.title } onblur={if (suppressOnBlur.bind) ignoreEvent else submit} onkeydown={keyDownHandler} />;
+    <li class={s"${if (todo.completed) "completed" else ""} ${if (editingTodo.bind.contains(todo)) "editing" else ""}"}>
+      <div class="view">
+        <input class="toggle" type="checkbox" checked={todo.completed} onclick={_: Event =>
           allTodos.get(allTodos.get.indexOf(todo)) = new Todo(todo.title, dom.currentTarget[HTMLInputElement].checked)
         }/>
-        <label ondblclick={ _: Any => editingTodo := Some(todo); edit.focus() }>{ todo.title }</label>
-        <button className="destroy" onclick={_: Any => allTodos.get.remove(allTodos.get.indexOf(todo))}></button>
+        <label ondblclick={ _: Event => editingTodo := Some(todo); edit.focus() }>{ todo.title }</label>
+        <button class="destroy" onclick={_: Event => allTodos.get.remove(allTodos.get.indexOf(todo))}></button>
       </div>
       { edit }
     </li>
   }
 
-  @dom def mainSection = <section className="main" style:display={if (allTodos.length.each == 0) "none" else ""}>
-    <input type="checkbox" className="toggle-all" checked={active.items.length.each == 0} onclick={_: Any =>
-    val newTodos = for {todo <- allTodos.get} yield new Todo(todo.title, dom.currentTarget[HTMLInputElement].checked)
-      allTodos.reset(newTodos: _*)
+  @dom def mainSection = <section class="main" style:display={if (allTodos.length.bind == 0) "none" else ""}>
+    <input type="checkbox" class="toggle-all" checked={active.items.length.bind == 0} onclick={_: Event =>
+      for ((todo, i) <- allTodos.get.zipWithIndex) {
+        if (todo.completed != dom.currentTarget[HTMLInputElement].checked) {
+          allTodos.get(i) = new Todo(todo.title, dom.currentTarget[HTMLInputElement].checked)
+        }
+      }
     }/>
-    <label htmlFor="toggle-all">Mark all as complete</label>
-    <ul className="todo-list">{ for { todo <- currentTodoList.each.items } yield todoListItem(todo).each }</ul>
+    <label for="toggle-all">Mark all as complete</label>
+    <ul class="todo-list">{ for { todo <- currentTodoList.bind.items } yield todoListItem(todo).bind }</ul>
   </section>
 
   @dom def filterListItem(todoList: TodoList) = <li>
-    <a href={ todoList.hash } className={ if (todoList == currentTodoList.each) "selected" else "" }>{ todoList.text }</a>
+    <a href={ todoList.hash } class={ if (todoList == currentTodoList.bind) "selected" else "" }>{ todoList.text }</a>
   </li>
 
-  @dom def footer = <footer className="footer" style:display={if (allTodos.length.each == 0) "none" else ""}>
-    <span className="todo-count">
-      <strong>{ active.items.length.each.toString }</strong> { if (active.items.length.each == 1) "item" else "items"} left
+  @dom def footer = <footer class="footer" style:display={if (allTodos.length.bind == 0) "none" else ""}>
+    <span class="todo-count">
+      <strong>{ active.items.length.bind.toString }</strong> { if (active.items.length.bind == 1) "item" else "items"} left
     </span>
-    <ul className="filters">{ for { todoList <- Constants(todoLists: _*) } yield filterListItem(todoList).each }</ul>
-    <button className="clear-completed"
-            style:visibility={if (completed.items.length.each == 0) "hidden" else "visible"}
-            onclick={_: Any => allTodos.reset((for {todo <- allTodos.get if !todo.completed} yield todo): _*)}>
+    <ul class="filters">{ for { todoList <- Constants(todoLists: _*) } yield filterListItem(todoList).bind }</ul>
+    <button class="clear-completed"
+            style:visibility={if (completed.items.length.bind == 0) "hidden" else "visible"}
+            onclick={_: Event => allTodos.get --= (for {todo <- allTodos.get if todo.completed} yield todo) }>
       Clear completed
     </button>
   </footer>
 
   @dom def todoapp = {
-    <section className="todoapp">{ header.each }{ mainSection.each }{ footer.each }</section>
-      <footer className="info">
-        <p>Double-click to edit a todo</p>
+    <section class="todoapp">{ header.bind }{ mainSection.bind }{ footer.bind }</section>
+    <footer class="info">
+      <p>Double-click to edit a todo</p>
       <p>Written by <a href="https://github.com/atry">Yang Bo</a></p>
       <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-      </footer>
+    </footer>
   }
 
   @JSExport def main() = dom.render(document.body, todoapp)
