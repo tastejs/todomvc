1e1d1e350b6f8e8e4c71fcec8ee0ec0808ebb641
Kevin Marcachi <kevin.marcachi@gmail.com>
2016-11-03 23:27:27 +0100

    Finishing example

--- examples/nucleon/js/models/todos.js
+++ examples/nucleon/js/models/todos.js
@@ -5,6 +5,9 @@
 
     'use strict';
 
+    var ENTER_KEY  = 13;
+    var ESCAPE_KEY = 27;
+
     // Registering a new model in the app
     var model = app.models.add('todos', {
 
@@ -23,12 +26,13 @@
 
         // Add a new todo
         add: function () {
-            // Extract title from new attribute (bound to an input), if not empty
+            // Extract title from "new" attribute (bound to an input), if not empty
             var title = this.new.trim();
             if (title !== '') {
                 // Create a new todo
                 var todo = {
                     title: title,
+                    originalTitle : title,
                     state: 'active',
                     class: 'active',
                     checked: false
@@ -37,8 +41,21 @@
                 this.all.push(todo);
                 // Increment corresponding counter
                 this[todo.state]++;
-                // Clear the input by clear new attribute value
+                // Clear the input by clearing "new" attribute value
                 this.new = '';
+                // Toggle general checkbox
+                this.updateToggleCheckbox();
+                // Save in browser memory
+                this.persist();
+            }
+        },
+
+        // Wait enter key press to add a new todo
+        addOnEnter: function (e) {
+            if (e.keyCode === ENTER_KEY) {
+                e.preventDefault();
+                this.new += ' ';
+                this.add();
             }
         },
 
@@ -46,32 +63,64 @@
         edit: function (todo) {
             // Just need to change class used in the thumb
             todo.class = 'editing';
+            // Focus input
+            var input = app.first('.editing input.edit');
+            input.focus();
         },
 
         // Save a todo
         save: function (todo) {
+            todo.title = todo.title.trim();
             // If todo still has a title value
-            if (todo.title.trim()) {
+            if (todo.title) {
                 // Just need to remove "editing" class
                 todo.class = todo.state;
+                todo.originalTitle = todo.title;
             // Elsewise, remove it
             } else {
                 this.remove(todo);
             }
+            // Save in browser memory
+            this.persist();
+        },
+
+        // Wait for enter key press to save current edited todo
+        saveOnEnter: function (e, todo) {
+            if (e.keyCode === ENTER_KEY) {
+                e.preventDefault();
+                todo.title += ' ';
+                this.save(todo);
+            }
+        },
+
+        // Wait for escape key press to cancel changes
+        cancelOnEscape: function (e, todo) {
+            if (e.keyCode === ESCAPE_KEY) {
+                e.preventDefault();
+                // Put back title as it was
+                todo.title = todo.originalTitle;
+                this.save(todo);
+            }
         },
 
         // Remove a todo
-        remove: function (todo) {
+        remove: function (todo, persist) {
             var index = this.all.indexOf(todo);
             if (index !== -1) {
                 // Remove it from container and decrement the corresponding counter
                 this.all.splice(index, 1);
                 this[todo.state]--;
+                // Toggle general checkbox
+                this.updateToggleCheckbox();
+            }
+            // Save in browser memory if allowed
+            if (persist !== false) {
+                this.persist();
             }
         },
 
         // Toggle a todo
-        toggleTodo: function (todo) {
+        toggleTodo: function (todo, updateToggle, persist) {
             // Decrement corresponding counter
             this[todo.state]--;
             // Toggle todo values
@@ -80,6 +129,14 @@
             todo.state = todo.class;
             // Increment corresponding counter
             this[todo.state]++;
+            // Toggle general checkbox
+            if (updateToggle !== false){
+                this.updateToggleCheckbox();
+            }
+            // Save in browser memory if allowed
+            if (persist !== false) {
+                this.persist();
+            }
         },
 
         // Toggle all todos
@@ -88,9 +145,22 @@
             for(var i = 0, len = this.all.length, todo; i < len; i++) {
                 todo = this.all[i];
                 if (todo.checked !== this.toggle) {
-                    this.toggleTodo(todo);
+                    this.toggleTodo(todo, false, false);
                 }
             }
+            // Save in browser memory
+            this.persist();
+        },
+
+        // Check or uncheck general checkbox
+        updateToggleCheckbox: function () {
+            if (this.completed === this.all.length) {
+                this.toggle = true;
+            } else {
+                this.toggle = false;
+            }
+            // Save in browser memory
+            this.persist();
         },
 
         // Remove all complete todos
@@ -100,10 +170,37 @@
             for (var i = this.all.length - 1, todo; i >= 0; i--) {
                 todo = this.all[i];
                 if (todo.state === 'completed') {
-                    this.remove(todo);
+                    this.remove(todo, false);
                 }
             }
+            // Save in browser memory if allowed
+            this.persist();
+        },
+
+        // Pluralize a word
+        pluralize: function (text, count) {
+            if (count === 0 || count > 1) {
+                text += 's';
+            }
+
+            return text;
+        },
+
+        // Save model in memory
+        persist: function () {
+            var that = this;
+            setTimeout(function () {
+                app.memory.set('todos', that);
+            }, 0);
         }
     });
+
+    // Fetch values in memory
+    var inMemory = app.memory.get('todos');
+    if (inMemory) {
+        model.hydrate(inMemory);
+    }
+
+    model.persist();
 
 }(this.app));
