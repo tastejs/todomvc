be459c62f86245eaeef8f5e19a992d9093a89430
Zipeng WU <zipeng.wu@gmail.com>
2016-11-24 17:34:16 +0100

    Tab indentation

--- examples/react/src/todoItem.js
+++ examples/react/src/todoItem.js
@@ -1,79 +1,79 @@
-import React from 'react';
-import classNames from 'classnames';
-import {ESCAPE_KEY, ENTER_KEY} from './constants'
+import React from "react";
+import classNames from "classnames";
+import {ESCAPE_KEY, ENTER_KEY} from "./constants";
 
 class TodoItem extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {editText: this.props.todo.title};
-        this.textInput = null;
-    }
+	constructor(props) {
+		super(props);
+		this.state = {editText: this.props.todo.title};
+		this.textInput = null;
+	}
 
-    handleSubmit() {
-        let val = !!this.state.editText ? this.state.editText.trim() : null;
-        if (val) {
-            this.props.onSave(val);
-            this.setState({editText: val});
-        } else {
-            this.props.onDestroy();
-        }
-    }
+	handleSubmit() {
+		let val = !!this.state.editText ? this.state.editText.trim() : null;
+		if (val) {
+			this.props.onSave(val);
+			this.setState({editText: val});
+		} else {
+			this.props.onDestroy();
+		}
+	}
 
-    handleEdit() {
-        this.props.onEdit();
-        this.setState({editText: this.props.todo.title});
-    }
+	handleEdit() {
+		this.props.onEdit();
+		this.setState({editText: this.props.todo.title});
+	}
 
-    handleKeyDown(event) {
-        if (event.which === ESCAPE_KEY) {
-            this.setState({editText: this.props.todo.title});
-            this.props.onCancel(event);
-        } else if (event.which === ENTER_KEY) {
-            this.handleSubmit(event);
-        }
-    }
+	handleKeyDown(event) {
+		if (event.which === ESCAPE_KEY) {
+			this.setState({editText: this.props.todo.title});
+			this.props.onCancel(event);
+		} else if (event.which === ENTER_KEY) {
+			this.handleSubmit(event);
+		}
+	}
 
-    handleChange(event) {
-        if (this.props.editing) {
-            this.setState({editText: event.target.value});
-        }
-    }
+	handleChange(event) {
+		if (this.props.editing) {
+			this.setState({editText: event.target.value});
+		}
+	}
 
-    componentDidUpdate(prevProps) {
-        if (!prevProps.editing && this.props.editing && this.textInput) {
-            this.textInput.focus();
-        }
-    }
+	componentDidUpdate(prevProps) {
+		if (!prevProps.editing && this.props.editing && this.textInput) {
+			this.textInput.focus();
+		}
+	}
 
-    render() {
-        return (
-            <li className={classNames({
-                completed: this.props.todo.completed,
-                editing: this.props.editing
-            })}>
-                <div className="view">
-                    <input
-                        className="toggle"
-                        type="checkbox"
-                        checked={this.props.todo.completed}
-                        onChange={() => this.props.onToggle()}
-                    />
-                    <label onDoubleClick={() => this.handleEdit()}>
-                        {this.props.todo.title}
-                    </label>
-                    <button className="destroy" onClick={(e) => this.props.onDestroy(e)}/>
-                </div>
-                <input
-                    className="edit"
-                    ref={input => this.textInput = input}
-                    value={this.state.editText}
-                    onBlur={() => this.handleSubmit()}
-                    onChange={e => this.handleChange(e)}
-                    onKeyDown={e => this.handleKeyDown(e)}
-                />
-            </li>
-        );
-    }
+	render() {
+		return (
+			<li className={classNames({
+				completed: this.props.todo.completed,
+				editing: this.props.editing
+			})}>
+				<div className="view">
+					<input
+						className="toggle"
+						type="checkbox"
+						checked={this.props.todo.completed}
+						onChange={() => this.props.onToggle()}
+					/>
+					<label onDoubleClick={() => this.handleEdit()}>
+						{this.props.todo.title}
+					</label>
+					<button className="destroy" onClick={(e) => this.props.onDestroy(e)}/>
+				</div>
+				<input
+					className="edit"
+					ref={input => this.textInput = input}
+					value={this.state.editText}
+					onBlur={() => this.handleSubmit()}
+					onChange={e => this.handleChange(e)}
+					onKeyDown={e => this.handleKeyDown(e)}
+				/>
+			</li>
+		);
+	}
 }
 
 export default TodoItem;
--- examples/react/src/utils.js
+++ examples/react/src/utils.js
@@ -1,28 +1,28 @@
 const uuid = () => {
-    let i, random;
-    let uuid = '';
+	let i, random;
+	let uuid = '';
 
-    for (i = 0; i < 32; i++) {
-        random = Math.random() * 16 | 0;
-        if (i === 8 || i === 12 || i === 16 || i === 20) {
-            uuid += '-';
-        }
-        uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random))
-            .toString(16);
-    }
+	for (i = 0; i < 32; i++) {
+		random = Math.random() * 16 | 0;
+		if (i === 8 || i === 12 || i === 16 || i === 20) {
+			uuid += '-';
+		}
+		uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random))
+			.toString(16);
+	}
 
-    return uuid;
+	return uuid;
 }
 
 const pluralize = (count, word) => count === 1 ? word : word + 's';
 
 const store = (namespace, data) => {
-    if (data) {
-        return localStorage.setItem(namespace, JSON.stringify(data));
-    }
+	if (data) {
+		return localStorage.setItem(namespace, JSON.stringify(data));
+	}
 
-    let store = localStorage.getItem(namespace);
-    return (store && JSON.parse(store)) || [];
+	let store = localStorage.getItem(namespace);
+	return (store && JSON.parse(store)) || [];
 }
 
 export {uuid, pluralize, store}
