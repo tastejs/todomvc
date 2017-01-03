05f705552714e1e7f1aece9bebd9c0b7d299df91
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-01-02 20:08:43 -0600

    Adding example with ExtJS 6 version modern

---
+++ examples/extjs6_modern/js/app/view/WrapperModel.js
@@ -0,0 +1,16 @@
+Ext.define('TodoMVC.view.WrapperModel', {
+    extend: 'Ext.app.ViewModel',    
+    alias: 'viewmodel.todomvc',
+    
+    stores: {
+        todomvc: {
+            model: 'TodoMVC.model.Todo',
+            listeners: {
+                update: 'onUpdateRecord',
+                remove: 'onRemoveRecord',
+                add: 'onAddRecord',
+                load: 'onLoadStore'
+            }
+        }
+    }
+});
