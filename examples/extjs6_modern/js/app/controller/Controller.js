e23461538759ed4c5ea78f100cafa2e9d9cad0ff
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2017-01-05 08:51:17 -0600

    fix code style errors

--- examples/extjs6_modern/js/app/controller/Controller.js
+++ examples/extjs6_modern/js/app/controller/Controller.js
@@ -1,255 +1,235 @@
 Ext.define('TodoMVC.controller.Controller', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.todomvc',
-    
     routes: {
         active: 'showActiveItems',
         completed: 'showCompletedItems',
         '/': 'showAllItems'
     },
-    
-    init: function() {
-        
+    init: function () {
+
         /* load record localstorage */
         this.getViewModel().getStore('todomvc').load();
-        
+
     },
-    
-    onLoadStore: function(store) {
-        
+    onLoadStore: function (store) {
+
         console.log('onLoadStore', arguments);
         this.updateLabels(store);
-        
+
     },
-    
-    onKeyupText: function(field, event) {
-        
+    onKeyupText: function (field, event) {
+
         var me = this,
-            store = me.getStore(),
-            value = field.getValue();
-        
-        if(event.getKey() !== Ext.event.Event.ENTER || Ext.isEmpty(value)) {
-            
+                store = me.getStore(),
+                value = field.getValue();
+
+        if (event.getKey() !== Ext.event.Event.ENTER || Ext.isEmpty(value)) {
+
             return;
-            
+
         }
-        
+
         store.add({
             text: value.trim(),
             active: true,
             completed: false
         });
-        
+
         store.sync();
-        
+
         field.reset();
         field.focus();
-        
+
     },
-    
-    onRenderColumnText: function(value, record, dataIndex, cell) {
-        
+    onRenderColumnText: function (value, record, dataIndex, cell) {
+
         console.log('onRenderColumnText', arguments);
-        
-        if( record.data.completed) {
-            
+
+        if (record.data.completed) {
+
             cell.el.addCls('completed');
 
         } else {
-            
+
             cell.el.removeCls('completed');
-            
+
         }
 
         return value;
-        
+
     },
-    
     /**
      * Need to add class to be able to delete
      */
-    onRendererDelete: function(value, record, dataIndex, cell) {
-        
+    onRendererDelete: function (value, record, dataIndex, cell) {
+
         cell.setCls('delete');
-        
+
     },
-    
-    onItemTapGrid: function(dataView, index, row, record, target) {
-        
+    onItemTapGrid: function (dataView, index, row, record, target) {
+
         /* Only if it is the class column delete */
-        if(target.target.className === 'x-gridcell delete') {
-            
+        if (target.target.className === 'x-gridcell delete') {
+
             var store = this.getStore();
             record.erase();
             store.sync();
             this.updateLabels(store);
-            
+
         }
-        
+
     },
-    
-    onRemoveRecord: function(store) {
-        
+    onRemoveRecord: function (store) {
+
         console.log('onRemoveRecord', store);
         store.sync();
-        
+
     },
-    
-    onAddRecord: function(store) {
-        
+    onAddRecord: function (store) {
+
         console.log('onAddRecord', store);
         this.updateLabels(store);
-        
+
     },
-    
-    updateLabels: function(store) {
-        
+    updateLabels: function (store) {
+
         console.log('updateLabels', arguments);
-        
+
         var me = this,
-            btnTotalCompleted = me.lookup('btnTotalCompleted'),
-            lblTotal = me.lookup('lblTotal'),
-            totalCompleted = 0,
-            totalItems = 0;
-        
-        store.each(function(record) {
-            
-            if(record.data.completed) {
-                
+                btnTotalCompleted = me.lookup('btnTotalCompleted'),
+                lblTotal = me.lookup('lblTotal'),
+                totalCompleted = 0,
+                totalItems = 0;
+
+        store.each(function (record) {
+
+            if (record.data.completed) {
+
                 ++totalCompleted;
-                
+
             }
-            
+
             ++totalItems;
-            
+
         }, me, {
             /* Necessary to include all records */
             filtered: true
         });
-        
+
         lblTotal.setData({
             total: totalItems
         });
-        
+
         btnTotalCompleted[totalCompleted > 0 ? 'show' : 'hide']();
-        
+
     },
-    
-    updateColumnText: function(record) {
-        
+    updateColumnText: function (record) {
+
         var grid = this.getView().down('grid'),
-            rowGrid = grid.getItem(record);
-        
-        if( !rowGrid) {
-            
+                rowGrid = grid.getItem(record);
+
+        if (!rowGrid) {
+
             return;
-            
+
         }
-        
-        if( record.data.completed) {
-            
+
+        if (record.data.completed) {
+
             rowGrid.cells[1].addCls('completed');
-            
+
         } else {
-            
+
             rowGrid.cells[1].removeCls('completed');
-            
+
         }
-        
+
     },
-    
-    onUpdateRecord: function(store, record, operation, modifiedFieldNames) {
-        
+    onUpdateRecord: function (store, record, operation, modifiedFieldNames) {
+
         console.log('onUpdateRecord', arguments);
-        
+
         this.updateColumnText(record);
-        
-        if( !modifiedFieldNames) {
-            
+
+        if (!modifiedFieldNames) {
+
             return;
         }
-        
-        if( modifiedFieldNames.indexOf('completed') !== -1) {
-            
+
+        if (modifiedFieldNames.indexOf('completed') !== -1) {
+
             console.log('update completed');
             record.set('active', !record.data.completed);
             store.sync();
             this.updateLabels(store);
-            
+
         }
-        
+
     },
-    
-    onTapBtnAll: function() {
-        
+    onTapBtnAll: function () {
+
         this.redirectTo('/');
-        
+
     },
-    
-    onTapBtnActive: function() {
-        
+    onTapBtnActive: function () {
+
         this.redirectTo('active');
-        
+
     },
-    
-    showAllItems: function() {
-        
+    showAllItems: function () {
+
         var store = this.getStore();
-        
+
         store.clearFilter();
-        
+
     },
-    
-    showActiveItems: function() {
-        
+    showActiveItems: function () {
+
         var store = this.getStore();
-        
+
         store.clearFilter();
         store.filter('active', true);
-        
+
     },
-    
-    showCompletedItems: function() {
-        
+    showCompletedItems: function () {
+
         var store = this.getStore();
-        
+
         store.clearFilter();
         store.filter('completed', true);
-        
+
     },
-    
-    onTapBtnCompleted: function() {
-        
+    onTapBtnCompleted: function () {
+
         this.redirectTo('completed');
-        
+
     },
-    
-    onTapBtnClearCompleted: function() {
-        
+    onTapBtnClearCompleted: function () {
+
         var me = this,
-            store = me.getStore();
-        
-        store.each(function(record) {
-            
-            if(record.data.completed) {
-                
+                store = me.getStore();
+
+        store.each(function (record) {
+
+            if (record.data.completed) {
+
                 record.erase();
-                
+
             }
-            
+
         }, me, {
             /* Necessary to include all records */
             filtered: true
         });
-        
+
         me.updateLabels(store);
-        
+
     },
-    
-    getStore: function() {
-        
+    getStore: function () {
+
         return this.getViewModel().getStore('todomvc');
-        
+
     }
-    
+
 });
