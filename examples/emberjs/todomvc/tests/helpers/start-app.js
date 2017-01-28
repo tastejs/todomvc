29bf8eaf925f0a0b2eb40d2d024b7a6c1825c32f
TasteBot <travis@rdrei.net>
2017-01-28 16:38:23 +0000

    update the build files for gh-pages [ci skip]

--- examples/emberjs/todomvc/tests/helpers/start-app.js
+++ examples/emberjs/todomvc/tests/helpers/start-app.js
@@ -5,8 +5,8 @@ import config from '../../config/environment';
 export default function startApp(attrs) {
 	let application;
 
-	let attributes = Ember.merge({}, config.APP);
-	attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
+	// use defaults, but you can override
+	let attributes = Ember.assign({}, config.APP, attrs);
 
 	Ember.run(() => {
 		application = Application.create(attributes);
