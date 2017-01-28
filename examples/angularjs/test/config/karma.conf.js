4521175a3bc674a2078c517dda67d9e3efb1e9f5
Konstantin Veretennicov <kveretennicov+github@gmail.com>
2017-01-28 17:38:14 +0100

    Fix #1616 (#1745)
    * angularjs: add Jasmine as dev dependency to fix "npm test"
    
    On a TodoMVC clone freshly configured with "npm install", the "npm test"
    failed to run due to missing Karma Jasmine. This adds the necessary
    dependencies, so that instructions from README.md actually work.
    
    * angularjs: fix tests failing due to missing ngResource
    
    * angularjs: update todoCtrl test to follow the change from 3765408

--- examples/angularjs/test/config/karma.conf.js
+++ examples/angularjs/test/config/karma.conf.js
@@ -7,6 +7,7 @@ module.exports = function (config) {
 		files: [
 			'node_modules/angular/angular.js',
 			'node_modules/angular-route/angular-route.js',
+			'node_modules/angular-resource/angular-resource.js',
 			'node_modules/angular-mocks/angular-mocks.js',
 			'js/**/*.js',
 			'test/unit/**/*.js'
