980e7916828483ef86a81ce8f5b6f646df1e20ce
Miguel Camba <miguel.camba@gmail.com>
2017-01-28 16:36:52 +0000

    Update to Ember 2.10 and use HashLocation (#1722)

--- examples/emberjs/todomvc/app/router.js
+++ examples/emberjs/todomvc/app/router.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-	location: config.locationType
+	location: config.locationType,
+	rootURL: config.rootURL
 });
 
 Router.map(function () {
