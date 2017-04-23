d81c55de36db16dcac606d06a460497b118bc007
Gianluca Guarini <gianluca.guarini@gmail.com>
2017-04-24 00:12:41 +0200

    Clean up the code and update to riot@3

--- examples/riotjs/js/app.js
+++ examples/riotjs/js/app.js
@@ -1,7 +1,19 @@
-/*global riot, todoStorage */
+/*global riot, route, store */
 
 (function () {
 	'use strict';
+  // let riot compiling all the external tags
+  // of course this step could be avoided precompiling them to js files
+  riot.compile(function() {
+    riot.mount('todo', { store: store });
 
-	riot.mount('todo', { data: todoStorage.fetch() });
+    // set the router callback
+    // in this case we have only one dimention urls
+    route(function(filter) {
+      store.filter(filter || 'all');
+    });
+
+    // start the router
+    route.start(true);
+  });
 }());
---
+++ examples/riotjs/js/constants.js
@@ -0,0 +1,8 @@
+(function (exports) {
+  'use strict';
+  exports.constants = Object.freeze({
+    ENTER_KEY: 13,
+    ESC_KEY: 27,
+    STORAGE_KEY: 'todos-riotjs'
+  });
+})(window);
