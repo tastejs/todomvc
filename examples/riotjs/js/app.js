b4ec6487b540bdcb4ff7ff3d4d3156e5bf2deedc
Gianluca Guarini <gianluca.guarini@gmail.com>
2017-04-24 21:03:27 +0200

    Fix linting errors and convert spaces into tabs

--- examples/riotjs/js/app.js
+++ examples/riotjs/js/app.js
@@ -1,19 +1,18 @@
 /*global riot, route, store */
-
 (function () {
 	'use strict';
-  // let riot compiling all the external tags
-  // of course this step could be avoided precompiling them to js files
-  riot.compile(function() {
-    riot.mount('todo', { store: store });
+	// let riot compiling all the external tags
+	// of course this step could be avoided precompiling them to js files
+	riot.compile(function () {
+		riot.mount('todo', { store: store });
 
-    // set the router callback
-    // in this case we have only one dimention urls
-    route(function(filter) {
-      store.filter(filter || 'all');
-    });
+		// set the router callback
+		// in this case we have only one dimention urls
+		route(function (filter) {
+			store.filter(filter || 'all');
+		});
 
-    // start the router
-    route.start(true);
-  });
-}());
+		// start the router
+		route.start(true);
+	});
+})();
