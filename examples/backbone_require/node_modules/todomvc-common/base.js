236f790453912dbe800ce1c7c466e124e9aec6a5
serv-inc <serv-inc@users.noreply.github.com>
2017-08-30 11:02:47 +0200

    jshint error: missing semicolon

--- examples/backbone_require/node_modules/todomvc-common/base.js
+++ examples/backbone_require/node_modules/todomvc-common/base.js
@@ -228,7 +228,7 @@
 			xhr.onload = function (e) {
 				var parsedResponse = JSON.parse(e.target.responseText);
 				if (parsedResponse instanceof Array) {
-					var count = parsedResponse.length
+					var count = parsedResponse.length;
 					if (count !== 0) {
 						issueLink.innerHTML = 'This app has ' + count + ' open issues';
 						document.getElementById('issue-count').style.display = 'inline';
