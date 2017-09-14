51346fa50d1ef77c7fd410edbbdcf9bb42cf4c82
lloydzhou <lloydzhou@qq.com>
2017-09-14 11:50:54 +0800

    fix jscs check

--- examples/tplite/js/store.js
+++ examples/tplite/js/store.js
@@ -1,16 +1,16 @@
 (function (exports) {
 
-  'use strict';
+	'use strict';
 
-  var STORAGE_KEY = 'todos-tplite';
+	var STORAGE_KEY = 'todos-tplite';
 
-  exports.todoStorage = {
-    fetch: function () {
-      return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-    },
-    save: function (todos) {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-    }
-  };
+	exports.todoStorage = {
+		fetch: function () {
+			return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+		},
+		save: function (todos) {
+			localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+		}
+	};
 
 })(window);
