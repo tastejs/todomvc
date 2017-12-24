e189ae9f262dace540c36fa1b902354f6d0e43ff
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2017-12-24 19:29:25 +0100

    Indentation fix for examples/firebase-angular2/protractor.conf.js file

--- examples/firebase-angular2/protractor.conf.js
+++ examples/firebase-angular2/protractor.conf.js
@@ -4,25 +4,25 @@
 const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  onPrepare() {
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
-    });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
-  }
+	allScriptsTimeout: 11000,
+	specs: [
+		'./e2e/**/*.e2e-spec.ts'
+	],
+	capabilities: {
+		'browserName': 'chrome'
+	},
+	directConnect: true,
+	baseUrl: 'http://localhost:4200/',
+	framework: 'jasmine',
+	jasmineNodeOpts: {
+		showColors: true,
+		defaultTimeoutInterval: 30000,
+		print: function() {}
+	},
+	onPrepare() {
+		require('ts-node').register({
+			project: 'e2e/tsconfig.e2e.json'
+		});
+		jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+	}
 };
