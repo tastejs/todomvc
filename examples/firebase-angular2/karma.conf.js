3540cb201a4e8749d7fbc3f36198bca6f11c4227
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2017-12-24 18:50:43 +0100

    Indentation fix for examples/firebase-angular2/karma.conf.js file

--- examples/firebase-angular2/karma.conf.js
+++ examples/firebase-angular2/karma.conf.js
@@ -12,11 +12,11 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
+    client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
