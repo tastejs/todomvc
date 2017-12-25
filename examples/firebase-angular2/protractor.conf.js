8acb4ae3b263379471976b55c53fe8fc7eefdb79
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2017-12-25 01:05:09 +0100

    Fix for examples/firebase-angular2/protractor.conf.js file

--- examples/firebase-angular2/protractor.conf.js
+++ examples/firebase-angular2/protractor.conf.js
@@ -8,9 +8,6 @@ exports.config = {
 	specs: [
 		'./e2e/**/*.e2e-spec.ts'
 	],
-	capabilities: {
-		browserName: 'chrome'
-	},
 	baseUrl: 'http://localhost:4200/',
 	framework: 'jasmine',
 	jasmineNodeOpts: {
