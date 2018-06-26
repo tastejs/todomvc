60f53b41ccd43054556ece6c911a3656132f4888
Peter Rybár <pr.rybar@gmail.com>
2018-06-26 20:13:05 +0200

    Update index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -2,8 +2,8 @@ import { createTodoCommands } from './commands'
 
 export default createTodoCommands
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-	// returning false here prevents Cypress from
-	// failing the test
-	return false
-  });
+// Cypress.on('uncaught:exception', (err, runnable) => {
+// 	// returning false here prevents Cypress from
+// 	// failing the test
+// 	return false
+//   });
