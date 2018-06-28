0bfa7132cb34a61c77c29b3f4b84f0b93d697957
Gavin Jefferies <gavin@runtime.io>
2018-06-28 14:41:17 -0700

    Fix cypress test run for vanilla-es6
    wrap item in a 'view' div for inline edit & to pass cypress tests
    use webpack & babel instead of closure compiler to avoid "completed" shortened to "c"
    update to latest todomvc-*
    
    vanilla-es6 saves edit on blur

---
+++ examples/vanilla-es6/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  devtool: 'source-map'
+};
