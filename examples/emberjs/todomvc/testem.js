40e5bab78d1020e58b13996f0485758a9b4f0b57
Zoltan <zdebre@gmail.com>
2018-07-13 21:11:52 +1200

    Update Ember Todomvc to 3.2 with ember-cli-update

--- examples/emberjs/todomvc/testem.js
+++ examples/emberjs/todomvc/testem.js
@@ -1,13 +1,25 @@
-/*jshint node:true*/
 module.exports = {
-	framework: 'qunit',
-	test_page: 'tests/index.html?hidepassed',
-	disable_watching: true,
-	launch_in_ci: [
-		'PhantomJS'
-	],
-	launch_in_dev: [
-		'PhantomJS',
-		'Chrome'
-	]
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: [
+    'Chrome'
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_args: {
+    Chrome: {
+      ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
+        '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
+  }
 };
