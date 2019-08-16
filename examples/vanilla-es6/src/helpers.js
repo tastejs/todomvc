32945451c0deea96316226fa9d19fab96e12f4bb
Alireza <alireza.mh@gmail.com>
2019-08-16 16:18:39 +0430

    chore: refactor qs helper with default argument value

--- examples/vanilla-es6/src/helpers.js
+++ examples/vanilla-es6/src/helpers.js
@@ -2,10 +2,10 @@
  * querySelector wrapper
  *
  * @param {string} selector Selector to query
- * @param {Element} [scope] Optional scope element for the selector
+ * @param {Element | HTMLDocument} [scope] Optional scope element for the selector
  */
-export function qs(selector, scope) {
-	return (scope || document).querySelector(selector);
+export function qs(selector, scope = document) {
+	return scope.querySelector(selector);
 }
 
 /**
