7821ce4283cb7be75e1f7642fd9f480333e9d95a
André Dias <andredias.us@gmail.com>
2019-10-23 19:08:04 -0300

    The h1 title element sets its inner HTML with page title

--- examples/react/js/app.jsx
+++ examples/react/js/app.jsx
@@ -154,7 +154,7 @@ var app = app || {};
 			return (
 				<div>
 					<header className="header">
-						<h1>todos</h1>
+						<h1>{document.title}</h1>
 						<input
 							className="new-todo"
 							placeholder="What needs to be done?"
