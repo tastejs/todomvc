78de35b330843139881229cde1ca1fd982c6ed16
jackhowa <jack.howard@washpost.com>
2020-05-11 10:08:29 -0500

    Add todo options to show logic

--- examples/react-hooks/src/utils/predictTodos.js
+++ examples/react-hooks/src/utils/predictTodos.js
@@ -7,9 +7,13 @@ export const getTodos = keys =>
 [
   'clean bathroom',
   'wash dishes',
-  'go for a run'
+  'go for a run',
+  'go to the store',
+  'go for a bike',
+  'wash teeth'
 ].filter(e => e.indexOf(keys.toLowerCase()) > -1);
 
+// pipe acts like reduce 
 export const fakeRequest = keys =>
   of(getTodos(keys)).pipe(
     tap(_ => console.log(`API CALL at ${new Date()}`))
