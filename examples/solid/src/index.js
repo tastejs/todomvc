93bc295c9ef8f4607bf43d93c9c6086ba3e0dbb5
Basix <basix@basix.tech>
2021-05-17 00:57:12 +0900

    Replace solid-scripts with custom rollup config

--- examples/solid/src/index.js
+++ examples/solid/src/index.js
@@ -1,18 +1,9 @@
 import { render } from 'solid-js/web';
 import App from './components/App';
 import { TodosProvider } from './utils/store'
-import * as serviceWorker from './serviceWorker';
-
-import 'todomvc-common/base.css';
-import 'todomvc-app-css/index.css';
 
 render(() => (
 	<TodosProvider>
 		<App />
 	</TodosProvider>
 ), document.querySelector('section.todoapp'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
