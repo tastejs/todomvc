667d38bc368f4e03848706baaeb379b935e3dd8a
will.tian <will.tian@hyatt.com>
2023-05-12 12:15:21 +0800

    test: add vitest for unit test

---
+++ examples/svelte/vitest.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  plugins: [svelte({ hot: !process.env.VITEST })],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    globals: true,
+    environment: 'jsdom',
+  },
+});
