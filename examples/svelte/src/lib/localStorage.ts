a6c706d68b01f46149894527176311524dfa9fb3
will.tian <will.tian@hyatt.com>
2023-05-12 15:07:50 +0800

    chore(localStorageStore): mark initialValue as optional

--- examples/svelte/src/lib/localStorage.ts
+++ examples/svelte/src/lib/localStorage.ts
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 
 const ls: Record<string, any> = typeof localStorage === 'undefined' ? {} : localStorage;
 
-function localStorageStore<T>({ key, initialValue = {} as T }: { key: string; initialValue: T }) {
+function localStorageStore<T>({ key, initialValue = {} as T }: { key: string; initialValue?: T }) {
 	const store = writable<T>(JSON.parse(ls[key] ?? JSON.stringify(initialValue)));
 	store.subscribe((value) => {
 		ls[key] = JSON.stringify(value);
