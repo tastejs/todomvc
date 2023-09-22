1f700e073b8f5805f28e8fc7dfbb9c0debd47203
Thorsten Kober <tkober@gmail.com>
2023-09-22 11:28:11 -0400

    initial commit

---
+++ updates/javascript-es6/webpack.dev.js
@@ -0,0 +1,18 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+});
