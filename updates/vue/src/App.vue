92cee65c2e20d5327962fb47069c67a77d4d9a0d
Thorsten Kober <tkober@gmail.com>
2023-12-18 10:53:32 -0500

    Add Vue rebuild to updates folder (#2213)

---
+++ updates/vue/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ updates/vue/src/components/TodoHeader.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+  <header class="header">
+    <RouterLink to="/"><h1>todos</h1></RouterLink>
+    <input
+            type="text"
+            class="new-todo"
+            autofocus
+            autocomplete="off"
+            placeholder="What needs to be done?"
+            @keyup.enter="
+                // @ts-ignore
+                $emit('add-todo', $event.target.value);
+                // @ts-ignore
+                $event.target.value = '';
+            "
+      />
+  </header>
+</template>
