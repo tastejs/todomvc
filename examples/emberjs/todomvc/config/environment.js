afe02c22cafde518f644c582a44b9510f93f17ca
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-18 15:08:56 -0500

    Update links

--- examples/emberjs/todomvc/config/environment.js
+++ examples/emberjs/todomvc/config/environment.js
@@ -12,7 +12,7 @@ module.exports = function (environment) {
 		 * These settings are required by todomvc's (the repo) tests
 		 * (And the deployed site -- using GH Pages to point at the pre-built assets in source)
 		 */
-		rootURL: '/examples/ember/dist/',
+		rootURL: '/examples/emberjs/todomvc/dist/',
 		locationType: 'history',
 		EmberENV: {
 			EXTEND_PROTOTYPES: false,
