61f08cd38eecd340b3d7894fea1d8208167919ce
Thorsten Kober <tkober@gmail.com>
2024-06-25 11:29:41 -0400

    add sidebar to es5

--- examples/javascript-es5/scripts/build.js
+++ examples/javascript-es5/scripts/build.js
@@ -7,7 +7,7 @@ const targetDirectory = "./dist";
 
 const htmlFile = "index.html";
 
-const filesToMove = ["node_modules/todomvc-common/base.css", "node_modules/todomvc-app-css/index.css"];
+const filesToMove = ["node_modules/todomvc-common/base.css", "node_modules/todomvc-app-css/index.css", "node_modules/todomvc-common/base.js"];
 
 const copy = async (src, dest) => {
     await fs.copyFile(src, dest);
