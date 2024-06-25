67fd1f27b8b1af27c832577297255de4768f1ff5
Thorsten Kober <tkober@gmail.com>
2024-06-24 20:07:53 -0400

    add sidebar to preact

--- examples/preact/src/index.js
+++ examples/preact/src/index.js
@@ -1,6 +1,8 @@
 import { h, render } from "preact";
 import App from "./app/app";
+
 import "todomvc-app-css/index.css";
+import "todomvc-common/base.css";
 import "./styles.css";
 
 render(<App />, document.querySelector(".todoapp"));
